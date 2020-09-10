16817f6620204c92f26ab10f6e3cc7988cfa653b
전현지 <emproject525@gmail.com>
2020-09-10 11:44:30 +0900

    스토어 생성

--- moka-web/moka-web-wms/src/main/front/src/App.js
+++ moka-web/moka-web-wms/src/main/front/src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
+// router, history
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+// redux, store
+import { Provider } from 'react-redux';
+import ReduxToastr from 'react-redux-toastr';
+import store from './store';
+
+const browserHistory = createBrowserHistory();
 function App() {
     return (
-        <div>
-            <h1>BackOffice</h1>
-        </div>
+        <Provider store={store}>
+            <Router history={browserHistory}>
+                <div>
+                    <h1>BackOffice</h1>
+                </div>
+            </Router>
+            <ReduxToastr
+                timeOut={5000}
+                newestOnTop={true}
+                position="top-right"
+                transitionIn="fadeIn"
+                transitionOut="fadeOut"
+                progressBar
+                closeOnToastrClick
+            />
+        </Provider>
     );
 }
 
---
+++ moka-web/moka-web-wms/src/main/front/src/store/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import { reducer as toastr } from 'react-redux-toastr';
+
+export default combineReducers({
+    toastr
+});

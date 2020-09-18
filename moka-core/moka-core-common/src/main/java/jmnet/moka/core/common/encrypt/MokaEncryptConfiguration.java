289218e6c8fc868c058b577e53866cee9cb5e2ac
박경수 <krapsk@gmail.com>
2020-09-18 14:32:02 +0900

    설정파일 암호화 모듈및 설정 정보 추가

---
+++ moka-core/moka-core-common/src/main/java/jmnet/moka/core/common/encrypt/MokaEncryptConfiguration.java
@@ -0,0 +1,35 @@
+package jmnet.moka.core.common.encrypt;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.jasypt.salt.StringFixedSaltGenerator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:moka_enc.properties")
+public class MokaEncryptConfiguration {
+
+    @Value("${moka.core.encrypt.key}")
+    private String encryptKey;
+    @Value("${moka.core.encrypt.salt}")
+    private String encryptSalt;
+
+    @Bean(name = "encryptorBean")
+    public StringEncryptor stringEncryptor() {
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(encryptKey);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGenerator(new StringFixedSaltGenerator(encryptSalt));
+        config.setStringOutputType("base64");
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}

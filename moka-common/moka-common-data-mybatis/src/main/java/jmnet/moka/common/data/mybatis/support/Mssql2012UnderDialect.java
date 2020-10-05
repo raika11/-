3abbb83742f18d6f52ace01dc803e5b78bf80757
ince <csinpgs@gmail.com>
2020-10-05 17:35:02 +0900

    mybatis 페이징 쿼리 생성시 select distinct, select top, select distinct top 등이 쿼리문 앞에 있을 경우 그 다음에 row_number 쿼리를 삽입 하도록 수정한다.

--- moka-common/moka-common-data-mybatis/src/main/java/jmnet/moka/common/data/mybatis/support/Mssql2012UnderDialect.java
+++ moka-common/moka-common-data-mybatis/src/main/java/jmnet/moka/common/data/mybatis/support/Mssql2012UnderDialect.java
@@ -1,5 +1,8 @@
 package jmnet.moka.common.data.mybatis.support;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 
  * <pre>
@@ -11,7 +14,11 @@ package jmnet.moka.common.data.mybatis.support;
  */
 public class Mssql2012UnderDialect implements Dialect {
 
-	public final static String SELECT_PREFIX = "select";
+	public final static String SELECT = "select";
+
+	public final static String DISTINCT = "distinct";
+
+	public final static String TOP = "top";
 
 	/**
 	 * 
@@ -43,8 +50,8 @@ public class Mssql2012UnderDialect implements Dialect {
 	 * 전달받은 쿼리 문을 페이징 쿼리 적용하여 리턴
 	 * </pre>
 	 * @param sql sql문
-	 * @param offset
-	 * @param scale
+	 * @param offset offset
+	 * @param scale scale
 	 * @return 페이징 쿼리 적용 된 쿼리 문
 	 */
 	private String getSQL(String sql, int offset, int scale) {
@@ -64,8 +71,8 @@ public class Mssql2012UnderDialect implements Dialect {
 	 * 전달받은 쿼리 문을 페이징 쿼리 적용하여 리턴
 	 * </pre>
 	 * @param sql sql문
-	 * @param offset
-	 * @param scale
+	 * @param offset offset
+	 * @param scale scale
 	 * @return 페이징 쿼리 적용 된 쿼리 문
 	 */
 	@Override
@@ -81,25 +88,36 @@ public class Mssql2012UnderDialect implements Dialect {
     */
 	private String getRowNumber(String sql) {
 		StringBuffer sqlBuffer = new StringBuffer(sql);
-		int startOfSelect = sql.toLowerCase().indexOf(SELECT_PREFIX);
+		int startOfSelect = getSelectPrefixIdx(sql);
+		if(startOfSelect > -1) {
+			StringBuffer rownumber = new StringBuffer(50)
+					.append(" ROW_NUMBER() OVER(");
+			int orderByIndex = sql.toLowerCase().lastIndexOf("order by");
+			if (orderByIndex > 0) {
+				rownumber.append(sql.substring(orderByIndex));
+				sqlBuffer = new StringBuffer(sqlBuffer.substring(0, orderByIndex));
+			} else {
+				rownumber.append(" ORDER BY (SELECT 1)");
+			}
+			rownumber.append(") AS row_num,");
 
-		StringBuffer rownumber = new StringBuffer(50)
-				.append(" ROW_NUMBER() OVER(");
-		int orderByIndex = sql.toLowerCase().lastIndexOf("order by");
-		if ( orderByIndex>0) {
-			rownumber.append( sql.substring(orderByIndex) );
-			sqlBuffer = new StringBuffer(sqlBuffer.substring(0, orderByIndex));
-		} else {
-			rownumber.append( " ORDER BY (SELECT 1)" );
+			sqlBuffer.insert(startOfSelect, rownumber);
 		}
-		rownumber.append(") AS row_num,");
-
-		sqlBuffer.insert(startOfSelect+SELECT_PREFIX.length(), rownumber);
 		return sqlBuffer.toString();
 	}
 
-//	public static void main(String[] args) {
-//		Mssql2012UnderDialect d = new Mssql2012UnderDialect();
-//		System.out.println(d.getSQL("select distinct * from ddd where 111 order by dba desc", 10, 50));
-//	}
+	/**
+	 *
+	 * <pre>
+	 * row_number 쿼리를 삽입 할 위치를 찾는다.
+	 * </pre>
+	 * @param sql sql문
+	 * @return row_number 쿼리가 위치 할 index
+	 */
+	private int getSelectPrefixIdx(String sql) {
+		Pattern pattern = Pattern.compile("^\\s++"+SELECT+"(\\s++"+DISTINCT+")?+(\\s++"+TOP+"\\s++\\d*)?+", Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(sql);
+
+		return matcher.find() ? matcher.end() : -1;
+	}
 }

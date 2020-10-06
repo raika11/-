f060a6afeb93f20722dd33145be2b12795b2f5d7
정준영 <fdjy0101@gmail.com>
2020-10-06 11:24:02 +0900

    테스크 컴포넌트 추가

---
+++ moka-web/moka-web-wms/src/main/front/src/pages/Task/TaskComponent.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import Form from 'react-bootstrap/Form';
+import avatar from './image_test.jpg';
+
+const TaskComponent = ({ text }) => {
+    return (
+        <Card className="my-3 bg-light cursor-grab border">
+            <Card.Body className="p-3">
+                <Form.Check type="checkbox" className="float-right" />
+                <p>{text}</p>
+                <Image src={avatar} style={{ width: '32px', height: '32px' }} roundedCircle className="float-right" />
+                <Button variant="primary" size="sm">
+                    View
+                </Button>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default TaskComponent;

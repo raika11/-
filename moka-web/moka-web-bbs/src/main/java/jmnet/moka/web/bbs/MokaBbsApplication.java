cf0157d5e92293c612d4a6ef9105ee7175c45f1e
박경수 <krapsk@gmail.com>
2020-10-20 19:10:08 +0900

    BBS inital commit

---
+++ moka-web/moka-web-bbs/src/main/java/jmnet/moka/web/bbs/MokaBbsApplication.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2020. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package jmnet.moka.web.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokaBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokaBbsApplication.class, args);
+	}
+
+}

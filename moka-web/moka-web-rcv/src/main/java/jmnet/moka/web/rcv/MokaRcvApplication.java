9eacee0046fbd36d321aeffed8fd02b52b81c80c
박성원 <saparksung@gmail.com>
2020-10-20 15:10:25 +0900

    RCV Initial Commit

---
+++ moka-web/moka-web-rcv/src/main/java/jmnet/moka/web/rcv/MokaRcvApplication.java
@@ -0,0 +1,13 @@
+package jmnet.moka.web.rcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokaRcvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokaRcvApplication.class, args);
+	}
+
+}
---
+++ moka-web/moka-web-rcv/src/test/java/jmnet/moka/web/rcv/MokaWebRcvApplicationTests.java
@@ -0,0 +1,13 @@
+package jmnet.moka.web.rcv;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokaWebRcvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7573d9dbc2f15174da3a53eb568377d49f38dd55
전현지 <emproject525@gmail.com>
2020-10-21 17:27:39 +0900

    도메인 관리 action, reducer, saga 수정

--- moka-web/moka-web-wms/src/main/front/src/pages/Domain/DomainAgGrid.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Domain/DomainAgGrid.js
@@ -1,9 +1,9 @@
-import { columnDefs } from './DomainAgGridColumns';
-import React, { useCallback, useEffect, useState } from 'react';
-import { MokaTable } from '@components';
-import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import { changeSearchOption, getDomainList } from '@store/domain';
+import React, { useCallback, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { MokaTable } from '@components';
+import { changeSearchOption, getDomainList } from '@store/domain';
+import { columnDefs } from './DomainAgGridColumns';
 
 /**
  * 도메인 AgGrid 목록
@@ -11,9 +11,9 @@ import { useHistory } from 'react-router-dom';
 const DomainAgGrid = () => {
     const dispatch = useDispatch();
     const history = useHistory();
-    const [domainRows, setDomainRows] = useState([]);
-    const { list, total, search, loading } = useSelector(
+    const { domain, list, total, search, loading } = useSelector(
         (store) => ({
+            domain: store.domain.domain,
             list: store.domain.list,
             total: store.domain.total,
             search: store.domain.search,
@@ -23,62 +23,45 @@ const DomainAgGrid = () => {
 
     useEffect(() => {
         dispatch(getDomainList());
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [dispatch]);
 
-    useEffect(() => {
-        if (list.length > 0) {
-            setDomainRows(
-                list.map((d) => ({
-                    id: String(d.domainId),
-                    domainId: d.domainId,
-                    domainName: d.domainName,
-                    domainUrl: d.domainUrl,
-                    link: `/domain/${d.domainId}`,
-                    useYn: d.useYn,
-                })),
-            );
-        }
-        //console.log(domainRows);
-    }, [list]);
     /**
-     * 테이블에서 검색옵션 변경하는 경우
-     * @param {object} payload 변경된 값
+     * 테이블에서 검색옵션 변경
      */
     const handleChangeSearchOption = useCallback(
-        (search) => {
-            dispatch(getDomainList(changeSearchOption(search), search.key === 'size' && changeSearchOption({ key: 'page', value: 0 })));
+        ({ key, value }) => {
+            dispatch(
+                getDomainList(
+                    changeSearchOption({
+                        ...search,
+                        [key]: value,
+                        page: 0,
+                    }),
+                ),
+            );
         },
-        [dispatch],
+        [dispatch, search],
     );
 
     /**
      * 목록에서 Row클릭
      */
-    const handleRowClicked = useCallback((params) => history.push(params.link), [history]);
+    const handleRowClicked = useCallback((rowData) => history.push(`/domain/${rowData.domainId}`), [history]);
 
     return (
-        <>
-            {/* 간단한 Table */}
-            <MokaTable
-                columnDefs={columnDefs}
-                rowData={domainRows}
-                onRowNodeId={(params) => params.domainId}
-                agGridHeight={600}
-                onRowClicked={handleRowClicked}
-                loading={loading}
-                total={total}
-                page={search.page}
-                size={search.size}
-                onChangeSearchOption={handleChangeSearchOption}
-                preventRowClickCell={['delete']}
-            />
-            {/* 설정 변경가능한 Table */}
-            {/* <div className="ag-theme-moka-grid mb-3" style={{ height: '550px' }}>
-                <AgGridReact columnDefs={columnDefs} rowData={rowData} getRowNodeId={(params) => params.containerSeq} immutableData animateRows />
-            </div>
-            <MokaPagination total={total} page={search.page} size={search.size} onChangeSearchOption={handleChangeSearchOption} /> */}
-        </>
+        <MokaTable
+            columnDefs={columnDefs}
+            rowData={list}
+            onRowNodeId={(rowData) => rowData.domainId}
+            agGridHeight={600}
+            onRowClicked={handleRowClicked}
+            loading={loading}
+            total={total}
+            page={search.page}
+            size={search.size}
+            selected={domain.domainId}
+            onChangeSearchOption={handleChangeSearchOption}
+        />
     );
 };
 
--- moka-web/moka-web-wms/src/main/front/src/pages/Domain/DomainEdit.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Domain/DomainEdit.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { useParams, withRouter } from 'react-router-dom';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+import { toastr } from 'react-redux-toastr';
+
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import { useParams, withRouter } from 'react-router-dom';
-import { useDispatch, useSelector, shallowEqual } from 'react-redux';
-import { clearDomain, getDomain, saveDomain, duplicateCheck, deleteDomain, GET_DOMAIN } from '@store/domain';
+
 import { notification } from '@utils/toastUtil';
-import { toastr } from 'react-redux-toastr';
-import { getApi, getLang } from '@store/codeMgt/codeMgtAction';
+import { clearDomain, getDomain, saveDomain, changeDomain, duplicateCheck, deleteDomain, changeInvalidList, hasRelations, GET_DOMAIN, SAVE_DOMAIN } from '@store/domain';
+import { getApi, getLang } from '@store/codeMgt';
 
 /**
  * 도메인 상세/수정/등록
@@ -16,9 +18,9 @@ import { getApi, getLang } from '@store/codeMgt/codeMgtAction';
  */
 const DomainEdit = ({ history }) => {
     const { domainId: paramId } = useParams();
-
     const elLang = useRef();
     const elApiCodeId = useRef();
+
     // entity
     const [domainId, setDomainId] = useState('');
     const [domainName, setDomainName] = useState('');
@@ -35,12 +37,13 @@ const DomainEdit = ({ history }) => {
     const [domainUrlError, setDomainUrlError] = useState(false);
 
     // getter
-    const { detail, langRows, apiRows, loading } = useSelector(
+    const { domain, langRows, apiRows, loading, invalidList } = useSelector(
         (store) => ({
-            detail: store.domain.detail,
+            domain: store.domain.domain,
             langRows: store.codeMgt.langRows,
             apiRows: store.codeMgt.apiRows,
-            loading: store.loading[GET_DOMAIN],
+            loading: store.loading[GET_DOMAIN] || store.loading[SAVE_DOMAIN],
+            invalidList: store.domain.invalidList,
         }),
         shallowEqual,
     );
@@ -50,7 +53,7 @@ const DomainEdit = ({ history }) => {
      * 각 항목별 값 변경
      * @param target javascript event.target
      */
-    const onChangeValue = ({ target }) => {
+    const handleChangeValue = ({ target }) => {
         const { name, value, checked } = target;
         if (name === 'domainId') {
             const regex = /^[0-9\b]+$/;
@@ -85,28 +88,42 @@ const DomainEdit = ({ history }) => {
      */
     const validate = (domain) => {
         let isInvalid = false;
+        let errList = [];
 
         // 도메인아이디체크
         if (!domain.domainId || domain.domainId === '') {
-            setDomainIdError(true);
+            errList.push({
+                field: 'domainId',
+                reason: '',
+            });
             isInvalid = isInvalid | true;
         } else if (!/^\d{4}$/.test(domain.domainId)) {
-            setDomainIdError(false);
+            errList.push({
+                field: 'domainId',
+                reason: '',
+            });
             isInvalid = isInvalid | true;
         }
 
         // 도메인명 체크
         if (!/[^\s\t\n]+/.test(domain.domainName)) {
-            setDomainNameError(true);
+            errList.push({
+                field: 'domainName',
+                reason: '',
+            });
             isInvalid = isInvalid | true;
         }
 
         // 도메인url 체크
         if (!/[^\s\t\n]+/.test(domain.domainUrl)) {
-            setDomainUrlError(true);
+            errList.push({
+                field: 'domainUrl',
+                reason: '',
+            });
             isInvalid = isInvalid | true;
         }
 
+        dispatch(changeInvalidList(errList));
         return !isInvalid;
     };
 
@@ -117,31 +134,93 @@ const DomainEdit = ({ history }) => {
     }, []);
 
     useEffect(() => {
-        /**
-         * 파라미터 domainId가 있는데 데이터가 없을 경우 로드,
-         * 파라미터가 없는데 데이터가 있을 경우 데이터 clear
-         */
-        if (paramId && paramId !== detail.domainId && !loading) {
+        if (paramId) {
             dispatch(getDomain(paramId));
-        } else if (!paramId && detail.domainId && !loading) {
-            dispatch(clearDomain({ detail: true }));
+        } else {
+            dispatch(clearDomain({ domain: true }));
         }
-    }, [detail.domainId, dispatch, loading, paramId]);
+    }, [dispatch, paramId]);
 
     useEffect(() => {
         // 도메인 데이터 셋팅
         setDomainIdError(false);
         setDomainNameError(false);
         setDomainUrlError(false);
-        setDomainId(detail.domainId || '');
-        setDomainName(detail.domainName || '');
-        setServicePlatform(detail.servicePlatform || 'P');
-        setLang(detail.lang || (elLang.current[0] ? elLang.current[0].value : ''));
-        setUseYn(detail.useYn || 'Y');
-        setDomainUrl(detail.domainUrl || '');
-        setApiCodeId(detail.apiCodeId || (elApiCodeId.current[0] ? elApiCodeId.current[0].value : ''));
-        setDescription(detail.description || '');
-    }, [detail]);
+        setDomainId(domain.domainId || '');
+        setDomainName(domain.domainName || '');
+        setServicePlatform(domain.servicePlatform || 'P');
+        setLang(domain.lang || (elLang.current[0] ? elLang.current[0].value : ''));
+        setUseYn(domain.useYn || 'Y');
+        setDomainUrl(domain.domainUrl || '');
+        setApiCodeId(domain.apiCodeId || (elApiCodeId.current[0] ? elApiCodeId.current[0].value : ''));
+        setDescription(domain.description || '');
+    }, [domain]);
+
+    /**
+     * 도메인 수정
+     * @param {object} tmp 도메인
+     */
+    const updateDomain = (tmp) => {
+        dispatch(
+            saveDomain({
+                type: 'update',
+                actions: [
+                    changeDomain({
+                        ...domain,
+                        ...tmp,
+                    }),
+                ],
+                callback: (response) => {
+                    // 만약 response.header.message로 서버 메세지를 전달해준다면, 그 메세지를 보여준다.
+                    if (response.header.success) {
+                        notification('success', '수정하였습니다.');
+                    } else {
+                        notification('warning', '실패하였습니다.');
+                    }
+                },
+            }),
+        );
+    };
+
+    /**
+     * 도메인 등록
+     * @param {object} tmp 도메인
+     */
+    const insertDomain = (tmp) => {
+        dispatch(
+            duplicateCheck({
+                domainId,
+                callback: (response) => {
+                    const { body } = response;
+
+                    if (!body) {
+                        dispatch(
+                            saveDomain({
+                                type: 'insert',
+                                actions: [
+                                    changeDomain({
+                                        ...domain,
+                                        ...tmp,
+                                    }),
+                                ],
+                                callback: (response) => {
+                                    if (response.header.success) {
+                                        notification('success', '등록하였습니다.');
+                                        history.push(`/domain/${domainId}`);
+                                    } else {
+                                        notification('warning', '실패하였습니다.');
+                                    }
+                                },
+                            }),
+                        );
+                    } else {
+                        console.log('hh');
+                        notification('warning', '중복된 도메인아이디가 존재합니다.');
+                    }
+                },
+            }),
+        );
+    };
 
     /**
      * 저장 이벤트
@@ -150,6 +229,7 @@ const DomainEdit = ({ history }) => {
     const handleSubmit = (event) => {
         event.preventDefault();
         event.stopPropagation();
+
         const tmp = {
             domainId,
             domainName,
@@ -162,48 +242,77 @@ const DomainEdit = ({ history }) => {
         };
 
         if (validate(tmp)) {
-            if (detail.domainId) {
-                // 업데이트
-                dispatch(saveDomain({ type: 'update', domain: tmp }));
+            if (paramId) {
+                updateDomain(tmp);
             } else {
-                // 추가
-                dispatch(
-                    duplicateCheck({
-                        domainId,
-                        unique: () =>
-                            dispatch(
-                                saveDomain({
-                                    type: 'insert',
-                                    domain: tmp,
-                                    success: () => history.push(`/domain/${domainId}`),
-                                }),
-                            ),
-                        duplicate: () => {
-                            console.log('hh');
-                            notification('warning', '중복된 도메인아이디가 존재합니다.');
-                        },
-                    }),
-                );
+                insertDomain(tmp);
             }
         }
     };
 
-    const handleDelete = (e) => {
+    /**
+     * 도메인 삭제
+     * @param {object} response response
+     */
+    const deleteCallback = (response) => {
+        if (response.body) {
+            notification('warning', '관련 아이템이 존재하여 삭제할 수 없습니다.');
+        } else {
+            dispatch(
+                deleteDomain({
+                    domainId: domain.domainId,
+                    callback: (response) => {
+                        if (response.header.success) {
+                            notification('success', '삭제하였습니다.');
+                            history.push('/domain');
+                        } else {
+                            notification('warning', response.header.message);
+                        }
+                    },
+                }),
+            );
+        }
+    };
+
+    /**
+     * 삭제 버튼 클릭
+     */
+    const handleDelete = () => {
         toastr.confirm('정말 삭제하시겠습니까?', {
             onOk: () => {
-                if (detail.domainId) {
-                    dispatch(deleteDomain({ domainId: detail.domainId }));
-                } else {
-                    console.log('삭제 실패');
-                }
+                dispatch(
+                    hasRelations({
+                        domainId: domain.domainId,
+                        callback: deleteCallback,
+                    }),
+                );
             },
             onCancel: () => {},
         });
     };
 
+    useEffect(() => {
+        // invalidList 처리
+        if (invalidList.length > 0) {
+            invalidList.forEach((i) => {
+                if (i.field === 'domainId') {
+                    setDomainIdError(true);
+                }
+                if (i.field === 'domainName') {
+                    setDomainNameError(true);
+                }
+                if (i.field === 'domainUrl') {
+                    setDomainUrlError(true);
+                }
+            });
+        }
+    }, [invalidList]);
+
     return (
         <div className="d-flex justify-content-center mb-20">
+            {loading && <div className="opacity-box"></div>}
             <Form noValidate onSubmit={handleSubmit} style={{ width: 605 }}>
+                {/* 도메인ID */}
                 <Form.Group as={Row}>
                     <Form.Label column xs={3} className="pt-1 pr-0 pl-0 mb-0">
                         <span className="required-text">*</span>도메인 ID
@@ -212,10 +321,10 @@ const DomainEdit = ({ history }) => {
                         <Form.Control
                             type="text"
                             placeholder="ID"
-                            onChange={onChangeValue}
+                            onChange={handleChangeValue}
                             value={domainId}
                             name="domainId"
-                            disabled={detail.domainId && true}
+                            disabled={domain.domainId && true}
                             isInvalid={domainIdError}
                             className="form-control"
                         />
@@ -224,6 +333,7 @@ const DomainEdit = ({ history }) => {
                         숫자 4자리로 입력하세요
                     </Form.Label>
                 </Form.Group>
+                {/* 도메인명 */}
                 <Form.Group as={Row}>
                     <Form.Label column xs={3} className="pt-2 pr-0 pl-0 mb-0">
                         <span className="required-text">*</span>도메인 명
@@ -232,7 +342,7 @@ const DomainEdit = ({ history }) => {
                         <Form.Control
                             type="text"
                             placeholder="도메인 명을 입력하세요"
-                            onChange={onChangeValue}
+                            onChange={handleChangeValue}
                             value={domainName}
                             name="domainName"
                             className="form-control"
@@ -240,6 +350,7 @@ const DomainEdit = ({ history }) => {
                         />
                     </Col>
                 </Form.Group>
+                {/* 도메인주소 */}
                 <Form.Group as={Row}>
                     <Form.Label column xs={3} className="pt-2 pr-0 pl-0 mb-0">
                         <span className="required-text">*</span>도메인 주소
@@ -248,7 +359,7 @@ const DomainEdit = ({ history }) => {
                         <Form.Control
                             type="text"
                             placeholder="도메인 주소에서 http(s)://를 빼고 입력하세요"
-                            onChange={onChangeValue}
+                            onChange={handleChangeValue}
                             value={domainUrl}
                             name="domainUrl"
                             className="form-control"
@@ -256,14 +367,16 @@ const DomainEdit = ({ history }) => {
                         />
                     </Col>
                 </Form.Group>
+                {/* 사용여부 */}
                 <Form.Group as={Row}>
                     <Form.Label column xs={3} className="pt-2 pr-0 pl-0 mr-1 mb-0">
                         <span className="required-text">*</span>사용여부
                     </Form.Label>
                     <Col xs={9} className="px-0 my-auto">
-                        <Form.Check type="switch" id="domain-useYN" label="" name="useYN" onChange={onChangeValue} checked={useYn === 'Y' && true} className="pt-2 pl-4 ml-2" />
+                        <Form.Check type="switch" id="domain-useYN" label="" name="useYN" onChange={handleChangeValue} checked={useYn === 'Y' && true} className="pt-2 pl-4 ml-2" />
                     </Col>
                 </Form.Group>
+                {/* 플랫폼 */}
                 <Form.Group as={Row}>
                     <Form.Label column xs={3} className="pt-1 pr-0 pl-0 mb-0">
                         <span className="required-text">*</span>플랫폼
@@ -276,7 +389,7 @@ const DomainEdit = ({ history }) => {
                             value="P"
                             id="domain-pc"
                             name="servicePlatform"
-                            onChange={onChangeValue}
+                            onChange={handleChangeValue}
                             checked={servicePlatform === 'P' && true}
                         />
                     </div>
@@ -288,17 +401,18 @@ const DomainEdit = ({ history }) => {
                             value="M"
                             id="domain-mobile"
                             name="servicePlatform"
-                            onChange={onChangeValue}
+                            onChange={handleChangeValue}
                             checked={servicePlatform === 'M' && true}
                         />
                     </div>
                 </Form.Group>
+                {/* 언어 */}
                 <Form.Group as={Row}>
                     <Form.Label column xs={3} className="pt-1 mr-0 pr-0 pl-0">
                         언어
                     </Form.Label>
                     <Col xs={4} className="pl-0 ml-0 pr-0 pl-0">
-                        <Form.Control as="select" custom onChange={onChangeValue} value={lang} name="lang" className="form-control" ref={elLang}>
+                        <Form.Control as="select" custom onChange={handleChangeValue} value={lang} name="lang" className="form-control" ref={elLang}>
                             {langRows &&
                                 langRows.map((row) => (
                                     <option key={row.id} value={row.dtlCd}>
@@ -308,12 +422,13 @@ const DomainEdit = ({ history }) => {
                         </Form.Control>
                     </Col>
                 </Form.Group>
+                {/* API 경로 */}
                 <Form.Group as={Row}>
                     <Form.Label column xs={3} className="pt-1 pr-0 pl-0 mb-0">
                         API 경로
                     </Form.Label>
                     <Col xs={9} className="pl-0 ml-0 pr-0 pl-0">
-                        <Form.Control as="select" onChange={onChangeValue} custom value={apiCodeId} name="apiCodeId" className="form-control" ref={elApiCodeId}>
+                        <Form.Control as="select" onChange={handleChangeValue} custom value={apiCodeId} name="apiCodeId" className="form-control" ref={elApiCodeId}>
                             {apiRows &&
                                 apiRows.map((row) => (
                                     <option key={row.id} value={row.dtlCd}>
@@ -323,14 +438,16 @@ const DomainEdit = ({ history }) => {
                         </Form.Control>
                     </Col>
                 </Form.Group>
+                {/* 메모 */}
                 <Form.Group as={Row}>
                     <Form.Label column xs={3} className="pt-2 pr-0 pl-0 mb-0">
                         메모
                     </Form.Label>
                     <Col xs={9} className="pl-0 pr-0">
-                        <Form.Control as="textarea" rows="3" onChange={onChangeValue} value={description} name="description" />
+                        <Form.Control as="textarea" rows="3" onChange={handleChangeValue} value={description} name="description" />
                     </Col>
                 </Form.Group>
+                {/* 버튼 */}
                 <Form.Group as={Row} className="d-flex pt-20 justify-content-center">
                     <Button type="submit" variant="primary" className="float-left mr-10 pr-20 pl-20">
                         저장
@@ -338,7 +455,7 @@ const DomainEdit = ({ history }) => {
                     <Button className="float-left mr-10 pr-20 pl-20" variant="gray150">
                         취소
                     </Button>
-                    {detail.domainId && (
+                    {paramId && (
                         <Button className="float-left mr-0 pr-20 pl-20" variant="gray150" onClick={handleDelete}>
                             삭제
                         </Button>

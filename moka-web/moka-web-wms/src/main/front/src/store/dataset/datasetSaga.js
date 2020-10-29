13bf9eecf94e2e1fdf5c88571c28568e28019879
김태형 <xorwk1@gmail.com>
2020-10-29 12:04:09 +0900

    데이터셋 store 상세조회 추가

--- moka-web/moka-web-wms/src/main/front/src/store/dataset/datasetSaga.js
+++ moka-web/moka-web-wms/src/main/front/src/store/dataset/datasetSaga.js
@@ -1,6 +1,6 @@
 import { takeLatest, put, call, select } from 'redux-saga/effects';
 import { startLoading, finishLoading } from '@store/loading/loadingAction';
-import { callApiAfterActions, createRequestSaga, errorResponse } from '../commons/saga';
+import { callApiAfterActions, createRequestSaga } from '../commons/saga';
 
 import * as datasetAction from './datasetAction';
 import * as datasetAPI from './datasetApi';
@@ -8,7 +8,13 @@ import * as datasetAPI from './datasetApi';
 const getDatasetList = callApiAfterActions(datasetAction.getDatasetList, datasetAPI.getDatasetList, (store) => store.dataset);
 const getDataset = createRequestSaga(datasetAction.GET_DATASET, datasetAPI.getDataset);
 
+/**
+ * 데이터셋 리스트 조회(모달에서 사용하는 리스트)
+ */
+const getDatasetListModal = createRequestSaga(datasetAction.GET_DATASET_LIST_MODAL, datasetAPI.getDatasetList, true);
+
 export default function* saga() {
     yield takeLatest(datasetAction.GET_DATASET_LIST, getDatasetList);
     yield takeLatest(datasetAction.GET_DATASET, getDataset);
+    yield takeLatest(datasetAction.GET_DATASET_LIST_MODAL, getDatasetListModal);
 }

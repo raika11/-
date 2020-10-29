a61ce42b9bec65c64a410bebf4289d47c2688ef8
ince <csinpgs@gmail.com>
2020-10-29 11:59:04 +0900

    메뉴 권한 존재 여부 RequestMapping 정보 수정

--- moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/menu/controller/MenuRestController.java
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/menu/controller/MenuRestController.java
@@ -401,7 +401,7 @@ public class MenuRestController {
      * @throws Exception 그외 모든 에러
      */
     @ApiOperation(value = "메뉴 권한 수정")
-    @DeleteMapping("/auths")
+    @PutMapping("/auths")
     public ResponseEntity<?> deleteMemberMenuAuth(HttpServletRequest request, List<Long> menuAuthSeqs)
             throws Exception {
 
@@ -458,6 +458,43 @@ public class MenuRestController {
     }
 
     /**
+     * 메뉴 권한 존재 여부
+     *
+     * @param request 요청
+     * @param menuId  존재 여부 확인 메뉴ID
+     * @return 존재여부
+     * @throws InvalidDataException 데이타유효성오류
+     * @throws NoDataException      삭제 할 메뉴 없음
+     * @throws Exception            그 외 에러처리
+     */
+    @ApiOperation(value = "메뉴 권한 존재 여부")
+    @GetMapping("/{menuId}/exist-auth")
+    public ResponseEntity<?> existAuth(HttpServletRequest request,
+            @PathVariable("menuId") @Pattern(regexp = "[0-9]{2,8}$", message = "{tps.menu.error.length.menuId}") String menuId)
+            throws InvalidDataException, NoDataException, Exception {
+
+
+        // 메뉴 데이터 조회
+        String noContentMessage = messageByLocale.get("tps.menu.error.no-data", request);
+        menuService
+                .findMenuById(menuId)
+                .orElseThrow(() -> new NoDataException(noContentMessage));
+
+        try {
+            boolean exists = menuService.isUsedGroupOrMember(menuId);
+            // 결과리턴
+            ResultDTO<Boolean> resultDto = new ResultDTO<>(exists, exists ? messageByLocale.get("tps.menu.error.delete.related", request) : null);
+            return new ResponseEntity<>(resultDto, HttpStatus.OK);
+
+        } catch (Exception e) {
+            log.error("[FAIL TO SELECT MENU] menuId: {}) {}", menuId, e.getMessage());
+            // 액션 로그에 에러 로그 출력
+            tpsLogger.error(e);
+            throw new Exception(messageByLocale.get("tps.common.error", request), e);
+        }
+    }
+
+    /**
      * 삭제
      *
      * @param request 요청
@@ -503,41 +540,6 @@ public class MenuRestController {
         }
     }
 
-    /**
-     * 삭제
-     *
-     * @param request 요청
-     * @param menuId  삭제 할 메뉴아이디 (필수)
-     * @return 삭제성공여부
-     * @throws InvalidDataException 데이타유효성오류
-     * @throws NoDataException      삭제 할 메뉴 없음
-     * @throws Exception            그 외 에러처리
-     */
-    @ApiOperation(value = "메뉴 권한 존재 여부")
-    @DeleteMapping("/{menuId}/exist-auth")
-    public ResponseEntity<?> existAuth(HttpServletRequest request,
-            @PathVariable("menuId") @Pattern(regexp = "[0-9]{2,8}$", message = "{tps.menu.error.length.menuId}") String menuId)
-            throws InvalidDataException, NoDataException, Exception {
 
-
-        // 메뉴 데이터 조회
-        String noContentMessage = messageByLocale.get("tps.menu.error.no-data", request);
-        menuService
-                .findMenuById(menuId)
-                .orElseThrow(() -> new NoDataException(noContentMessage));
-
-        try {
-            boolean exists = menuService.isUsedGroupOrMember(menuId);
-            // 결과리턴
-            ResultDTO<Boolean> resultDto = new ResultDTO<>(exists, exists ? messageByLocale.get("tps.menu.error.delete.related", request) : null);
-            return new ResponseEntity<>(resultDto, HttpStatus.OK);
-
-        } catch (Exception e) {
-            log.error("[FAIL TO SELECT MENU] menuId: {}) {}", menuId, e.getMessage());
-            // 액션 로그에 에러 로그 출력
-            tpsLogger.error(e);
-            throw new Exception(messageByLocale.get("tps.common.error", request), e);
-        }
-    }
 
 }

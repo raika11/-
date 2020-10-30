bbeabe0aaaa55d7149c10a13bcd9e7e3038c642e
정준영 <fdjy0101@gmail.com>
2020-10-30 11:16:39 +0900

    기타코드 관리 API를 수정한다.

--- moka-web/moka-web-wms/src/main/front/src/store/codeMgt/codeMgtApi.js
+++ moka-web/moka-web-wms/src/main/front/src/store/codeMgt/codeMgtApi.js
@@ -1,7 +1,7 @@
 import qs from 'qs';
 import instance from '@store/commons/axios';
 
-// 코드 그룹 목록 조회
+// 그룹 목록 조회
 export const getCodeMgtGrpList = ({ grpSearch }) => {
     const queryString = qs.stringify(grpSearch);
     return instance.get(`/api/codeMgtGrps?${queryString}`).catch((err) => {
@@ -13,6 +13,76 @@ export const getCodeMgtGrpList = ({ grpSearch }) => {
 export const getCodeMgtList = ({ cdSearch }) => {
     const queryString = qs.stringify(cdSearch);
     return instance.get(`/api/codeMgtGrps/${cdSearch.grpCd}/codeMgts?${queryString}`).catch((err) => {
+        throw err;
+    });
+};
+
+// 그룹 상세 조회
+export const getCodeMgtGrp = (grpSeq) => {
+    return instance.get(`/api/codeMgtGrps/${grpSeq}`).catch((err) => {
+        throw err;
+    });
+};
+
+// 코드 상세 조회
+export const getCodeMgt = (cdSeq) => {
+    return instance.get(`/api/codeMgtGrps/codeMgts/${cdSeq}`).catch((err) => {
+        throw err;
+    });
+};
+
+// 그룹 등록
+export const postCodeMgtGrp = ({ codeMgtGrp }) => {
+    return instance.post('/api/codeMgtGrps', qs.stringify(codeMgtGrp)).catch((err) => {
+        throw err;
+    });
+};
+
+// 코드 등록
+export const postCodeMgt = ({ codeMgt }) => {
+    const codeMgts = {
+        ...codeMgt,
+        codeMgtGrp: undefined,
+        grpCd: codeMgt.grpCd,
+        seqNo: codeMgt.seqNo,
+        // 'codeMgt.grpCd': codeMgt.codeTypeSeq
+    };
+    return instance.post('/api/codeMgtGrps/codeMgts', qs.stringify(codeMgts)).catch((err) => {
+        throw err;
+    });
+};
+
+// 그룹 수정
+export const putCodeMgtGrp = ({ codeMgtGrp }) => {
+    return instance.put(`/api/codeMgtGrps/${codeMgtGrp.grpSeq}`, qs.stringify(codeMgtGrp)).catch((err) => {
+        throw err;
+    });
+};
+
+// 코드 수정
+// export const putEtccode = ({ etccode }) => {
+//     const etccodeSet = {
+//         ...etccode,
+//         'etccodeType.codeTypeId': etccode.codeTypeId,
+//         'etccodeType.codeTypeSeq': etccode.codeTypeSeq
+//     };
+//     return instance
+//         .put(`/api/codeMgtGrps/codeMgts/${etccodeSet.seq}`, qs.stringify(etccodeSet))
+//         .catch((err) => {
+//             throw err;
+//         });
+// };
+
+// 그룹 삭제
+export const deleteCodeMgtGrp = (grpSeq) => {
+    return instance.delete(`/api/codeMgtGrps/${grpSeq}`).catch((err) => {
+        throw err;
+    });
+};
+
+// 코드 삭제
+export const deleteCodeMgt = (cdSeq) => {
+    return instance.delete(`/api/codeMgtGrps/codeMgts/${cdSeq}`).catch((err) => {
         throw err;
     });
 };

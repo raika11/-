a861b253863a3ec8b690ebd0955ff15465d89482
ince <csinpgs@gmail.com>
2020-11-06 09:08:03 +0900

    [tps] 등록자정보 조회용 MemberSimpleDTO 생성
    Signed-off-by: ince <csinpgs@gmail.com>

---
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/member/dto/MemberSimpleDTO.java
@@ -0,0 +1,45 @@
+package jmnet.moka.core.tps.mvc.member.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.lang.reflect.Type;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * <pre>
+ *
+ * Project : moka
+ * Package : jmnet.moka.core.tps.mvc.member.dto
+ * ClassName : MemberDTO
+ * Created : 2020-10-22 ince
+ * </pre>
+ *
+ * @author ince
+ * @since 2020-10-22 16:08
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@SuperBuilder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MemberSimpleDTO {
+
+    public static final Type TYPE = new TypeReference<List<MemberSimpleDTO>>() {
+    }.getType();
+
+    /**
+     * 사용자ID
+     */
+    private String memberId;
+
+    /**
+     * 사용자명
+     */
+    private String memberNm;
+}

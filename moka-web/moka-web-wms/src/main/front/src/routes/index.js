eaa44302c21ec4e28eabf4cb238dfead2e5bce82
정준영 <fdjy0101@gmail.com>
2020-11-06 18:52:45 +0900

    [react] 머지

--- moka-web/moka-web-wms/src/main/front/src/routes/index.js
+++ moka-web/moka-web-wms/src/main/front/src/routes/index.js
@@ -23,7 +23,6 @@ const Area = React.lazy(() => import('@pages/Area'));
 const Menu = React.lazy(() => import('@pages/Menu'));
 const Group = React.lazy(() => import('@pages/Group'));
 const reporterMgr = React.lazy(() => import('@pages/reporterMgr'));
-const Special = React.lazy(() => import('@pages/Special'));
 
 const routes = [
     {
@@ -217,17 +216,6 @@ const routes = [
         strict: true,
     },
     */
-    {
-        path: '/special',
-        name: 'Special',
-        displayName: '디지털 스페셜 관리',
-        component: Special,
-
-        layout: SidebarOpenLayout,
-        nonResponsive: true,
-        exact: false,
-        strict: true,
-    },
 ];
 
 export default routes;

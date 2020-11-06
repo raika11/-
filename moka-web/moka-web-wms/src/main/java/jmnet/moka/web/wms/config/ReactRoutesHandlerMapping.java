c7ed77cac14a95a6042fc19bff6509bbe3dea539
ince <csinpgs@gmail.com>
2020-11-06 11:05:49 +0900

    [wms] routes 정보를 메뉴 테이블에서 조회한다.
    Signed-off-by: ince <csinpgs@gmail.com>

--- moka-web/moka-web-wms/src/main/java/jmnet/moka/web/wms/config/ReactRoutesHandlerMapping.java
+++ moka-web/moka-web-wms/src/main/java/jmnet/moka/web/wms/config/ReactRoutesHandlerMapping.java
@@ -1,7 +1,7 @@
 package jmnet.moka.web.wms.config;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.AbstractHandlerMapping;
@@ -11,29 +11,40 @@ import org.springframework.web.servlet.handler.AbstractHandlerMapping;
  * React Routes 해당하는 경로의 패핑을 처리한다.
  * 2019. 9. 10. kspark 최초생성
  * </pre>
- * @since 2019. 9. 10. 오후 3:53:26
+ *
  * @author kspark
+ * @since 2019. 9. 10. 오후 3:53:26
  */
 public class ReactRoutesHandlerMapping extends AbstractHandlerMapping {
 
-	
-    private List<String> reactRoutesList;
+
+    private Set<String> reactRoutesList;
     private HandlerMethod reactRoutesHandlerMethod;
-	
-	
+
+
     public ReactRoutesHandlerMapping(String routesPath)
             throws NoSuchMethodException, SecurityException {
         String[] splitted = routesPath.split(",");
-        this.reactRoutesList = new ArrayList<String>(16);
+        this.reactRoutesList = new HashSet<>(16);
         for (String path : splitted) {
             this.reactRoutesList.add(path.trim());
         }
-        this.reactRoutesHandlerMethod =
-                new HandlerMethod(this, this.getClass().getMethod("gotoMain"));
-	}
-	
-	@Override
-	protected Object getHandlerInternal(HttpServletRequest request) throws Exception {
+        this.reactRoutesHandlerMethod = new HandlerMethod(this, this
+                .getClass()
+                .getMethod("gotoMain"));
+    }
+
+    public ReactRoutesHandlerMapping(Set<String> routesPaths)
+            throws NoSuchMethodException, SecurityException {
+        this.reactRoutesList = routesPaths;
+        this.reactRoutesHandlerMethod = new HandlerMethod(this, this
+                .getClass()
+                .getMethod("gotoMain"));
+    }
+
+    @Override
+    protected Object getHandlerInternal(HttpServletRequest request)
+            throws Exception {
         String uri = request.getRequestURI();
         for (String routePath : this.reactRoutesList) {
             if (uri.startsWith(routePath)) {
@@ -45,6 +56,6 @@ public class ReactRoutesHandlerMapping extends AbstractHandlerMapping {
 
     public String gotoMain() {
         return "forward:/index.html";
-	}
+    }
 
 }

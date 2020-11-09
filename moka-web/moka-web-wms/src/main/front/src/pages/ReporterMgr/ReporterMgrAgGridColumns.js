e5a198470d2abf65db6d2d125e203ad8be7ecebd
kimdongkui <ddrsbram@hanmail.net>
2020-11-09 13:25:59 +0900

    [react] 기자관리 신규생성

---
+++ moka-web/moka-web-wms/src/main/front/src/pages/ReporterMgr/ReporterMgrAgGridColumns.js
@@ -0,0 +1,37 @@
+
+import React from 'react';
+import { faCircle } from '@moka/fontawesome-pro-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const localeText = { noRowsToShow: '조회 결과가 없습니다.', loadingOoo: '조회 중입니다..' };
+export const columnDefs = [
+    {
+        headerName: '그룹코드',
+        field: 'groupCd',
+        cellStyle: { textAlign: 'center' },
+        width: 65,
+    },
+    {
+        headerName: '그룹명',
+        field: 'groupNm',
+        // cellClass: 'ag-cell-center',
+        //cellStyle: { textAlign: 'center' },
+        width: 75,
+    },
+    {
+        headerName: '그룹 한글명',
+        field: 'groupKorNm',
+        //cellStyle: { textAlign: 'center' },
+        width: 95,
+    },
+    {
+        headerName: '등록자',
+        field: 'regId',
+        width: 60,
+    },
+    {
+        headerName: '등록일시',
+        field: 'regDt', // 안나옴.
+        width: 135,
+    },
+];
---
+++ moka-web/moka-web-wms/src/main/front/src/pages/ReporterMgr/index.js
@@ -0,0 +1,1 @@
+export { default } from '../../../../../../../../../../Desktop/소스백업/reporterMgr/ReporterMgr';

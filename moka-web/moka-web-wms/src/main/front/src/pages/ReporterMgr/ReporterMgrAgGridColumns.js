96ed8fb3646dab9ec294025b5ec1d738f477d03c
kimdongkui <ddrsbram@hanmail.net>
2020-11-10 09:35:37 +0900

    [react] 기자관리 그리드 컬럼 변경처리

--- moka-web/moka-web-wms/src/main/front/src/pages/ReporterMgr/ReporterMgrAgGridColumns.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/ReporterMgr/ReporterMgrAgGridColumns.js
@@ -5,31 +5,45 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export const localeText = { noRowsToShow: '조회 결과가 없습니다.', loadingOoo: '조회 중입니다..' };
 export const columnDefs = [
     {
-        headerName: '그룹코드',
+        headerName: '동그라미',
         field: 'groupCd',
         cellStyle: { textAlign: 'center' },
         width: 65,
     },
     {
-        headerName: '그룹명',
+        headerName: '번호',
+        field: 'groupCd',
+        cellStyle: { textAlign: 'center' },
+        width: 65,
+    },
+    {
+        headerName: '아이디',
         field: 'groupNm',
-        // cellClass: 'ag-cell-center',
-        //cellStyle: { textAlign: 'center' },
         width: 75,
     },
     {
-        headerName: '그룹 한글명',
+        headerName: '이름',
         field: 'groupKorNm',
         //cellStyle: { textAlign: 'center' },
         width: 95,
     },
     {
-        headerName: '등록자',
+        headerName: '소속',
         field: 'regId',
         width: 60,
     },
     {
-        headerName: '등록일시',
+        headerName: '이메일',
+        field: 'regDt', // 안나옴.
+        width: 135,
+    },
+    {
+        headerName: '노출',
+        field: 'regDt', // 안나옴.
+        width: 135,
+    },
+    {
+        headerName: '기자페이지',
         field: 'regDt', // 안나옴.
         width: 135,
     },

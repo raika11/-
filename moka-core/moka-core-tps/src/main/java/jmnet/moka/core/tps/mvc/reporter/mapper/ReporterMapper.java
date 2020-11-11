44b1c0ae20ba8a54796d54c93e8d83ec074e00b1
kimdongkui <ddrsbram@hanmail.net>
2020-11-11 19:14:08 +0900

    [TPS] 기자관리 조회 매퍼로 변경처리

--- moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/reporter/mapper/ReporterMapper.java
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/reporter/mapper/ReporterMapper.java
@@ -1,4 +1,4 @@
-package jmnet.moka.core.tps.mvc.reporter.Mapper;
+package jmnet.moka.core.tps.mvc.reporter.mapper;
 
 import jmnet.moka.common.data.mybatis.support.BaseMapper;
 import jmnet.moka.core.tps.mvc.reporter.dto.ReporterSearchDTO;

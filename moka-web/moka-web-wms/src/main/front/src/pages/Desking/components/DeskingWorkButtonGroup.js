c8cae3b6785cea4aeab882656fd92d76fb4adb7d
장미라 <ohtah78@gmail.com>
2020-11-18 15:56:27 +0900

    [react] desking work 등록 api연결

--- moka-web/moka-web-wms/src/main/front/src/pages/Desking/components/DeskingWorkButtonGroup.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Desking/components/DeskingWorkButtonGroup.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 import { MokaIcon, MokaOverlayTooltipButton } from '@components';
+import toast from '@utils/toastUtil';
+
+import { postPreComponentWork } from '@store/desking';
 
 /**
  * 데스킹 워크 버튼 그룹 컴포넌트
  */
 const DeskingWorkButtonGroup = (props) => {
     const { component, agGridIndex } = props;
+    const dispatch = useDispatch();
 
     const title = `ID: CP${component.componentSeq} ${component.componentName}`;
+
+    /**
+     * 임시저장
+     */
+    const handlePreComponentWork = () => {
+        const option = {
+            componentWorkSeq: component.seq,
+            callback: ({ header }) => {
+                if (!header.success) {
+                    toast.warn(header.message);
+                }
+            },
+        };
+        dispatch(postPreComponentWork(option));
+    };
 
     const iconName = [
         { title: '관련기사 정보', iconName: 'fal-minus-circle' },
@@ -21,6 +41,7 @@ const DeskingWorkButtonGroup = (props) => {
         { title: '히스토리', iconName: 'fal-minus-circle' },
         { title: '기사 이동', iconName: 'fal-minus-circle' },
         { title: '더미기사 등록', iconName: 'fal-minus-circle' },
+        { title: '임시저장', iconName: 'fal-minus-circle', onClick: handlePreComponentWork },
         { title: '전송', iconName: 'fal-minus-circle' },
         { title: '삭제', iconName: 'fal-minus-circle' },
     ];
@@ -34,7 +55,7 @@ const DeskingWorkButtonGroup = (props) => {
                 <Col className="p-0 d-flex align-items-center justify-content-between" xs={6}>
                     {iconName.map((icon, idx) => (
                         <MokaOverlayTooltipButton key={idx} tooltipText={icon.title} variant="white" className="p-0">
-                            <MokaIcon iconName={icon.iconName} />
+                            <MokaIcon iconName={icon.iconName} onClick={icon.onClick} />
                         </MokaOverlayTooltipButton>
                     ))}
                 </Col>

772e1ada15acbc9d8fcb44a14ca86b89fb36d39d
ince <csinpgs@gmail.com>
2020-11-18 13:12:00 +0900

    [tps] 로그인 이력 목록 조회 기능 추가
    Signed-off-by: ince <csinpgs@gmail.com>

--- moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/member/repository/LoginLogRepository.java
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/member/repository/LoginLogRepository.java
@@ -2,9 +2,13 @@ package jmnet.moka.core.tps.mvc.member.repository;
 
 import java.util.List;
 import jmnet.moka.core.tps.mvc.member.entity.LoginLog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface LoginLogRepository extends JpaRepository<LoginLog, Long>, JpaSpecificationExecutor<LoginLog> {
     public List<LoginLog> findAllByMemId(String memId);
+
+    public Page<LoginLog> findAllByMemIdOrderBySeqNoDesc(String memId, Pageable pageable);
 }

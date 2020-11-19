63dd41f65bc43890ec4c03fb51c0bcc4fd42b229
ince <csinpgs@gmail.com>
2020-11-19 19:06:15 +0900

    [wms] 사용자 메뉴 권한 수정 api 추가 및 암호화 기능 테스트 클래스 추가
    Signed-off-by: ince <csinpgs@gmail.com>

---
+++ moka-web/moka-web-wms/src/test/java/jmnet/moka/web/wms/util/CryptTest.java
@@ -0,0 +1,45 @@
+package jmnet.moka.web.wms.util;
+
+import jmnet.moka.core.common.encrypt.MokaCrypt;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * <pre>
+ * KISA SEED CBC 암호화 처리 테스트
+ * Project : moka
+ * Package : jmnet.moka.web.wms.util
+ * ClassName : CryptTest
+ * Created : 2020-11-19 ince
+ * </pre>
+ *
+ * @author ince
+ * @since 2020-11-19 09:02
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Slf4j
+public class CryptTest {
+
+    @Autowired
+    private MokaCrypt mokaCrypt;
+
+    @Test
+    public void cryptTest()
+            throws Exception {
+        // 암호화
+        String encode = mokaCrypt.encrypt("test");
+        log.debug(encode);
+        // 복호화
+        log.debug(mokaCrypt.decrypt(encode));
+    }
+
+
+
+}

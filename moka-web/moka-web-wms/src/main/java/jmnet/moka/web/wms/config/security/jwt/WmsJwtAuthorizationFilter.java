1fb1f6e7aabe329a4c4f09cbf8b2e745c71b5fdb
ince <csinpgs@gmail.com>
2020-11-20 14:03:51 +0900

    [wms] 권한이 없는 메뉴 접근시 403 에러 처리
    Signed-off-by: ince <csinpgs@gmail.com>

--- moka-web/moka-web-wms/src/main/java/jmnet/moka/web/wms/config/security/jwt/WmsJwtAuthorizationFilter.java
+++ moka-web/moka-web-wms/src/main/java/jmnet/moka/web/wms/config/security/jwt/WmsJwtAuthorizationFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import jmnet.moka.common.utils.McpString;
 import jmnet.moka.core.common.mvc.MessageByLocale;
 import jmnet.moka.core.common.util.ResourceMapper;
 import jmnet.moka.core.tps.common.TpsConstants;
@@ -37,7 +38,7 @@ public class WmsJwtAuthorizationFilter extends BasicAuthenticationFilter {
     @Autowired
     private MessageByLocale messageByLocale;
 
-    private static ObjectMapper MAPPER = ResourceMapper.getDefaultObjectMapper();
+    private static final ObjectMapper MAPPER = ResourceMapper.getDefaultObjectMapper();
 
     static {
         MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
@@ -57,24 +58,29 @@ public class WmsJwtAuthorizationFilter extends BasicAuthenticationFilter {
         // Read the Authorization header, where the JWT Token should be
         String header = request.getHeader(WmsJwtHelper.HEADER_STRING);
 
+        // 메뉴 ID
+        String menuId = request.getHeader(TpsConstants.HEADER_MENU_ID);
+
         // 인증된 세션이 있으면 다음 필터로 넘김, 중복 로그인 확인
         Authentication auth = SecurityContextHolder
                 .getContext()
                 .getAuthentication();
-        if (auth != null && auth instanceof UsernamePasswordAuthenticationToken) {
+        if (auth instanceof UsernamePasswordAuthenticationToken) {
             // Authorization 헤더가 있는 경우만 expire/중복로그인 여부 체크
             if (header != null && expiredOrDuplicated(request, response, auth, request
                     .getSession(true)
                     .getId())) {
                 return;
             } else {
-                chain.doFilter(request, response);
+                if (menuAuthCheck(request, response, auth, menuId)) {
+                    chain.doFilter(request, response);
+                }
                 return;
             }
         }
 
         // If header does not contain BEARER or is null delegate to Spring impl and exit
-        if (header == null || header.startsWith(WmsJwtHelper.TOKEN_PREFIX) == false) {
+        if (header == null || !header.startsWith(WmsJwtHelper.TOKEN_PREFIX)) {
             // rest of the spring pipeline
             chain.doFilter(request, response);
             return;
@@ -83,10 +89,12 @@ public class WmsJwtAuthorizationFilter extends BasicAuthenticationFilter {
         // If header is present, try grab user principal from database and perform authorization
         Authentication jwtAuth = WmsJwtHelper.getUsernamePasswordAuthentication(request, authService);
 
+        assert jwtAuth != null;
         UserDTO details = (UserDTO) jwtAuth.getDetails();
         if (expiredOrDuplicated(request, response, jwtAuth, details.getSessionId())) {
             return;
         }
+
 
         SecurityContextHolder
                 .getContext()
@@ -97,6 +105,28 @@ public class WmsJwtAuthorizationFilter extends BasicAuthenticationFilter {
 
         // Continue filter execution
         chain.doFilter(request, response);
+    }
+
+    /**
+     * 메뉴 권한 체크
+     *
+     * @param request  HttpServletRequest
+     * @param response HttpServletResponse
+     * @param auth     Authentication
+     * @param menuId   메뉴 ID
+     * @return 메뉴 권한 여부
+     * @throws IOException 에러 처리
+     */
+    private boolean menuAuthCheck(HttpServletRequest request, HttpServletResponse response, Authentication auth, String menuId)
+            throws IOException {
+        if (McpString.isNotEmpty(menuId) && !"null".equals(menuId)) {
+            if (!authService.hasMenuAuth(auth.getName(), menuId)) {
+                String message = messageByLocale.get("wms.member.error.forbidden", request);
+                ResponseUtil.error(response, TpsConstants.HEADER_FORBIDDEN, message);
+                return false;
+            }
+        }
+        return true;
     }
 
     private boolean expiredOrDuplicated(HttpServletRequest request, HttpServletResponse response, Authentication auth, String sessionId)

b7495f407af7895d845cbc5f10014699cd669aa9
kimdongkui <ddrsbram@hanmail.net>
2020-11-23 17:07:01 +0900

    [tps] 헬퍼콘피그레이션 원복

--- moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/config/HelperConfiguration.java
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/config/HelperConfiguration.java
@@ -69,18 +69,4 @@ public class HelperConfiguration {
         return new EditFormHelper(xmlMapper);
     }
 
-    /**
-     * RestTemplate 빈
-     *
-     * @return 빈
-     */
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder
-                .requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
-                .setConnectTimeout(Duration.ofMillis(5000)) // connection-timeout
-                .setReadTimeout(Duration.ofMillis(5000)) // read-timeout
-                .additionalMessageConverters(new StringHttpMessageConverter(Charset.forName("UTF-8")))
-                .build();
-    }
 }

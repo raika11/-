fccea89e7f8919d7f91038bbff1025eae229fae9
kimdongkui <ddrsbram@hanmail.net>
2020-11-23 18:26:53 +0900

    [tps] 수정반영

--- moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/bright/controller/BrightController.java
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/bright/controller/BrightController.java
@@ -30,21 +30,21 @@ public class BrightController {
 //        clientId : 03cf20ff-f4c6-4668-87c8-ccaadb3680b4
 //        clientSecret : J7VeL4iUGc4xkVTLi7LZXj0fX0eX4CBvuYyPa3fHsIukwDIOG5kOHB0nYsVeHl0kD8_2n7UQK4dYihv_y-DJdw
         // 프로퍼티에서 가져오는것으로 저장[tps-auto]
-        String url = "https://oauth.brightcove.com/v4/access_token?grant_type=client_credentials";
-        String params = "client_id=" + data.getClientId() + "&client_secret=" + data.getClientSecret();
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .POST(HttpRequest.BodyPublishers.ofString(params))
-                .uri(URI.create(url))
-                .setHeader("Content-Type", "application/x-www-form-urlencoded")
-                .build();
-
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        // print status code
-        System.out.println(response.statusCode());
-        // print response body
-        System.out.println(response.body());
-        return new ResponseEntity<>(response.body(), HttpStatus.OK);
+//        String url = "https://oauth.brightcove.com/v4/access_token?grant_type=client_credentials";
+//        String params = "client_id=" + data.getClientId() + "&client_secret=" + data.getClientSecret();
+//
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .POST(HttpRequest.BodyPublishers.ofString(params))
+//                .uri(URI.create(url))
+//                .setHeader("Content-Type", "application/x-www-form-urlencoded")
+//                .build();
+//
+//        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+//        // print status code
+//        System.out.println(response.statusCode());
+//        // print response body
+//        System.out.println(response.body());
+        return new ResponseEntity<>("", HttpStatus.OK);
     }
 }
 

05797fe796d2c856f49fb9d080e07bb70d5199c6
kimdongkui <ddrsbram@hanmail.net>
2020-11-26 11:06:05 +0900

    [tps] 네이버벌크 신규추가

---
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/naverbulk/dto/NaverBulkSearchDTO.java
@@ -0,0 +1,97 @@
+/**
+ * msp-tps DirectLinkSearchDTO.java 2020. 11. 16. 오전 11:35:05 ssc
+ */
+package jmnet.moka.core.tps.mvc.naverbulk.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jmnet.moka.common.data.support.SearchDTO;
+import jmnet.moka.core.common.MokaConstants;
+import jmnet.moka.core.tps.common.TpsConstants;
+import jmnet.moka.core.tps.common.dto.DTODateTimeFormat;
+import lombok.*;
+import org.apache.ibatis.type.Alias;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Pattern;
+import java.util.Date;
+
+/**
+ * 네이버벌크문구 검색 DTO
+ * 2020. 11. 16. ssc 최초생성
+ * 
+ * @since 2020. 11. 16. 오전 11:35:05
+ * @author ssc
+ */
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@EqualsAndHashCode(callSuper = true)
+@Alias("NaverBulkSearchDTO")
+public class NaverBulkSearchDTO extends SearchDTO {
+
+    private static final long serialVersionUID = 1972229889422176779L;
+
+    /**
+     * int   10,0    NO  클릭기사일련번호
+     */
+    Long clickartSeq;
+
+    /**
+     * char 1   NO  클릭기사구분 - H(아티클핫클릭) N(네이버벌크)
+     */
+    @Pattern(regexp = "[H|N]{1}$", message = "{tps.naver-bulk.error.pattern.clickartDiv}")
+    String clickartDiv;
+
+    /**
+     * varchar  2   NO  출처 - 썬데이[60] 중앙일보[3]
+     */
+    String sourceCode;
+
+    /**
+     * char 1   ('N')   NO  서비스여부
+     */
+    @Pattern(regexp = "[Y|N]{1}$", message = "{tps.common.error.pattern.useTotal}")
+    String usedYn;
+
+    /**
+     * varchar  10  YES 상태 - SAVE(임시) / PUBLISH(전송)
+     */
+    String status;
+
+    /**
+     * 총갯수 사용여부
+     */
+    @Pattern(regexp = "[Y|N]{1}$", message = "{tps.common.error.pattern.useTotal}")
+    private String useTotal;
+
+    /**
+     * 시작일시
+     */
+    @DTODateTimeFormat
+    private Date startDt;
+
+    /**
+     * 종료일시
+     */
+    @DTODateTimeFormat
+    private Date endDt;
+
+    /**
+     * 총갯수
+     */
+    private Long total;
+
+    /**
+     * 검색결과 성공여부
+     */
+    private Integer returnValue;
+
+    // 검색 조건의 기본값을 설정
+    public NaverBulkSearchDTO() {
+        super("clickartSeq,desc");
+        useTotal = MokaConstants.YES;
+        returnValue = TpsConstants.PROCEDURE_SUCCESS;
+    }
+}
---
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/naverbulk/service/NaverBulkService.java
@@ -0,0 +1,76 @@
+/**
+ * msp-tps DomainService.java 2020. 1. 8. 오후 2:06:54 ssc
+ */
+package jmnet.moka.core.tps.mvc.naverbulk.service;
+
+import jmnet.moka.core.tps.mvc.naverbulk.dto.NaverBulkListDTO;
+import jmnet.moka.core.tps.mvc.naverbulk.dto.NaverBulkSearchDTO;
+import jmnet.moka.core.tps.mvc.naverbulk.entity.Article;
+import jmnet.moka.core.tps.mvc.naverbulk.entity.ArticleList;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 네이버벌크문구 서비스 2020. 1. 8. ssc 최초생성
+ * 메소드 생성 규칙
+ * 목록 조회 : find{Target}List
+ * ID로 상세 조회 : find{Target}ById
+ * 여러 속성으로 상세 조회 : find{Target}
+ * 수정 : update{Target}
+ *
+ * @author ssc
+ * @since 2020. 1. 8. 오후 2:06:54
+ */
+public interface NaverBulkService {
+
+    /**
+     * 네이버벌크문구목록 조회
+     *
+     * @param search 검색조건
+     * @return 네이버벌크문구 목록조회
+     */
+    Page<Article> findAllNaverBulkList(NaverBulkSearchDTO search);
+
+    /**
+     * 네이버벌크문구목록 목록조회
+     *
+     * @param clickartSeq 클릭기사일련번호
+     * @return 네이버벌크문구 목록조회
+     */
+    List<ArticleList> findAllByClickartSeq(Long clickartSeq);
+
+
+    /**
+     * 네이버벌크문구정보 조회
+     *
+     * @param clickartSeq 클릭기사일련번호
+     * @return 네이버벌크문구 단건조회
+     */
+    Optional<Article> findById(Long clickartSeq);
+
+    /**
+     * 네이버벌크문구정보 조회
+     *
+     * @param article 클릭기사일련번호
+     * @return 네이버벌크문구 단건조회
+     */
+    void updateArticle(Article article);
+
+    /**
+     * 중복여부
+     *
+     * @param clickartSeq 일련번호
+     * @return 존재 여부
+     */
+    boolean isDuplicatedId(Long clickartSeq);
+
+    /**
+     * 벌크문구목록저장
+     *
+     * @param naverBulkListDTO 벌크문구목록
+     * @return 존재 여부
+     */
+    Article insertNaverBulk(List<NaverBulkListDTO> naverBulkListDTO, String clickartDiv, String sourceCode, String status);
+}

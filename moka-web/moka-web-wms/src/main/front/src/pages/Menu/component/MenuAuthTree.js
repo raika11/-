68e6cf9137b468c9a1afc61e8aa0ec9c6af54d10
김태형 <xorwk1@gmail.com>
2020-11-27 16:18:17 +0900

    [react] rc-tree를 이용한 메뉴 권한 Component 최초 커밋

---
+++ moka-web/moka-web-wms/src/main/front/src/pages/Menu/component/MenuAuthTree.js
@@ -0,0 +1,162 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import clsx from 'clsx';
+import { MokaRCTree } from '@components/MokaTree';
+import commonUtil from '@utils/commonUtil';
+
+/**
+ * 메뉴 권한 관리를 위한 RC-Tree Cutom Tree
+ * @param menuAuthInfo 메뉴 권한 정보(TreeData)
+ * @param onChange 변경 이벤트
+ * @returns 메뉴 권한 관리를 위한 RC-Tree Cutom Tree
+ * @constructor
+ */
+const MenuAuthTree = ({ menuAuthInfo, onChange }) => {
+    const [customTreeInfos, setCustomTreeInfos] = useState([]);
+
+    /**
+     * 메뉴 USED CheckBox 변경 이벤트
+     * @param checkKeys RC-Tree에서 전달받은 Check된 Tree Key 리스트
+     * @param info RC-Tree에서 전달받은 메뉴 정보
+     */
+    const handleClickUsedChange = (checkKeys, info) => {
+        const halfCheckedKeys = info.halfCheckedKeys;
+        const list = menuAuthInfo.list;
+        const key = info.node.key;
+
+        if (!info.checked) {
+            let edited = menuAuthInfo.edited.filter((id) => id !== key);
+            const nodeInfo = commonUtil.findNode(list, key);
+
+            if (nodeInfo.children) {
+                const childrenKeys = commonUtil.findChildNodeKeys(list, nodeInfo);
+                edited = edited.filter((id) => !childrenKeys.includes(id));
+            }
+
+            const parentNodeKeys = commonUtil.findParentNodeKeys(list, nodeInfo);
+            for (const parentNodeKey of parentNodeKeys) {
+                const parentNode = commonUtil.findNode(list, parentNodeKey);
+                const childNodeKeys = commonUtil.findChildNodeKeys(list, parentNode);
+
+                if (checkKeys.filter((id) => childNodeKeys.includes(id)).length === 0 || edited.filter((id) => childNodeKeys.includes(id)).length === 0) {
+                    edited = edited.filter((id) => id !== parentNodeKey);
+                }
+            }
+            onChange({ name: 'edited', value: edited });
+        }
+        onChange({ name: 'used', value: checkKeys });
+        onChange({ name: 'halfCheckedKeys', value: halfCheckedKeys });
+    };
+
+    /**
+     * 메뉴 EDIT CheckBox 변경 이벤트
+     * @param info 메뉴 정보
+     */
+    const handleClickEditChange = useCallback(
+        (info) => {
+            const { key } = info;
+            let edited = menuAuthInfo.edited;
+            const used = menuAuthInfo.used;
+            const halfCheckKeys = menuAuthInfo.halfCheckedKeys;
+            const list = menuAuthInfo.list;
+
+            if (used.includes(key) || halfCheckKeys.includes(key)) {
+                const childKeys = commonUtil.findChildNodeKeys(list, info);
+                const parentKeys = commonUtil.findParentNodeKeys(list, info);
+
+                if (!edited.includes(key)) {
+                    const hasUsedEditingKeys = [...childKeys, ...parentKeys, key].filter((id) => used.includes(id) || halfCheckKeys.includes(id));
+                    edited = [...edited, ...hasUsedEditingKeys];
+                } else {
+                    [key, ...childKeys].forEach((removeId) => {
+                        edited = edited.filter((id) => id !== removeId);
+                    });
+
+                    for (const parentKey of parentKeys) {
+                        const nodeInfo = commonUtil.findNode(list, parentKey);
+                        const nodeChildKeys = commonUtil.findChildNodeKeys(list, nodeInfo);
+                        if (edited.filter((id) => nodeChildKeys.includes(id)).length === 0) {
+                            edited = edited.filter((id) => id !== parentKey);
+                        }
+                    }
+                }
+                onChange({ name: 'edited', value: edited });
+                //dispatch(changeGroupMenuAuthInfo({ name: 'edited', value: edited }));
+            }
+        },
+        [menuAuthInfo, onChange],
+    );
+
+    /**
+     * Edit CheckBox가 포함 된 Tree Cutom Label을 만든다
+     * @param info 메뉴 정보
+     * @param isUsed 사용 여부
+     * @param isEdited 수정 여부
+     * @returns Custom Label
+     */
+    const makeCustomLabel = useCallback(
+        (info, isUsed, isEdited) => {
+            const { title } = info;
+            return (
+                <div className="cus-label">
+                    <span>{title}</span>
+                    <span
+                        className={clsx('rc-tree-checkbox', isEdited && 'rc-tree-checkbox-checked', !isUsed && 'rc-tree-checkbox-disabled')}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            handleClickEditChange(info);
+                        }}
+                    />
+                </div>
+            );
+        },
+        [handleClickEditChange],
+    );
+
+    /**
+     * Custom Label로 구성된 Tree Data를 만든다.
+     * @param orgTreeData Tree Data
+     * @returns Custom Label로 구성된 Tree Data
+     */
+    const makeCustomLabelTreeData = useCallback(
+        (orgTreeData) => {
+            const treeData = [];
+            for (const item of orgTreeData) {
+                const key = item.key;
+                const isEdited = menuAuthInfo.edited.includes(key);
+                let isUsed = menuAuthInfo.used.includes(key) || menuAuthInfo.halfCheckedKeys.includes(key);
+                const title = makeCustomLabel(item, isUsed, isEdited);
+
+                let children = null;
+                if (item.children) {
+                    children = makeCustomLabelTreeData(item.children);
+                }
+
+                treeData.push({
+                    ...item,
+                    title: title,
+                    children,
+                    /*switcherIcon: item.children && <MokaIcon iconName="fal-minus-circle" />,
+                icon: <MokaIcon iconName="fal-minus-circle" />,*/
+                });
+            }
+
+            return treeData;
+        },
+        [makeCustomLabel, menuAuthInfo],
+    );
+
+    useEffect(() => {
+        if (menuAuthInfo && menuAuthInfo.list) {
+            const tree = makeCustomLabelTreeData(menuAuthInfo.list);
+            setCustomTreeInfos(tree);
+        }
+    }, [makeCustomLabelTreeData, menuAuthInfo]);
+
+    return (
+        <>
+            <MokaRCTree treeData={customTreeInfos} checkedKeys={customTreeInfos.length > 0 && menuAuthInfo.used} onCheck={handleClickUsedChange} />
+        </>
+    );
+};
+
+export default MenuAuthTree;
--- moka-web/moka-web-wms/src/main/front/src/utils/commonUtil.js
+++ moka-web/moka-web-wms/src/main/front/src/utils/commonUtil.js
@@ -47,7 +47,151 @@ const isEmpty = (str) => {
     return (typeof str == 'string' && !str.trim()) || typeof str == 'undefined' || str === null;
 };
 
+/**
+ * TreeInfo List Key가 일치하는 TreeNode를 찾는다.
+ * @param list TreeInfo List
+ * @param key 검색할 Key
+ * @returns TreeInfo List Key가 일치하는 TreeNode
+ */
+function findNode(list, key) {
+    let result = null;
+    let idx = 0;
+    let childrenList = [];
+    while (idx < list.length && list[idx].key !== key) {
+        if (list[idx].children) {
+            childrenList = [...childrenList, ...list[idx].children];
+        }
+        idx++;
+    }
+
+    if (list.length > 0 && idx === list.length) {
+        result = findNode(childrenList, key);
+    } else {
+        result = list[idx];
+    }
+
+    return result;
+}
+
+/**
+ * 매개변수로 전달받은 TreeInfo를 이용해 TreeNode의 자식 Key를 찾는다.
+ * @param list TreeInfo List
+ * @param info TreeInfo
+ * @returns TreeNode 자식 Key List
+ */
+function findChildNodeKeys(list, info) {
+    let childKeys = [];
+    if (info.children) {
+        childKeys = info.children.map((child) => child.key);
+        for (const childKey of childKeys) {
+            const childInfo = findNode(list, childKey);
+            if (childInfo.children) {
+                childKeys = [...childKeys, ...findChildNodeKeys(list, childInfo)];
+            }
+        }
+    }
+
+    return childKeys;
+}
+
+/**
+ * 매개변수로 전달받은 TreeInfo를 이용해 TreeNode의 부모 Key를 찾는다.
+ * @param list TreeInfo List
+ * @param info TreeInfo
+ * @returns TreeNode 부모 Key List
+ */
+function findParentNodeKeys(list, info) {
+    let parentKeys = [];
+    let parentKey = info.parentKey;
+    let parentNode = findNode(list, parentKey);
+
+    while (parentKey && parentKey.parentKey !== '00' && parentNode !== undefined) {
+        parentKeys = [...parentKeys, parentKey];
+        parentKey = parentNode.parentKey;
+        parentNode = findNode(list, parentKey);
+    }
+
+    return parentKeys;
+}
+
+/**
+ * 서버에서 전달 받은 Data를 이용하여 TreeList로 변환한다.
+ * @param serverData 서버에서 전달받은 데이터
+ * @returns TreeList
+ */
+const toTreeList = (serverData) => {
+    return serverData.map((data) => {
+        let treeInfos = { key: data.menuId, title: data.menuDisplayNm, selectable: false };
+        const parentKey = data.parentMenuId;
+        if (parentKey) {
+            treeInfos = { ...treeInfos, parentKey };
+        }
+
+        if (data.children) {
+            const childrenList = toTreeList(data.children);
+            treeInfos.children = childrenList;
+        }
+
+        return treeInfos;
+    });
+};
+
+/**
+ * 서버에서 전달받은 Data를 이용하여 체크된 Key에 대한 목록으로 변환한다..
+ * @param treeInfos Tree 정보
+ * @param serverData 서버에서 전달받은 Data
+ * @param type 찾을 타입(editYn, usedYn)
+ * @returns 체크된 Key에 대한 목록
+ */
+const toCheckedData = (treeInfos, serverData, type) => {
+    let yList = [];
+    let halfCheckedKeys = [];
+    serverData.forEach((data) => {
+        let list = [];
+        const menuId = data.menuId;
+        const isYn = data[type] === 'Y';
+
+        let childrenLength = 0;
+        if (data.children) {
+            const node = findNode(treeInfos, menuId);
+            const checkData = toCheckedData(treeInfos, data.children, type);
+            yList = [...yList, ...checkData['yList']];
+            halfCheckedKeys = [...halfCheckedKeys, ...checkData['halfCheckedKeys']];
+            childrenLength = findChildNodeKeys(treeInfos, node).length;
+        }
+
+        if (isYn) {
+            if (list.length === childrenLength) {
+                yList.push(menuId);
+            } else {
+                halfCheckedKeys.push(menuId);
+            }
+        }
+    });
+    return { yList, halfCheckedKeys };
+};
+
+/**
+ * 서버에서 전달받은 Data를 이용하여 RC-Tree에 필요한 Data로 만든다.
+ * @param serverData 서버에서 전달받은 Data
+ * @returns RC-Tree Data
+ */
+function makeRCTreeData(serverData) {
+    const list = toTreeList(serverData);
+    const useKeys = toCheckedData(list, serverData, 'usedYn');
+    const used = useKeys['yList'];
+    const halfCheckedKeys = useKeys['halfCheckedKeys'];
+    const editedKeys = toCheckedData(list, serverData, 'editYn');
+    const edited = [...editedKeys['yList'], ...editedKeys['halfCheckedKeys']];
+
+    return { list, used, edited, halfCheckedKeys };
+}
+
 export default {
     fileDownload,
     isEmpty,
+    findNode,
+    findChildNodeKeys,
+    findParentNodeKeys,
+    makeRCTreeData,
 };

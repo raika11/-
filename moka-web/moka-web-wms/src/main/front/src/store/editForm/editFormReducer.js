9c7dfe9ca9f1b7589e02193ace0c42127e821fc8
ince <csinpgs@gmail.com>
2020-11-30 19:43:12 +0900

    [react]

--- moka-web/moka-web-wms/src/main/front/src/store/editForm/editFormReducer.js
+++ moka-web/moka-web-wms/src/main/front/src/store/editForm/editFormReducer.js
@@ -189,6 +189,7 @@ export default handleActions(
          */
         [act.FORM_XML_INPORT_MODAL]: (state, { payload }) => {
             return produce(state, (draft) => {
+                console.log(payload.importForm);
                 if (payload.importForm) {
                     draft.importForm = payload.importForm;
                 }

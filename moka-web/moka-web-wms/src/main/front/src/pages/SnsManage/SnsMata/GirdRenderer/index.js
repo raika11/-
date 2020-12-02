82f903187f0231b9837d715f54c1aa910712b515
박성민 <psmever@gmail.com>
2020-12-02 11:39:02 +0900

    [react] FB & TW 관리 퍼블리싱 수정.

--- moka-web/moka-web-wms/src/main/front/src/pages/SnsManage/SnsMata/GirdRenderer/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ListTitleRenderer } from './ListTitleRenderer';
-export { default as ImageRenderer } from './ImageRenderer';
-export { default as ButtonStatusRenderer } from './ButtonStatusRenderer';
-export { default as SendStatusRenderer } from './SendStatusRenderer';

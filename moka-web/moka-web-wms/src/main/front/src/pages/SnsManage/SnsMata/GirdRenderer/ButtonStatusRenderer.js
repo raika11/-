11711beb611238ddfd635d8b81a646cf0622e417
정준영 <fdjy0101@gmail.com>
2020-12-02 11:42:43 +0900

    [react] 소스 merge

--- moka-web/moka-web-wms/src/main/front/src/pages/SnsManage/SnsMata/GirdRenderer/ButtonStatusRenderer.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/SnsManage/SnsMata/GirdRenderer/ButtonStatusRenderer.js
@@ -2,23 +2,20 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 
 const ButtonStatusRenderer = ({ value }) => {
-    console.log(value);
     const handleClickSaveButton = () => {};
     return (
         <>
             <div className="d-flex py-2">
-                <div className="d-flex py-2">
-                    <div className="justify-content-between mr-3 p-1">
-                        {value === 'Y' ? (
-                            <Button variant="positive" className="mr-0" onClick={handleClickSaveButton}>
-                                재전송
-                            </Button>
-                        ) : (
-                            <Button variant="negative" className="mr-0" onClick={handleClickSaveButton}>
-                                전송
-                            </Button>
-                        )}
-                    </div>
+                <div className="d-flex py-2 text-center">
+                    {value === 'Y' ? (
+                        <Button variant="positive" className="mr-0" onClick={handleClickSaveButton}>
+                            재전송
+                        </Button>
+                    ) : (
+                        <Button variant="negative" className="mr-0" onClick={handleClickSaveButton}>
+                            전송
+                        </Button>
+                    )}
                 </div>
             </div>
         </>

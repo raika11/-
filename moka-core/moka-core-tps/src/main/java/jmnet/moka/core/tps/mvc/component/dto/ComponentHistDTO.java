2c16c827c99150a599e5ea42af2d9eb21e47928c
장미라 <ohtah78@gmail.com>
2020-12-03 15:17:28 +0900

    [tps] 컴포넌트에 DATA TYPE 유효성검사 수정

--- moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/component/dto/ComponentHistDTO.java
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/component/dto/ComponentHistDTO.java
@@ -76,7 +76,7 @@ public class ComponentHistDTO implements Serializable {
     /**
      * 데이터유형:NONE, DESK, AUTO
      */
-    @Pattern(regexp = "[(NONE)|(DESK)|(AUTO)]{4}$", message = "{tps.component.error.pattern.dataType}")
+    @Pattern(regexp = "[(NONE)|(DESK)|(AUTO)|(FORM)]{4}$", message = "{tps.component.error.pattern.dataType}")
     @Builder.Default
     private String dataType = TpsConstants.DATATYPE_NONE;
 

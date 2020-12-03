5798296700fa5bf608fe61c4bd77b3702b9b3667
ince <csinpgs@gmail.com>
2020-12-03 10:26:14 +0900

    [wms] 테스트 로그인 api가 로그인이 잘 되지 않는 현상을 수정해 본다.
    Signed-off-by: ince <csinpgs@gmail.com>

--- moka-web/moka-web-wms/src/main/java/jmnet/moka/web/wms/mvc/test/TestLoginController.java
+++ moka-web/moka-web-wms/src/main/java/jmnet/moka/web/wms/mvc/test/TestLoginController.java
@@ -5,12 +5,16 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import javax.servlet.http.HttpServletRequest;
-import jmnet.moka.web.wms.config.security.jwt.WmsJwtAuthenticationToken;
+import jmnet.moka.core.common.util.HttpHelper;
+import jmnet.moka.core.tps.mvc.auth.dto.UserDTO;
+import jmnet.moka.core.tps.mvc.auth.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,10 +32,23 @@ public class TestLoginController {
 
     private static ObjectMapper MAPPER = new ObjectMapper();
 
+    private UserDetailsService userService;    //UserService
+
+    private AuthService authService;    //UserService
+
     @Autowired
-    private AuthenticationProvider authenticationProvider;    //AuthenticationProvider
+    private SessionRegistry sessionRegistry;    //UserService
 
 
+    /**
+     * 생성자 UserService를 Autowired 하지 않고 Arguments로 받는다.
+     *
+     * @param authService userService
+     */
+    public TestLoginController(AuthService authService, UserDetailsService userService) {
+        this.authService = authService;
+        this.userService = userService;
+    }
 
     /**
      * @param request HttpServletRequest
@@ -40,13 +57,25 @@ public class TestLoginController {
      */
     @ApiOperation(value = "테스트용 로그인", tags = {"*** TEST LOGIN ***"})
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "userId", value = "사용자 ID", required = true, dataType = "String", paramType = "query", defaultValue = "ssc01"),
-            @ApiImplicitParam(name = "userPassword", value = "사용자 ID", required = true, dataType = "String", paramType = "query", format = "password", defaultValue = "sscMoka#2020")})
+            @ApiImplicitParam(name = "userId", value = "사용자 ID", required = true, dataType = "String", paramType = "query", defaultValue = "ssc01")})
     @PostMapping("/api/user/test-login")
-    public ResponseEntity<?> postLogin(HttpServletRequest request, String userId, String userPassword) {
+    public ResponseEntity<?> postLogin(HttpServletRequest request, String userId) {
 
-        Authentication authentication = authenticationProvider.authenticate(new WmsJwtAuthenticationToken(userId, userPassword));
+        UserDTO userDetails = (UserDTO) userService.loadUserByUsername(userId);
+        String userIp = HttpHelper.getRemoteAddr();
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userId, null, userDetails.getAuthorities());
+        authenticationToken.setDetails(userDetails);
 
-        return new ResponseEntity<>(authentication, HttpStatus.OK);
+
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(authenticationToken);
+
+        sessionRegistry.registerNewSession(request
+                .getSession()
+                .getId(), authenticationToken.getPrincipal());
+
+        return new ResponseEntity<>(userDetails, HttpStatus.OK);
+
     }
 }

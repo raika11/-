3d677b80e5e8a4801242ae6fbd3e82d12ffa0e77
ince <csinpgs@gmail.com>
2020-12-08 09:37:35 +0900

    [tps] SNS 메타에 fb 기사 제목, 이미지 url, 예약발송일시 추가
    Signed-off-by: ince <csinpgs@gmail.com>

--- moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/sns/vo/ArticleSnsShareItemVO.java
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/sns/vo/ArticleSnsShareItemVO.java
@@ -21,37 +21,103 @@ import org.apache.ibatis.type.Alias;
 @Builder
 public class ArticleSnsShareItemVO implements Serializable {
 
+    /**
+     * 기사ID
+     */
     private Long totalId;
 
+    /**
+     * 입력일시
+     */
     @DTODateTimeFormat
     private Date insDt;
 
+    /**
+     * iud
+     */
     private String iud;
 
+    /**
+     * 전송일시
+     */
     @DTODateTimeFormat
     private Date sendDt;
 
+    /**
+     * 호출 메세지
+     */
     private String callMsg;
 
+    /**
+     * 페이스북 상태 ID
+     */
     private String fbStatusId;
 
+    /**
+     * 페이스북 기사 ID
+     */
     private String fbArticleId;
 
+    /**
+     * 소스 코드
+     */
     private String sourceCode;
 
+    /**
+     * 서비스 일시
+     */
     @DTODateTimeFormat
     private Date serviceDt;
 
+    /**
+     * 기사 제목
+     */
     private String artTitle;
 
+    /**
+     * 기사 썸네일
+     */
+    private String artThumb;
+
+    /**
+     * 전송 SNS 유형
+     */
     private String sendSnsType;
 
+    /**
+     * 페이스북 전송 SNS Article ID
+     */
     private String fbSendSnsArtId;
 
+    /**
+     * 페이스북 전송 SNS 상태
+     */
     private String fbSendSnsArtSts;
 
+    /**
+     * 트위터 전송 SNS Article ID
+     */
     private String twSendSnsArtId;
 
+    /**
+     * 트위터 전송 SNS 상태
+     */
     private String twSendSnsArtSts;
+
+    /**
+     * 페이스북 제목
+     */
+    private String fbMetaTitle;
+
+    /**
+     * 페이스북 이미지
+     */
+    private String fbMetaImage;
+
+    /**
+     * 페이스북 예약 전송 일시
+     */
+    @DTODateTimeFormat
+    private Date fbReserveDt;
 
 }

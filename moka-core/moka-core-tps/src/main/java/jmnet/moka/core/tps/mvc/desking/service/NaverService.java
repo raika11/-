01f5d9a506ecd1197a51114a6f0f8f9b2a609249
장미라 <ohtah78@gmail.com>
2020-12-10 18:51:43 +0900

    [tps] 뉴스스탠드 api 추가중

---
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/desking/service/NaverService.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017 Joongang Ilbo, Inc. All rights reserved.
+ */
+
+package jmnet.moka.core.tps.mvc.desking.service;
+
+/**
+ * Description: 설명
+ *
+ * @author ssc
+ * @since 2020-12-10
+ */
+public interface NaverService {
+    void send(Long componentSeq);
+}

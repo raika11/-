1f751278fd6741552da92a81efba49d45a495c17
ince <csinpgs@gmail.com>
2020-12-15 09:56:06 +0900

    [wms] 사용자 사용 등록 관련 api를 MemberRestController과 분리하고 /api/member-join url은 인증 처리 하지 않도록 수정
    Signed-off-by: ince <csinpgs@gmail.com>

--- moka-web/moka-web-wms/src/main/java/jmnet/moka/web/wms/mvc/member/controller/MemberRestController.java
+++ moka-web/moka-web-wms/src/main/java/jmnet/moka/web/wms/mvc/member/controller/MemberRestController.java
@@ -21,7 +21,6 @@ import jmnet.moka.common.utils.dto.ResultListDTO;
 import jmnet.moka.core.common.exception.MokaException;
 import jmnet.moka.core.common.logger.LoggerCodes.ActionType;
 import jmnet.moka.core.common.mvc.MessageByLocale;
-import jmnet.moka.core.tps.common.code.MemberRequestCode;
 import jmnet.moka.core.tps.common.code.MemberStatusCode;
 import jmnet.moka.core.tps.common.code.MenuAuthTypeCode;
 import jmnet.moka.core.tps.common.controller.AbstractCommonController;
@@ -32,7 +31,6 @@ import jmnet.moka.core.tps.mvc.group.entity.GroupMember;
 import jmnet.moka.core.tps.mvc.member.dto.LoginLogDTO;
 import jmnet.moka.core.tps.mvc.member.dto.MemberDTO;
 import jmnet.moka.core.tps.mvc.member.dto.MemberGroupSaveDTO;
-import jmnet.moka.core.tps.mvc.member.dto.MemberRequestDTO;
 import jmnet.moka.core.tps.mvc.member.dto.MemberSaveDTO;
 import jmnet.moka.core.tps.mvc.member.dto.MemberSearchDTO;
 import jmnet.moka.core.tps.mvc.member.dto.MemberUpdateDTO;
@@ -44,9 +42,6 @@ import jmnet.moka.core.tps.mvc.menu.dto.MenuNodeDTO;
 import jmnet.moka.core.tps.mvc.menu.dto.MenuSearchDTO;
 import jmnet.moka.core.tps.mvc.menu.entity.MenuAuth;
 import jmnet.moka.core.tps.mvc.menu.service.MenuService;
-import jmnet.moka.web.wms.config.security.exception.PasswordNotMatchedException;
-import jmnet.moka.web.wms.config.security.exception.SmsAuthNumberBadCredentialsException;
-import jmnet.moka.web.wms.config.security.exception.SmsAuthNumberExpiredException;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
@@ -214,7 +209,33 @@ public class MemberRestController extends AbstractCommonController {
         }
 
         try {
-            MemberInfo returnValue = processUserSave(member, memberDTO.getMemberGroups(), MemberStatusCode.Y);
+
+            member.setPassword(passwordEncoder.encode(member.getPassword()));
+            List<MemberGroupSaveDTO> groupSaveDTOs = memberDTO.getMemberGroups();
+            // SMS 서버에 문자 발송 요청
+            String smsAuth = "4885";
+            Date smsExp = McpDate.minutePlus(McpDate.now(), 3);
+            member.setSmsAuth(smsAuth);
+            member.setSmsExp(smsExp);
+            member.setStatus(MemberStatusCode.Y);
+
+            // insert
+            MemberInfo returnValue = memberService.insertMember(member);
+            if (returnValue != null && groupSaveDTOs != null) {
+                groupSaveDTOs.forEach(group -> {
+                    GroupMember groupMember = GroupMember
+                            .builder()
+                            .groupCd(group.getGroupCd())
+                            .memberId(member.getMemberId())
+                            .usedYn(group.getUsedYn())
+                            .build();
+                    memberService.insertGroupMember(groupMember);
+                });
+
+            }
+
+            // 액션 로그에 성공 로그 출력
+            tpsLogger.success(ActionType.INSERT);
 
             MemberDTO dto = modelMapper.map(returnValue, MemberDTO.class);
             ResultDTO<MemberDTO> resultDto = new ResultDTO<>(dto, msg("tps.member.success.insert"));
@@ -228,40 +249,6 @@ public class MemberRestController extends AbstractCommonController {
             throw new Exception(msg("tps.member.error.save"), e);
         }
     }
-
-    private MemberInfo processUserSave(MemberInfo member, List<MemberGroupSaveDTO> groupSaveDTOs, MemberStatusCode statusCode) {
-
-        member.setPassword(passwordEncoder.encode(member.getPassword()));
-
-        // SMS 서버에 문자 발송 요청
-        String smsAuth = "4885";
-        Date smsExp = McpDate.minutePlus(McpDate.now(), 3);
-        member.setSmsAuth(smsAuth);
-        member.setSmsExp(smsExp);
-        member.setStatus(statusCode);
-
-        // insert
-        MemberInfo returnValue = memberService.insertMember(member);
-        if (returnValue != null && groupSaveDTOs != null) {
-            groupSaveDTOs.forEach(group -> {
-                GroupMember groupMember = GroupMember
-                        .builder()
-                        .groupCd(group.getGroupCd())
-                        .memberId(member.getMemberId())
-                        .usedYn(group.getUsedYn())
-                        .build();
-                memberService.insertGroupMember(groupMember);
-            });
-
-        }
-
-        // 액션 로그에 성공 로그 출력
-        tpsLogger.success(ActionType.INSERT);
-
-        return returnValue;
-    }
-
-
 
     /**
      * 수정
@@ -452,186 +439,7 @@ public class MemberRestController extends AbstractCommonController {
         }
     }
 
-    /**
-     * Member 신규 등록 요청
-     *
-     * @param memberDTO 등록할 Member정보
-     * @return 등록된 Member정보
-     * @throws InvalidDataException 데이타 유효성 오류
-     * @throws Exception            예외처리
-     */
-    @ApiOperation(value = "Member 신규 등록 요청")
-    @PostMapping("/register-request")
-    public ResponseEntity<?> postRegisterRequest(@Valid MemberRequestDTO memberDTO)
-            throws InvalidDataException, Exception {
 
-        // MemberDTO -> Member 변환
-        MemberInfo member = modelMapper.map(memberDTO, MemberInfo.class);
-        if (McpString.isNotEmpty(member.getMemberId())) { // 자동 발번이 아닌 경우 중복 체크
-            if (memberService.isDuplicatedId(member.getMemberId())) {
-                throw new InvalidDataException(msg("tps.member.error.duplicated.memberId"));
-            }
-        }
-
-        try {
-
-            String passwordSameMessage = msg("tps.member.error.same.password");
-
-            if (memberDTO
-                    .getPassword()
-                    .equals(memberDTO.getConfirmPassword())) {
-                throw new MokaException(passwordSameMessage);
-            }
-
-            MemberInfo returnValue = processUserSave(member, memberDTO.getMemberGroups(), MemberRequestCode.NEW_REQUEST.getNextStatus());
-
-            MemberDTO dto = modelMapper.map(returnValue, MemberDTO.class);
-            ResultDTO<MemberDTO> resultDto = new ResultDTO<>(dto, msg(MemberRequestCode.NEW_REQUEST.getMessageKey()));
-
-            return new ResponseEntity<>(resultDto, HttpStatus.OK);
-
-        } catch (Exception e) {
-            log.error("[FAIL TO INSERT MEMBER]", e);
-            // 액션 로그에 오류 내용 출력
-            tpsLogger.error(ActionType.INSERT, e);
-            throw new Exception(msg("tps.member.error.save"), e);
-        }
-    }
-
-    /**
-     * Member SMS 인증문자 요청
-     *
-     * @param memberId         MemberID
-     * @param memberRequestDTO 잠금 해제 요청 정보
-     * @return 사용자 정보
-     * @throws NoDataException 데이터없음 예외처리
-     */
-    @ApiOperation(value = "Member SMS 인증문자 요청")
-    @GetMapping("/{memberId}/sms-request")
-    public ResponseEntity<?> putSmsRequest(
-            @PathVariable("memberId") @Size(min = 1, max = 30, message = "{tps.member.error.pattern.memberId}") String memberId,
-            @Valid MemberRequestDTO memberRequestDTO)
-            throws Exception {
-
-        String noDataMsg = msg("tps.common.error.no-data");
-
-        MemberInfo member = memberService
-                .findMemberById(memberId)
-                .orElseThrow(() -> new NoDataException(noDataMsg));
-
-        // 비밀번호와 비밀번호 확인 비교
-        boolean same = memberRequestDTO
-                .getPassword()
-                .equals(memberRequestDTO.getConfirmPassword());
-        if (!same) {
-            throw new PasswordNotMatchedException(msg("wms.login.error.PasswordNotMatchedException"));
-        }
-
-        same = passwordEncoder.matches(memberRequestDTO.getPassword(), member.getPassword());
-        if (!same) {
-            throw new UserPrincipalNotFoundException(memberId);
-        }
-
-        // SMS 서버에 문자 발송 요청
-        String smsAuth = "4885";
-        Date smsExp = McpDate.minutePlus(McpDate.now(), 3);
-
-        member.setSmsAuth(smsAuth);
-        member.setSmsExp(smsExp);
-        memberService.updateMember(member);
-
-        MemberDTO memberDTO = modelMapper.map(member, MemberDTO.class);
-
-        String smsInfoMsg = msg("wms.login.info.sms-sand");
-
-        // 결과리턴
-        ResultDTO<MemberDTO> resultDto = new ResultDTO<>(memberDTO, smsInfoMsg);
-        return new ResponseEntity<>(resultDto, HttpStatus.OK);
-    }
-
-    /**
-     * Member 잠금 해제 및 신규 등록 요청
-     *
-     * @param memberId         MemberID
-     * @param memberRequestDTO 잠김 해제 요청 정보
-     * @return 사용자 정보
-     * @throws NoDataException 데이터없음 예외처리
-     */
-    @ApiOperation(value = "Member 잠금 해제 및 신규 등록 요청")
-    @GetMapping({"/{memberId}/approval-request", "/{memberId}/unlock-request"})
-    public ResponseEntity<?> putStatusChange(
-            @PathVariable("memberId") @Size(min = 1, max = 30, message = "{tps.member.error.pattern.memberId}") String memberId,
-            @Valid MemberRequestDTO memberRequestDTO)
-            throws Exception {
-
-        // 결과리턴
-        String noDataMsg = msg("tps.common.error.no-data");
-        String successMsg;
-
-        MemberInfo member = memberService
-                .findMemberById(memberId)
-                .orElseThrow(() -> new NoDataException(noDataMsg));
-
-        throwPasswordCheck(memberRequestDTO.getPassword(), memberRequestDTO.getConfirmPassword(), member);
-
-        if (memberRequestDTO.getRequestType() == MemberRequestCode.UNLOCK_SMS
-                || memberRequestDTO.getRequestType() == MemberRequestCode.NEW_SMS) {// SMS 인증문자로 잠김해제 요청 또는 신규 등록 신청
-            if (McpString.isEmpty(member.getSmsAuth())) {
-                throw new InvalidDataException(msg("wms.login.error.notempty.smsAuth"));
-            }
-            if (!member
-                    .getSmsAuth()
-                    .equals(memberRequestDTO.getSmsAuth())) {
-                throw new SmsAuthNumberBadCredentialsException(msg("wms.login.error.sms-unmatched"));
-            }
-
-            if (McpDate.term(member.getSmsExp()) < 0) {
-                throw new SmsAuthNumberExpiredException(msg("wms.login.error.unlock-sms-expired"));
-            }
-            // 잠금 해제
-            member.setPasswordModDt(McpDate.now());
-            member.setErrCnt(0);
-        }
-
-        member.setStatus(memberRequestDTO
-                .getRequestType()
-                .getNextStatus());
-        memberService.updateMember(member);
-        successMsg = msg(memberRequestDTO
-                .getRequestType()
-                .getMessageKey());
-
-        MemberDTO memberDTO = modelMapper.map(member, MemberDTO.class);
-
-        // 결과리턴
-        ResultDTO<MemberDTO> resultDto = new ResultDTO<>(memberDTO, successMsg);
-
-        return new ResponseEntity<>(resultDto, HttpStatus.OK);
-    }
-
-    /**
-     * 비밀번호 확인
-     *
-     * @param password        비밀번호 입력값
-     * @param confirmPassword 비밀번호 확인 입력값
-     * @param orgMember       기존 사용자 정보
-     * @throws PasswordNotMatchedException    비밀번호와 비밀번호 확인 불일치
-     * @throws UserPrincipalNotFoundException 비밀번호와 기존 비밀번호 불일치
-     */
-    private void throwPasswordCheck(String password, String confirmPassword, MemberInfo orgMember)
-            throws PasswordNotMatchedException, UserPrincipalNotFoundException {
-
-        // 비밀번호와 비밀번호 확인 비교
-        boolean same = password.equals(confirmPassword);
-        if (!same) {
-            throw new PasswordNotMatchedException(msg("wms.login.error.PasswordNotMatchedException"));
-        }
-
-        same = passwordEncoder.matches(password, orgMember.getPassword());
-        if (!same) {
-            throw new UserPrincipalNotFoundException(orgMember.getMemberId());
-        }
-    }
 
     /**
      * Member 메뉴 트리 조회

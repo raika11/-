57bc5f86f9ebd014e5929f7dcc6bc0cfa9b9e2b5
장미라 <ohtah78@gmail.com>
2020-12-29 14:19:12 +0900

    [tps] 수신 기사 미리보기 추가

--- moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/rcvArticle/controller/RcvArticleRestController.java
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/rcvArticle/controller/RcvArticleRestController.java
@@ -23,13 +23,11 @@ import jmnet.moka.core.tps.mvc.rcvArticle.service.RcvArticleService;
 import jmnet.moka.core.tps.mvc.rcvArticle.vo.RcvArticleBasicVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -120,9 +118,9 @@ public class RcvArticleRestController extends AbstractCommonController {
      * @throws Exception
      */
     @ApiOperation(value = "등록기사로 등록")
-    @PostMapping(value = "/articles/{rid}", headers = {"content-type=application/json"}, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping("/articles/{rid}")
     public ResponseEntity<?> postArticle(@ApiParam("수신기사아이디(필수)") @PathVariable("rid") Long rid,
-            @ApiParam("수정할 정보(기자목록,분류코드목록,태그목록)") @RequestBody RcvArticleBasicUpdateDTO updateDto)
+            @ApiParam("수정할 정보(기자목록,분류코드목록,태그목록)") @Valid RcvArticleBasicUpdateDTO updateDto)
             throws Exception {
 
         // 수신기사 상세조회

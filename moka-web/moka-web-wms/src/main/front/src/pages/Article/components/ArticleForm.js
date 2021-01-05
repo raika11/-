fe651e1ece9635217d461c344100930771b2d4b0
전현지 <emproject525@gmail.com>
2021-01-05 16:06:27 +0900

    [react] 등록기사, 수신기사 기자 변경

--- moka-web/moka-web-wms/src/main/front/src/pages/Article/components/ArticleForm.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Article/components/ArticleForm.js
@@ -3,10 +3,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 import copy from 'copy-to-clipboard';
 import { initialState, getArticle, GET_ARTICLE, SAVE_ARTICLE, saveArticle } from '@store/article';
 import { CodeListModal, CodeAutocomplete } from '@pages/commons';
-import { MokaInputLabel, MokaInput, MokaCard } from '@components';
+import { MokaInputLabel, MokaInput, MokaCard, MokaIcon } from '@components';
 import { MokaEditorCore } from '@components/MokaEditor';
 import toast from '@utils/toastUtil';
 import { popupPreview } from '@utils/commonUtil';
@@ -19,6 +21,7 @@ const ArticleForm = ({ totalId, reporterList, inRcv, onCancle }) => {
     const article = useSelector((store) => store.article.article);
     const loading = useSelector((store) => store.loading[GET_ARTICLE] || store.loading[SAVE_ARTICLE]);
     const [tagStr, setTagStr] = useState(''); // 태그리스트
+    const [repStr, setRepStr] = useState(''); // 기자리스트
     const [content, setContent] = useState(''); // 본문
     const [codeModalShow, setCodeModalShow] = useState(false); // 분류코드 모달
     const [historyModalShow, setHistoryModalShow] = useState(false); // 히스토리 모달
@@ -33,6 +36,8 @@ const ArticleForm = ({ totalId, reporterList, inRcv, onCancle }) => {
         if (name === 'tagList') {
             // 계속 파싱할 수 없어서
             setTagStr(value);
+        } else if (name === 'repStr') {
+            setRepStr(value);
         } else {
             setTemp({
                 ...temp,
@@ -42,7 +47,7 @@ const ArticleForm = ({ totalId, reporterList, inRcv, onCancle }) => {
     };
 
     /**
-     * 태그 input blur시에 onChange 실행
+     * 태그 input blur
      */
     const handleTagBlur = () => {
         setTemp({
@@ -52,11 +57,35 @@ const ArticleForm = ({ totalId, reporterList, inRcv, onCancle }) => {
     };
 
     /**
-     * 본문 blur 시
+     * 본문 blur
      * @param {string} value editor value
      */
     const handleContentBlur = (value) => {
         setContent(value);
+    };
+
+    /**
+     * 기자 입력 input blur
+     * @param {object} e event
+     */
+    const handleBlurRep = (e) => {
+        const { value } = e.target;
+        let tmpArr = temp.reporterList;
+
+        const result = value.split('.').map((repName) => {
+            let idx = tmpArr.findIndex((t) => t.repName === repName);
+            if (idx > -1) {
+                return tmpArr.splice(idx, 1)[0];
+            } else {
+                return {
+                    repName,
+                };
+            }
+        });
+        setTemp({
+            ...temp,
+            reporterList: result,
+        });
     };
 
     /**
@@ -76,20 +105,21 @@ const ArticleForm = ({ totalId, reporterList, inRcv, onCancle }) => {
 
     /**
      * 기자 자동완성 변경
-     * @param {array} value value
+     * @param {object} value value
      */
     const handleReporter = (value) => {
-        let result = [];
         if (value) {
-            result = value.map((reporter) => ({
-                ...reporter,
-            }));
+            if (temp.reporterList.findIndex((s) => s.value === value.value) > -1) {
+                toast.warning('이미 선택한 기자입니다');
+            } else {
+                const narr = [...temp.reporterList, value];
+                setTemp({
+                    ...temp,
+                    reporterList: narr,
+                });
+                setRepStr(narr.map((r) => r.repName).join(','));
+            }
         }
-
-        setTemp({
-            ...temp,
-            reporterList: result,
-        });
     };
 
     /**
@@ -163,7 +193,7 @@ const ArticleForm = ({ totalId, reporterList, inRcv, onCancle }) => {
             // 기자리스트
             reporterList: article.reporterList.map((reporter) => ({
                 ...reporter,
-                label: reporter.reporterName,
+                label: reporter.repName,
                 value: reporter.repSeq,
             })),
             // 링크
@@ -171,6 +201,8 @@ const ArticleForm = ({ totalId, reporterList, inRcv, onCancle }) => {
         });
         // 태그 리스트
         setTagStr(article.tagList.join(','));
+        // 기자리스트(text)
+        setRepStr(article.reporterList.map((r) => r.repName).join(','));
         // 본문
         setContent(article.artContent?.artContent || '');
     }, [article, temp.totalId]);
@@ -235,15 +267,22 @@ const ArticleForm = ({ totalId, reporterList, inRcv, onCancle }) => {
                     </Col>
                 </Form.Row>
                 <Form.Row className="mb-2">
-                    <Col className="p-0" xs={12}>
-                        <MokaInputLabel
-                            label="기자"
+                    <Col className="p-0" xs={6}>
+                        <MokaInputLabel label="기자" name="repStr" value={repStr} onChange={handleChangeValue} inputProps={{ onBlur: handleBlurRep }} />
+                    </Col>
+                    <Col className="p-0 d-flex justify-content-center align-items-center" xs={2}>
+                        <p className="mb-0 ft-12">구분자는 마침표(.)</p>
+                    </Col>
+                    <Col className="p-0 d-flex align-items-center" xs={4}>
+                        <MokaInput
                             className="mb-0"
                             as="autocomplete"
-                            value={temp.reporterList}
                             onChange={handleReporter}
-                            inputProps={{ options: reporterList, isMulti: true, className: 'ft-12', maxMenuHeight: 140, placeholder: '선택한 기자가 없습니다' }}
+                            inputProps={{ options: reporterList, className: 'ft-12', maxMenuHeight: 150, placeholder: '기자 선택' }}
                         />
+                        <OverlayTrigger overlay={<Tooltip id="reporter-info">중앙일보 기자인 경우 기자를 선택해주세요</Tooltip>}>
+                            <MokaIcon iconName="fas-info-circle" className="ml-1 color-info" />
+                        </OverlayTrigger>
                     </Col>
                 </Form.Row>
                 <Form.Row className="mb-2">
--- moka-web/moka-web-wms/src/main/front/src/pages/RcvArticle/components/RcvArticleForm.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/RcvArticle/components/RcvArticleForm.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import toast from '@utils/toastUtil';
 import { CodeListModal, CodeAutocomplete } from '@pages/commons';
-import { MokaInputLabel, MokaCard } from '@components';
+import { MokaInputLabel, MokaCard, MokaInput, MokaIcon } from '@components';
 
 const RcvArticleForm = ({ reporterList, article, onChange, loading, onCancle, onPreview, onRegister }) => {
     const [selectedMasterCode, setSelectedMasterCode] = useState([]); // 마스터코드 리스트
     const [selectedReporter, setSelectedReporter] = useState([]); // 기자 리스트
+    const [repStr, setRepStr] = useState('');
     const [tagStr, setTagStr] = useState(''); // 태그리스트
     const [pressDt, setPressDt] = useState(''); // 발행일자
     const [codeModalShow, setCodeModalShow] = useState(false); // 분류코드 모달
@@ -21,6 +25,9 @@ const RcvArticleForm = ({ reporterList, article, onChange, loading, onCancle, on
         if (name === 'tagList') {
             // 계속 파싱할 수 없어서
             setTagStr(value);
+        } else if (name === 'repStr') {
+            // 기자명은 별도 처리
+            setRepStr(value);
         } else {
             onChange({
                 key: name,
@@ -30,12 +37,36 @@ const RcvArticleForm = ({ reporterList, article, onChange, loading, onCancle, on
     };
 
     /**
-     * 태그 input blur시에 onChange 실행
+     * 태그 input blur
      */
-    const handleBlur = () => {
+    const handleBlurTag = () => {
         onChange({
             key: 'tagList',
             value: tagStr.split(','),
+        });
+    };
+
+    /**
+     * 기자 입력 input blur
+     * @param {object} e event
+     */
+    const handleBlurRep = (e) => {
+        const { value } = e.target;
+        let tmpArr = selectedReporter;
+
+        const result = value.split('.').map((reporterName) => {
+            let idx = tmpArr.findIndex((t) => t.reporterName === reporterName);
+            if (idx > -1) {
+                return tmpArr.splice(idx, 1)[0];
+            } else {
+                return {
+                    reporterName,
+                };
+            }
+        });
+        onChange({
+            key: 'reporterList',
+            value: result,
         });
     };
 
@@ -58,26 +89,29 @@ const RcvArticleForm = ({ reporterList, article, onChange, loading, onCancle, on
 
     /**
      * 기자 자동완성 변경
-     * @param {array} value value
+     * @param {object} value value
      */
     const handleReporter = (value) => {
-        let result = [];
         if (value) {
-            result = value.map((reporter) => ({
-                rid: reporter.rid,
-                reporterBlog: reporter.reporterBlog,
-                reporterEmail: reporter.reporterEmail || reporter.repEmail1,
-                reporterEtc: reporter.reporterEtc,
-                reporterName: reporter.reporterName || reporter.repName,
-                label: reporter.reporterName || reporter.repName,
-                value: reporter.reporterName ? valueCreator(reporter.reporterName, reporter.reporterEmail) : valueCreator(reporter.repName, reporter.repEmail1),
-            }));
-        }
+            const result = {
+                // rid: value.rid,
+                reporterBlog: value.reporterBlog,
+                reporterEmail: value.reporterEmail || value.repEmail1,
+                reporterEtc: value.reporterEtc,
+                reporterName: value.reporterName || value.repName,
+                label: value.reporterName || value.repName,
+                value: value.reporterName ? valueCreator(value.reporterName, value.reporterEmail) : valueCreator(value.repName, value.repEmail1),
+            };
 
-        onChange({
-            key: 'reporterList',
-            value: result,
-        });
+            if (selectedReporter.findIndex((s) => s.value === result.value) > -1) {
+                toast.warning('이미 선택한 기자입니다');
+            } else {
+                onChange({
+                    key: 'reporterList',
+                    value: [...selectedReporter, result],
+                });
+            }
+        }
     };
 
     /**
@@ -91,19 +125,28 @@ const RcvArticleForm = ({ reporterList, article, onChange, loading, onCancle, on
     const handleMobilePreview = () => onPreview('M');
 
     useEffect(() => {
-        setSelectedMasterCode(article.categoryList || []);
         if (article.pressDt) {
             setPressDt(article.pressDt.slice(0, 10));
         }
+    }, [article.pressDt]);
+
+    useEffect(() => {
+        setSelectedMasterCode(article.categoryList || []);
+    }, [article.categoryList]);
+
+    useEffect(() => {
         setTagStr((article.tagList || []).join(','));
-        setSelectedReporter(
-            (article.reporterList || []).map((reporter) => ({
-                ...reporter,
-                label: reporter.reporterName,
-                value: valueCreator(reporter.reporterName, reporter.reporterEmail),
-            })),
-        );
-    }, [article]);
+    }, [article.tagList]);
+
+    useEffect(() => {
+        const sr = (article.reporterList || []).map((reporter) => ({
+            ...reporter,
+            label: reporter.reporterName,
+            value: valueCreator(reporter.reporterName, reporter.reporterEmail),
+        }));
+        setSelectedReporter(sr);
+        setRepStr(sr.map((s) => s.reporterName).join('.'));
+    }, [article.reporterList]);
 
     return (
         <MokaCard
@@ -136,11 +179,10 @@ const RcvArticleForm = ({ reporterList, article, onChange, loading, onCancle, on
                             label="분류표"
                             className="mb-0"
                             searchIcon={false}
-                            labelType="masterCode"
+                            labelType="contentKorname"
                             value={selectedMasterCode.join(',')}
                             onChange={handleMasterCode}
                             maxMenuHeight={150}
-                            labelType="contentKorname"
                             selectable={['content']}
                             isMulti
                         />
@@ -162,20 +204,27 @@ const RcvArticleForm = ({ reporterList, article, onChange, loading, onCancle, on
                     </Col>
                 </Form.Row>
                 <Form.Row className="mb-2">
-                    <Col className="p-0" xs={12}>
-                        <MokaInputLabel
-                            label="기자"
+                    <Col className="p-0" xs={6}>
+                        <MokaInputLabel label="기자" name="repStr" value={repStr} onChange={handleChangeValue} inputProps={{ onBlur: handleBlurRep }} />
+                    </Col>
+                    <Col className="p-0 d-flex justify-content-center align-items-center" xs={2}>
+                        <p className="mb-0 ft-12">구분자는 마침표(.)</p>
+                    </Col>
+                    <Col className="p-0 d-flex align-items-center" xs={4}>
+                        <MokaInput
                             className="mb-0"
                             as="autocomplete"
-                            value={selectedReporter}
                             onChange={handleReporter}
-                            inputProps={{ options: reporterList, isMulti: true, className: 'ft-12', maxMenuHeight: 100, placeholder: '선택한 기자가 없습니다' }}
+                            inputProps={{ options: reporterList, className: 'ft-12', maxMenuHeight: 150, placeholder: '기자 선택' }}
                         />
+                        <OverlayTrigger overlay={<Tooltip id="reporter-info">중앙일보 기자인 경우 기자를 선택해주세요</Tooltip>}>
+                            <MokaIcon iconName="fas-info-circle" className="ml-1 color-info" />
+                        </OverlayTrigger>
                     </Col>
                 </Form.Row>
                 <Form.Row className="mb-2">
                     <Col className="p-0" xs={10}>
-                        <MokaInputLabel label="태그" name="tagList" className="mb-0" value={tagStr} onChange={handleChangeValue} inputProps={{ onBlur: handleBlur }} />
+                        <MokaInputLabel label="태그" name="tagList" className="mb-0" value={tagStr} onChange={handleChangeValue} inputProps={{ onBlur: handleBlurTag }} />
                     </Col>
                     <Col className="p-0 pl-2 d-flex align-items-center" xs={2}>
                         <p className="mb-0 ml-2 ft-12">콤마(,) 구분입력</p>

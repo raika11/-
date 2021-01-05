831e391b840852dc32b1f362197f1971291ee530
정준영 <fdjy0101@gmail.com>
2021-01-05 13:13:56 +0900

    [react] 견학 메세지 설정 수정

--- moka-web/moka-web-wms/src/main/front/src/pages/TourMsgSet/TourMsgSet.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/TourMsgSet/TourMsgSet.js
@@ -24,7 +24,7 @@ const MessageSettings = () => {
                 footerClassName="justify-content-center"
             >
                 <Form.Group className="d-flex align-items-start">
-                    <Form.Label className="pt-1 mr-3 ft-12 d-flex flex-column align-items-end" style={{ width: 66 }} htmlFor="none">
+                    <Form.Label className="pt-1 mr-2 d-flex flex-column align-items-end" style={{ width: 66 }} htmlFor="none">
                         <p className="mb-0">견학 신청/안내</p>
                         <p className="mb-0">'신청 방법'</p>
                     </Form.Label>
@@ -39,7 +39,7 @@ const MessageSettings = () => {
                     />
                 </Form.Group>
                 <Form.Group className="d-flex align-items-start">
-                    <Form.Label className="pt-1 mr-3 ft-12 d-flex flex-column align-items-end" style={{ width: 66 }} htmlFor="none">
+                    <Form.Label className="pt-1 mr-2 d-flex flex-column align-items-end" style={{ width: 66 }} htmlFor="none">
                         <p className="mb-0">견학 신청/안내</p>
                         <p className="mb-0">'견학 신청'</p>
                     </Form.Label>
@@ -54,7 +54,7 @@ const MessageSettings = () => {
                     />
                 </Form.Group>
                 <Form.Group className="d-flex align-items-start">
-                    <Form.Label className="pt-1 mr-3 ft-12 d-flex flex-column align-items-end" style={{ width: 66 }} htmlFor="none">
+                    <Form.Label className="pt-1 mr-2 d-flex flex-column align-items-end" style={{ width: 66 }} htmlFor="none">
                         <p className="mb-0">견학 신청/안내</p>
                         <p className="mb-0">'견학 시 유의사항'</p>
                     </Form.Label>
@@ -69,7 +69,7 @@ const MessageSettings = () => {
                     />
                 </Form.Group>
                 <Form.Group className="d-flex align-items-start">
-                    <Form.Label className="pt-1 mr-3 ft-12 d-flex flex-column align-items-end" style={{ width: 66 }} htmlFor="none">
+                    <Form.Label className="pt-1 mr-2 d-flex flex-column align-items-end" style={{ width: 66 }} htmlFor="none">
                         <p className="mb-0">견학 신청/안내</p>
                         <p className="mb-0">'관람 및 주차 안내'</p>
                     </Form.Label>
@@ -84,7 +84,7 @@ const MessageSettings = () => {
                     />
                 </Form.Group>
                 <Form.Group className="mb-0 d-flex align-items-start">
-                    <Form.Label className="pt-1 mr-3 ft-12 d-flex flex-column align-items-end" style={{ width: 66 }} htmlFor="none">
+                    <Form.Label className="pt-1 mr-2 d-flex flex-column align-items-end" style={{ width: 66 }} htmlFor="none">
                         <p className="mb-0">질의응답</p>
                         <p className="mb-0">'자주 하는 질문'</p>
                     </Form.Label>

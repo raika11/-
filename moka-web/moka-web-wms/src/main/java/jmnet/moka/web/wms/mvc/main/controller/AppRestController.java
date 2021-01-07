1545eb30e607be311f185724e1a705db16ab3edc
장미라 <ohtah78@gmail.com>
2021-01-07 19:05:02 +0900

    [tps]워터마크이미지 wms에 추가, 크롭사이즈 APP에 추가

--- moka-web/moka-web-wms/src/main/java/jmnet/moka/web/wms/mvc/main/controller/AppRestController.java
+++ moka-web/moka-web-wms/src/main/java/jmnet/moka/web/wms/mvc/main/controller/AppRestController.java
@@ -69,6 +69,15 @@ public class AppRestController {
     @Value("${photo.archive.url}")
     private String photoArchiveUrl;
 
+    @Value("${crop.width}")
+    private String cropWidth;
+
+    @Value("${crop.height}")
+    private String cropHeight;
+
+    @Value("${watermark.path}")
+    private String watermarkPath;
+
     @Autowired
     private TpsLogger tpsLogger;
 
@@ -100,6 +109,9 @@ public class AppRestController {
                 .add("IR_URL", irUrl)                                                // ir Url
                 .add("OVP_PREVIEW_URL", ovpPreviewUrl)                               // ovp 미리보기 url
                 .add("PHOTO_ARCHIVE_URL", photoArchiveUrl)                           // 포토아카이브 url
+                .add("CROP_WIDTH", cropWidth)                                        // 편집이미지 crop 기본 가로사이즈
+                .add("CROP_HEIGHT", cropHeight)                                      // 편집이미지 crop 기본 세로사이즈
+                .add("WATERMARK_PATH", watermarkPath)                                // 워터마크 이미지 경로
                 .getMap();
 
         result.put("MEMBER_STATUS_CODE", MemberStatusCode.toList());

0d75f03fd3aef79c0276128889d415b1cbb388c8
김태형 <xorwk1@gmail.com>
2021-01-11 15:30:01 +0900

    [react] SEO메타관리 : 서버연동 처리 검색기능 구현 / 제목 셀 클릭시 기사 페이지 팝업 처리

--- moka-web/moka-web-wms/src/main/front/src/pages/SEOMeta/SEOMetaEdit.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/SEOMeta/SEOMetaEdit.js
@@ -3,7 +3,7 @@ import { MokaCard, MokaInputLabel } from '@components';
 import { Form, Col, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
-import { getSeoMeta, getSeoMetaList, initialState, saveSeoMeta } from '@store/seoMeta';
+import { clearSeoMeta, getSeoMeta, getSeoMetaList, initialState, saveSeoMeta, GET_SEO_META } from '@store/seoMeta';
 import produce from 'immer';
 import toast from '@utils/toastUtil';
 
@@ -14,9 +14,10 @@ const SEOMetaEdit = () => {
     const { totalId } = useParams();
 
     const [edit, setEdit] = useState(initialState.seoMeta);
-    const { seoMeta, search } = useSelector((store) => ({
+    const { seoMeta, search, loading } = useSelector((store) => ({
         seoMeta: store.seoMeta.seoMeta,
         search: store.seoMeta.search,
+        loading: store.loading[GET_SEO_META],
     }));
 
     const handleChangeValue = (name, value) => {
@@ -40,6 +41,11 @@ const SEOMetaEdit = () => {
         );
     };
 
+    const handleClickCancel = () => {
+        dispatch(clearSeoMeta());
+        history.push('/seo-meta');
+    };
+
     useEffect(() => {
         setEdit(seoMeta);
     }, [seoMeta]);
@@ -55,9 +61,10 @@ const SEOMetaEdit = () => {
             footerClassName="justify-content-center"
             footerButtons={[
                 { text: '저장', variant: 'positive', onClick: handleClickSave, className: 'mr-2' },
-                { text: '취소', variant: 'negative', onClick: () => console.log('cancel'), className: 'mr-2' },
+                { text: '취소', variant: 'negative', onClick: handleClickCancel, className: 'mr-2' },
             ]}
             footer
+            loading={loading}
         >
             <Form className="pb-2">
                 <Form.Row className="mb-2">
--- moka-web/moka-web-wms/src/main/front/src/pages/SEOMeta/SEOMetaList.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/SEOMeta/SEOMetaList.js
@@ -2,16 +2,28 @@ import React, { useEffect } from 'react';
 import SEOMetaSearch from '@pages/SEOMeta/SEOMetaSearch';
 import SEOMetaAgGrid from '@pages/SEOMeta/SEOMetaAgGrid';
 import { useDispatch, useSelector } from 'react-redux';
-import { getSeoMetaList } from '@store/seoMeta';
+import { changeSeoMetaSearchOptions, getSeoMetaList, initialState, GET_SEO_META_LIST } from '@store/seoMeta';
 
 const SEOMetaList = () => {
     const dispatch = useDispatch();
-    const { list, search, total, totalId } = useSelector((store) => ({
+    const { list, search, total, totalId, loading } = useSelector((store) => ({
         list: store.seoMeta.list,
         search: store.seoMeta.search,
         total: store.seoMeta.total,
-        totalId: store.seoMeta.totalId,
+        totalId: store.seoMeta.seoMeta.totalId,
+        loading: store.loading[GET_SEO_META_LIST],
     }));
+
+    const handleClickSearch = (options) => {
+        dispatch(changeSeoMetaSearchOptions(options));
+    };
+
+    const handleClickReset = (callback) => {
+        if (callback instanceof Function) {
+            callback(initialState.search);
+        }
+        dispatch(changeSeoMetaSearchOptions(initialState.search));
+    };
 
     useEffect(() => {
         dispatch(getSeoMetaList(search));
@@ -19,8 +31,8 @@ const SEOMetaList = () => {
 
     return (
         <>
-            <SEOMetaSearch searchOptions={search} />
-            <SEOMetaAgGrid rows={list} searchOptions={search} total={total} />
+            <SEOMetaSearch searchOptions={search} onSearch={handleClickSearch} onReset={handleClickReset} />
+            <SEOMetaAgGrid rows={list} searchOptions={search} total={total} selected={totalId} loading={loading} />
         </>
     );
 };

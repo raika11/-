202253fbf48291f97eda3a93bebe8d54d389060d
정준영 <fdjy0101@gmail.com>
2021-01-11 18:53:09 +0900

    [react] 시민 마이크 공통 배너 관리 모달 수정

--- moka-web/moka-web-wms/src/main/front/src/pages/Mic/modals/BannerModalAgGridColumns.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Mic/modals/BannerModalAgGridColumns.js
@@ -6,39 +6,36 @@ export default [
         headerName: '번호',
         field: 'seqNo',
         width: 40,
-        cellStyle: { fontSize: '12px' },
+        cellStyle: { fontSize: '12px', display: 'flex', alignItems: 'center' },
     },
     {
         headerName: '등록일시',
         field: 'regDt',
         width: 110,
-        cellStyle: { fontSize: '12px' },
+        cellStyle: { fontSize: '12px', display: 'flex', alignItems: 'center' },
     },
     {
         headerName: '이미지',
         field: 'thumbnail',
         cellRenderer: 'imageRenderer',
+        cellStyle: {},
         flex: 1,
     },
     {
         headerName: '사용',
         field: 'usedYn',
         width: 50,
-        cellStyle: { display: 'flex', alignItems: 'center', justifyContent: 'center' },
+        cellStyle: { alignItems: 'center' },
         cellRendererFramework: (params) => {
-            console.log(params.data);
             return (
                 <MokaInput
                     as="switch"
                     id="switch-usedYn"
                     inputProps={{
                         custom: true,
-                        checked: params.data.usedYn === 'Y',
+                        checked: params.value === 'Y' ? true : false,
                     }}
-                    onChange={(e) => {
-                        console.log(e.target.checked);
-                        // setTemp({ ...temp, usedYn: e.target.checked ? 'Y' : 'N' }
-                    }}
+                    onChange={(e) => e.target.checked}
                 />
             );
         },

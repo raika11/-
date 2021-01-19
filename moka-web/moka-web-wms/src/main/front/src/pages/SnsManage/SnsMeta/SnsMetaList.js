a0054369fbf94a44324f41f8dfba6eeee47b2ac5
김태형 <xorwk1@gmail.com>
2021-01-19 17:01:51 +0900

    [react] SNS 메타: 수정 시 리스트가 갱신 안되는 문제 처리

--- moka-web/moka-web-wms/src/main/front/src/pages/SnsManage/SnsMeta/SnsMetaList.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/SnsManage/SnsMeta/SnsMetaList.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import Search from './SnsMetaSearch';
 import AgGrid from './SnsMetaAgGrid';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeSnsMetaSearchOptions, getSNSMetaList } from '@store/snsManage/snsAction';
+import { changeSnsMetaSearchOptions, getSnsMetaList } from '@store/snsManage/snsAction';
 import { GET_SNS_META_LIST, initialState } from '@store/snsManage';
 
 const SnsMetaList = () => {
@@ -27,7 +27,7 @@ const SnsMetaList = () => {
     };
 
     useEffect(() => {
-        dispatch(getSNSMetaList({ payload: search }));
+        dispatch(getSnsMetaList({ payload: search }));
     }, [dispatch, search]);
 
     return (

6c1c6eb98d782938fd4e28bcbaf846476dcdc6f6
김태형 <xorwk1@gmail.com>
2021-01-20 17:34:38 +0900

    [react] 투표관리: 투표생성 - 일반형 텍스트 컴포넌트 및 일반형 이미지 컴포넌트 처리

---
+++ moka-web/moka-web-wms/src/main/front/src/pages/Survey/Poll/components/PollDetailBasicPhotoAnswerComponent.js
@@ -0,0 +1,34 @@
+import React, { useRef, useState } from 'react';
+import PollPhotoComponent from '@pages/Survey/Poll/components/PollPhotoComponent';
+
+const PollDetailBasicPhotoAnswerComponent = ({ items, hasUrl }) => {
+    return (
+        <div className="justify-content-center text-center">
+            {items.map((item, index) => (
+                /*<MokaInput
+                    as="imageFile"
+                    className="mb-2"
+                    name="chnlImgMobFile"
+                    isInvalid={null}
+                    key={index}
+                    inputProps={{
+                        height: 80,
+                        width: 80,
+                        // width: 600, // width: '100%' number type 에러남.
+                        //img: `${PDS_URL}${item.imgPath}${item.imgName}`,
+                        img: '',
+                        selectAccept: ['image/jpeg'], // 이미지중 업로드 가능한 타입 설정.
+                        onClick: () => {
+                            console.log('hhhhhhhhhh');
+                        },
+                    }}
+                />*/
+                <PollPhotoComponent key={index} width="100px" height="100px" src={item.imgPath && item.imgName && `https://pds.joins.com/${item.imgPath}${item.imgName}`}>
+                    150x150
+                </PollPhotoComponent>
+            ))}
+        </div>
+    );
+};
+
+export default PollDetailBasicPhotoAnswerComponent;

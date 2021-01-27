0b85bab732cc35450e3e69593d5d3ff97aec96f0
ince <csinpgs@gmail.com>
2021-01-27 17:45:58 +0900

    [tps] 검색어 로그의 전체 건수 조회 API 추가
    Signed-off-by: ince <csinpgs@gmail.com>

--- moka-web/moka-web-wms/src/main/front/.eslintrc.js
+++ moka-web/moka-web-wms/src/main/front/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     extends: ['react-app', 'prettier'],
     plugins: ['prettier'],
     rules: {
-        'prettier/prettier': ['error', { endOfLine: 'auto' }],
+        'prettier/prettier': ['warn', { endOfLine: 'auto' }],
         'no-console': ['error', { allow: ['warn', 'error', 'log'] }],
         camelcase: 0,
         indent: 0,

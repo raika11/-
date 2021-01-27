3d787ff505d45daca65e38b2ab4a59c665fb4645
박성원 <saparksung@gmail.com>
2021-01-27 09:15:08 +0900

    [bulk] bulk sender 까지 1차 구현

---
+++ moka-web/moka-web-bulk/src/main/java/jmnet/moka/web/bulk/task/bulkdump/env/sub/BulkDumpEnvCP.java
@@ -0,0 +1,87 @@
+package jmnet.moka.web.bulk.task.bulkdump.env.sub;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import jmnet.moka.common.utils.McpString;
+import jmnet.moka.web.bulk.config.FtpConfig;
+import jmnet.moka.web.bulk.exception.BulkException;
+import jmnet.moka.web.bulk.util.BulkFileUtil;
+import jmnet.moka.web.bulk.util.BulkStringUtil;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <pre>
+ *
+ * Project : moka-web-bulk
+ * Package : jmnet.moka.web.bulk.task.bulkdump.env.Sub
+ * ClassName : BulkDumpEnvCP
+ * Created : 2021-01-21 021 sapark
+ * </pre>
+ *
+ * @author sapark
+ * @since 2021-01-21 021 오후 4:54
+ */
+@Getter
+@Setter
+@XmlRootElement(name = "CP")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class BulkDumpEnvCP implements Serializable {
+    private static final long serialVersionUID = 8904214866750684898L;
+
+    @XmlAttribute(name = "Name")
+    private String name;
+
+    @XmlAttribute(name = "Comment")
+    private String comment;
+
+    @XmlAttribute(name = "Dir")
+    private String dir;
+
+    @XmlAttribute(name = "Content")
+    private String content;
+
+    @XmlAttribute(name = "FileExt")
+    private String fileExt;
+
+    @XmlAttribute(name = "DnVideo")
+    private String dnVideo;
+
+    @XmlAttribute(name = "Type")
+    private String type;
+
+    @XmlAttribute(name = "SendSiteCode")
+    private String sendSiteCode;
+
+    @XmlElement(name = "Format")
+    private String format;
+
+    @XmlElement(name = "FormatDelete")
+    private String formatDelete;
+
+    @XmlElement(name = "EncodeType")
+    private String encodeType;
+
+    @XmlElement(name = "FtpConfig")
+    private FtpConfig ftpConfig;
+
+    public void init()
+            throws BulkException {
+        if (McpString.isNullOrEmpty(this.dir)) {
+            throw new BulkException(BulkStringUtil.format("[{}]-{} Input Path is Blank", this.name, this.comment ) );
+        }
+        if (!BulkFileUtil.createDirectories(this.dir)) {
+            throw new BulkException(BulkStringUtil.format("[{}]-{} Input Path can't create", this.name, this.comment ) );
+        }
+
+        if (!McpString.isNullOrEmpty(format))
+            format = format.replace("{_TAB_}", "\t").replace("{_CRLF_}", "\r\n");
+
+        if (!McpString.isNullOrEmpty(formatDelete))
+            formatDelete = formatDelete.replace("{_TAB_}", "\t").replace("{_CRLF_}", "\r\n");
+    }
+}
---
+++ moka-web/moka-web-bulk/src/main/java/jmnet/moka/web/bulk/task/bulkdump/env/sub/BulkDumpEnvGlobal.java
@@ -0,0 +1,39 @@
+package jmnet.moka.web.bulk.task.bulkdump.env.sub;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import jmnet.moka.web.bulk.task.bulkdump.env.sub.BulkDumpEnvTarget;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <pre>
+ *
+ * Project : moka-springboot-parent
+ * Package : jmnet.moka.web.bulk.task.bulkdump.env.Sub
+ * ClassName : BulkDumpEnvGlobal
+ * Created : 2020-12-28 028 sapark
+ * </pre>
+ *
+ * @author sapark
+ * @since 2020-12-28 028 오후 5:37
+ */
+@Getter
+@Setter
+@XmlRootElement(name = "Global")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class BulkDumpEnvGlobal implements Serializable {
+    private static final long serialVersionUID = -7101528553026147185L;
+
+    @XmlElement(name = "opEmail")
+    private String opEmail;
+
+    @XmlElementWrapper(name = "Targets")
+    @XmlElement(name = "Target")
+    private List<BulkDumpEnvTarget> dumpEnvTargets;
+}

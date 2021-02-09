a5994bc279c67eabe4852d7a801c2bac590763b1
김태형 <xorwk1@gmail.com>
2021-02-09 15:12:23 +0900

    [react] 권한 버튼이 로드 될 때 메뉴ID가 없는 경우에 대하여 예외처리 한다.

--- moka-web/moka-web-wms/src/main/front/src/pages/Auth/AuthButton/AuthButtonContainer.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Auth/AuthButton/AuthButtonContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import commonUtil from '@utils/commonUtil';
 
 const AuthButtonContainer = ({ text, variant, children, ...rest }) => {
     const [hasAuth, setHasAuth] = useState(false);
@@ -10,9 +11,13 @@ const AuthButtonContainer = ({ text, variant, children, ...rest }) => {
     }));
 
     useEffect(() => {
-        const target = menu.menuById[latestMenuId];
-        setHasAuth(target.editYn === 'Y');
-    }, [latestMenuId]);
+        let hasAuth = false;
+        if (!commonUtil.isEmpty(menu) && !Array.isArray(menu) && menu instanceof Object && !commonUtil.isEmpty(latestMenuId)) {
+            const target = menu.menuById[latestMenuId];
+            hasAuth = target.editYn === 'Y';
+        }
+        setHasAuth(hasAuth);
+    }, [menu, latestMenuId]);
 
     return (
         hasAuth && (

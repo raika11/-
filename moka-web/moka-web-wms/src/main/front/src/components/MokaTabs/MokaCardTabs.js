49b657922327bf2cb0a12d504d27bada6efe6793
전현지 <emproject525@gmail.com>
2021-02-09 17:49:34 +0900

    [react] MokaCardTabs 수정

--- moka-web/moka-web-wms/src/main/front/src/components/MokaTabs/MokaCardTabs.js
+++ moka-web/moka-web-wms/src/main/front/src/components/MokaTabs/MokaCardTabs.js
@@ -92,7 +92,7 @@ const MokaCardTabs = (props) => {
 
     return (
         <div className={clsx('tab card-tab flex-fill', className)} style={{ width, height }}>
-            <Tab.Container id={id} defaultActiveKey={0}>
+            <Tab.Container id={id} activeKey={activeKey}>
                 <div className="d-flex px-card">
                     <Nav fill={fill} activeKey={activeKey} variant="tabs" className="flex-row" onSelect={handleSelect}>
                         {tabNavs.map((nav, idx) =>

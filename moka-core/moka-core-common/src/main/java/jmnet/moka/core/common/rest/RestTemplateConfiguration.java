71615bcf6130f667399554cd63410b0984a3b35c
ince <csinpgs@gmail.com>
2021-02-10 11:20:27 +0900

    [common] RestTemplateHelper ssl trustmanager 추가
    Signed-off-by: ince <csinpgs@gmail.com>

--- moka-core/moka-core-common/src/main/java/jmnet/moka/core/common/rest/RestTemplateConfiguration.java
+++ moka-core/moka-core-common/src/main/java/jmnet/moka/core/common/rest/RestTemplateConfiguration.java
@@ -1,11 +1,21 @@
 package jmnet.moka.core.common.rest;
 
 import java.nio.charset.Charset;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
+import org.apache.http.ssl.TrustStrategy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -73,17 +83,42 @@ public class RestTemplateConfiguration {
      */
     @Bean
     @ConditionalOnProperty(name = "moka.rest-template.enable", havingValue = "true")
-    public RestTemplateHelper restTemplate() {
+    public RestTemplateHelper restTemplate()
+            throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
         RestTemplate restTemplate = new RestTemplate();
 
         List<HttpMessageConverter<?>> messageConverters = restTemplate.getMessageConverters();
         messageConverters.add(new StringHttpMessageConverter(Charset.forName("utf-8")));
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+        TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
+                    throws CertificateException {
+            }
+
+            @Override
+            public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
+                    throws CertificateException {
+            }
+
+            @Override
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new java.security.cert.X509Certificate[] {};
+            }
+        }};
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCerts, null);
+        SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
 
         OkHttpClient client = new OkHttpClient.Builder()
                 .readTimeout(connectionReadTimeout, TimeUnit.MILLISECONDS)
                 .connectTimeout(connectionTimeout, TimeUnit.MILLISECONDS)
                 .connectionPool(new ConnectionPool(maxIdleConnections, keepAliveDuration, TimeUnit.MILLISECONDS)) //커넥션풀 적용
+                .sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0])
                 .build();
+
 
         OkHttp3ClientHttpRequestFactory crf = new OkHttp3ClientHttpRequestFactory(client);
         restTemplate.setRequestFactory(crf);

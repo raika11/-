a0ab2fcb55ae72d7e820e4ed18109d6ed3c72442
전현지 <emproject525@gmail.com>
2021-02-10 10:32:14 +0900

    [react] 편집영역 수정

--- moka-web/moka-web-wms/src/main/front/src/pages/Area/components/ContainerLoadBox.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Area/components/ContainerLoadBox.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
 import { MokaOverlayTooltipButton, MokaIcon } from '@components';
 
 const ContainerLoadBox = ({ message, onClick }) => {
     return (
-        <Form.Row className="mb-2">
-            <Col xs={2} className="p-0"></Col>
-            <Col xs={8} className="p-0 pl-2">
-                <p className="mb-0 text-danger" dangerouslySetInnerHTML={{ __html: message.replace('\n', '<br/>') }} />
-            </Col>
-            <Col xs={2} className="p-0 d-flex align-items-center justify-content-start">
-                <MokaOverlayTooltipButton variant="outline-neutral" className="border" tooltipText="컨테이너 리로드" onClick={onClick}>
-                    <MokaIcon iconName="far-redo-alt" />
-                </MokaOverlayTooltipButton>
-            </Col>
+        <Form.Row className="justify-content-end align-items-center mb-2">
+            <p className="mb-0 text-danger" dangerouslySetInnerHTML={{ __html: message.replace('\n', ' ') }} />
+            <MokaOverlayTooltipButton variant="outline-neutral" className="border ml-3" tooltipText="컨테이너 리로드" onClick={onClick}>
+                <MokaIcon iconName="far-redo-alt" />
+            </MokaOverlayTooltipButton>
         </Form.Row>
     );
 };

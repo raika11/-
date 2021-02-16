b863e0ca8f22c2b5d14f5ed09523e27a0ec76b57
장미라 <ohtah78@gmail.com>
2021-02-16 16:29:13 +0900

    [common] merge시 content 수정오류 조치

--- moka-core/moka-core-tms/src/main/java/jmnet/moka/core/tms/merge/MokaPreviewTemplateMerger.java
+++ moka-core/moka-core-tms/src/main/java/jmnet/moka/core/tms/merge/MokaPreviewTemplateMerger.java
@@ -427,9 +427,13 @@ public class MokaPreviewTemplateMerger extends MokaTemplateMerger {
         basicJson.replace("ART_TITLE", title);
         basicJson.replace("ART_SUB_TITLE", subTitle);
         article.put("basic", basicJson);
-        Map<String, Object> contentJson = jsonResult.getDataListFirst("CONTENT");
-        contentJson.replace("ART_CONTENT", content);
-        article.put("content", contentJson);
+        List<Map<String, Object>> contentList = jsonResult.getDataList("CONTENT");
+        if (contentList.size() > 0) {
+            contentList
+                    .get(0)
+                    .replace("ART_CONTENT", content);
+        }
+        article.put("content", contentList);
         article.put("reporter", reporterList);
         article.put("mastercode", categoryList);
         article.put("keyword", tagList);

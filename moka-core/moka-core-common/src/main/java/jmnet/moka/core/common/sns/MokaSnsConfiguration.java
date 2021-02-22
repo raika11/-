52ecf8e3be4c16e1fe518e8b50970f88cae39afb
ince <csinpgs@gmail.com>
2021-02-22 18:05:22 +0900

    [common] sns 활성화 옵션 추가
    Signed-off-by: ince <csinpgs@gmail.com>

--- moka-core/moka-core-common/src/main/java/jmnet/moka/core/common/sns/MokaSnsConfiguration.java
+++ moka-core/moka-core-common/src/main/java/jmnet/moka/core/common/sns/MokaSnsConfiguration.java
@@ -1,16 +1,19 @@
 package jmnet.moka.core.common.sns;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MokaSnsConfiguration {
 
+    @ConditionalOnProperty(name = "moka.sns-publish.enable", havingValue = "true")
     @Bean("facebookApiService")
     public SnsApiService facebookApiService() {
         return new FacebookApiServiceImpl();
     }
 
+    @ConditionalOnProperty(name = "moka.sns-publish.enable", havingValue = "true")
     @Bean("twitterApiService")
     public SnsApiService twitterApiService() {
         return new TwitterApiServiceImpl();

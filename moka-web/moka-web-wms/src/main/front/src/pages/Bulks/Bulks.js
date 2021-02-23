8938688b3aba25f498476cf5f03126baabd8e965
박성민 <psmever@gmail.com>
2021-02-23 15:33:31 +0900

    [react] 벌크 문구 구분 코드 위치 변경.

--- moka-web/moka-web-wms/src/main/front/src/pages/Bulks/Bulks.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Bulks/Bulks.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { clearStore, initializeParams } from '@store/bulks';
 import { clearSpecialCharCode } from '@store/codeMgt';
+import { BULKS_CODE } from '@/constants';
 
 const Bulkh = React.lazy(() => import('./Bulkh'));
 const Bulkn = React.lazy(() => import('./Bulkn'));
@@ -11,60 +12,16 @@ const Bulks = ({ match }) => {
     const dispatch = useDispatch();
     const pathName = useRef(match.path);
 
-    // 중앙일보 : bulk_source = '3'
-    // 중앙썬데이 : bulk_source = '60'
-    // 네이버 벌크 : bulk_div = 'N'
-    // 핫클릭 : bulk_div = 'H'
-
-    // /bulkn-ja
-    // 출처는 3 이고, 벌크 구분 N
-    // /bulkh-ja
-    // 출처는 3이고, 벌크 구분  H
-
-    // /bulkn-su
-    // 출처는 60 이고, 벌크 구분 N
-    // /bulkh-su
-    // 출처는 60이고, 벌크 구분  H
-
     const [bulksParams, setBulkParams] = useState(initbulksParams);
 
     useEffect(() => {
         // 라우터로 기본 공통 구분값 설정.
         const initPageParams = ({ path }) => {
             const pathName = path.split('/').reverse()[0];
-            switch (pathName) {
-                case 'bulkn-ja':
-                    setBulkParams({
-                        bulkPathName: pathName,
-                        bulk_source: '3',
-                        bulk_div: 'N',
-                    });
-                    break;
-                case 'bulkn-su':
-                    setBulkParams({
-                        bulkPathName: pathName,
-                        bulk_source: '61',
-                        bulk_div: 'N',
-                    });
-                    break;
-                case 'bulkh-ja':
-                    setBulkParams({
-                        bulkPathName: pathName,
-                        bulk_source: '3',
-                        bulk_div: 'H',
-                    });
-                    break;
-                case 'bulkh-su':
-                    setBulkParams({
-                        bulkPathName: pathName,
-                        bulk_source: '61',
-                        bulk_div: 'H',
-                    });
-                    break;
-                default:
-                    // 없는 데이터 처리는 어떻게?
-                    console.log('경로에서 페이지 데이터를 가지고 오지 못했습니다.');
-            }
+            setBulkParams({
+                bulkPathName: pathName,
+                ...BULKS_CODE[pathName],
+            });
         };
 
         // 최초 로딩 이거나 path 변경 되었을때.

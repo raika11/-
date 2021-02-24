ceb0d5a2e67c1c7e8bc8e356e0141223a387ac9b
ince <csinpgs@gmail.com>
2021-02-24 14:06:30 +0900

    [comment] 불필요한 모듈 제거
    Signed-off-by: ince <csinpgs@gmail.com>

--- moka-core/moka-core-comment/src/main/java/jmnet/moka/core/comment/mvc/comment/repository/CommentUrlRepository.java
+++
@@ -1,9 +0,0 @@
-package jmnet.moka.core.comment.mvc.comment.repository;
-
-import jmnet.moka.core.comment.mvc.comment.entity.CommentUrl;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface CommentUrlRepository extends JpaRepository<CommentUrl, Integer>, JpaSpecificationExecutor<CommentUrl> {
-
-}

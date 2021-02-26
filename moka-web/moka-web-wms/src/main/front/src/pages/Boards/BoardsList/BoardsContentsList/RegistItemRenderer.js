c7de001f6bea11eb8b950310510ff6ef94a314cd
정준영 <fdjy0101@gmail.com>
2021-02-26 17:50:13 +0900

    [react] 게시판 관리 UI 수정

--- moka-web/moka-web-wms/src/main/front/src/pages/Boards/BoardsList/BoardsContentsList/RegistItemRenderer.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Boards/BoardsList/BoardsContentsList/RegistItemRenderer.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 
 // 등록자 정보
-const RegistItemRenderer = ({ regDt, regName }) => {
+const RegistItemRenderer = ({ regDt, regName, regId }) => {
     return (
         <div className="h-100 d-flex flex-column justify-content-center">
             <Row className="m-0">
-                <Col className="p-0">{regName}</Col>
+                <Col className="p-0">{`${regName}(${regId})`}</Col>
             </Row>
             <Row className="m-0">
                 <Col className="p-0">{regDt}</Col>

5967ff2dc99a686fd9b3c42cc63bf93a4ede74db
전현지 <emproject525@gmail.com>
2021-02-26 09:59:17 +0900

    [react] 아티클핫클릭 기사리스트 => 기사탭으로 변경함 (기사/영상/이슈키워드/기자/칼럼니스트)

--- moka-web/moka-web-wms/src/main/front/src/pages/Desking/components/DeskingArticleTab.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Desking/components/DeskingArticleTab.js
@@ -1,24 +1,19 @@
 import React, { useState, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { MokaCardTabs } from '@components';
-import ArticleList from '@pages/Article/components/Desking/ArticleList';
-import ArticleMediaList from '@pages/Article/components/Desking/ArticleMediaList';
+import { ArticleList, ArticleMediaList } from '@pages/Article/components/Desking';
 import ReporterList from '@pages/Reporter/components/Desking/ReporterList';
 import ColumnistList from '@pages/Columnist/components/Desking/ColumnistList';
 import { deskingDragStop } from '@store/desking';
-import toast from '@utils/toastUtil';
+import { messageBox } from '@utils/toastUtil';
 
 /**
- * 페이지편집 > 기사보기
+ * 페이지편집 > 기사보기 탭
  */
 const DeskingArticleTab = (props) => {
     const { componentList, componentAgGridInstances, show } = props;
     const dispatch = useDispatch();
-    const area = useSelector((store) => store.desking.area);
-    const selectedComponent = useSelector((store) => store.desking.selectedComponent);
-    const isNaverChannel = useSelector((store) => store.desking.isNaverChannel);
-
-    // state
+    const { area, selectedComponent, isNaverChannel } = useSelector(({ desking }) => desking);
     const [tabNavs] = useState(['기사', '영상', '이슈키워드', '기자', '칼럼니스트']); // 컴포넌트 폼여부에 따라 리스트 변경o
     const [navIdx, setNavIdx] = useState(0);
 
@@ -26,11 +21,11 @@ const DeskingArticleTab = (props) => {
      * 기사 드래그 끝났을 때 액션
      * @param {object} source 드래그 row의 본체? ag-grid instance
      * @param {object} target 드래그 stop되는 타겟 ag-grid의 dragStop 이벤트
-     * @param {number} agGridIndex agGridIndex
+     * @param {number} targetIndex agGridInstance 리스트에서 타겟의 순서
      */
     const handleArticleDragStop = useCallback(
-        (source, target, agGridIndex) => {
-            const tgtComponent = componentList[agGridIndex];
+        (source, target, targetIndex) => {
+            const tgtComponent = componentList[targetIndex];
 
             // deskingPart를 찾아야해서 area.areaComp || area.areaComps 에서 찾음
             let areaComp = null;
@@ -47,7 +42,7 @@ const DeskingArticleTab = (props) => {
                     areaComp: areaComp,
                     callback: ({ header }) => {
                         if (!header.success) {
-                            toast.fail(header.message);
+                            messageBox.alert(header.message);
                         }
                     },
                 }),

bafbef0016afe08983e3226da62165430463bb79
ince <csinpgs@gmail.com>
2021-03-02 13:35:57 +0900

    [common] ActionLoggerConfiguration이 먼저 실행 되도록 수정
    Signed-off-by: ince <csinpgs@gmail.com>

--- moka-core/moka-core-common/src/main/java/jmnet/moka/core/common/logger/ActionLoggerConfiguration.java
+++ moka-core/moka-core-common/src/main/java/jmnet/moka/core/common/logger/ActionLoggerConfiguration.java
@@ -3,6 +3,8 @@ package jmnet.moka.core.common.logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 /**
  * <pre>
@@ -12,24 +14,27 @@ import org.springframework.context.annotation.Configuration;
  * 2020. 9. 28. ince 최초생성
  * </pre>
  *
- * @since 2020. 9. 28. 오후 1:14:45
  * @author ince
+ * @since 2020. 9. 28. 오후 1:14:45
  */
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class ActionLoggerConfiguration {
 
-  @Value("${system.id:moka}")
-  private String systemId;
+    @Value("${system.id:moka}")
+    private String systemId;
 
-  /**
-   * <pre>
-   * ActionLogger Bean을 생성한다.
-   * </pre>
-   * @return ActionLogger
-   * @throws Exception 예외
-   */
-  @Bean
-  public ActionLogger actionLogger() throws Exception {
-    return ActionLogger.getInstance(systemId);
-  }
+    /**
+     * <pre>
+     * ActionLogger Bean을 생성한다.
+     * </pre>
+     *
+     * @return ActionLogger
+     * @throws Exception 예외
+     */
+    @Bean
+    public ActionLogger actionLogger()
+            throws Exception {
+        return ActionLogger.getInstance(systemId);
+    }
 }

7bf13989665493f7e7ce4cc5d6152f484fb7f48e
장미라 <ohtah78@gmail.com>
2021-03-03 18:52:41 +0900

    [dps] list api 수정

--- moka-core/moka-core-dps/src/main/java/jmnet/moka/core/dps/api/handler/DbRequestHandler.java
+++ moka-core/moka-core-dps/src/main/java/jmnet/moka/core/dps/api/handler/DbRequestHandler.java
@@ -18,64 +18,82 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class DbRequestHandler implements RequestHandler {
-	public final static Logger logger = LoggerFactory.getLogger(DbRequestHandler.class);
-	private final static String PARAM_MAP = "PARAM_MAP";
-	@Autowired
-	private DpsSqlSessionFactory sessionFactory;
-	
+    public final static Logger logger = LoggerFactory.getLogger(DbRequestHandler.class);
+    private final static String PARAM_MAP = "PARAM_MAP";
+    @Autowired
+    private DpsSqlSessionFactory sessionFactory;
+
     private HashMap<String, JexlScript> scriptMap = new HashMap<String, JexlScript>(256);
 
-	@Override
-	public ApiResult processRequest(ApiContext apiContext) {
-		DbRequest dbRequest = (DbRequest)apiContext.getCurrentRequest();
-		long startTime = System.currentTimeMillis();
+    @Override
+    public ApiResult processRequest(ApiContext apiContext) {
+        DbRequest dbRequest = (DbRequest) apiContext.getCurrentRequest();
+        long startTime = System.currentTimeMillis();
 
         String mapperId = getMapperId(dbRequest, apiContext);
 
-		List<Map<String,Object>> result = null;
-		if ( dbRequest.getDmlType().equals(DbRequest.DML_TYPE_SELECT)) {
+        List<Map<String, Object>> result = null;
+        if (dbRequest
+                .getDmlType()
+                .equals(DbRequest.DML_TYPE_SELECT)) {
             result = select(apiContext.getSqlSession(), mapperId, apiContext.getCheckedParamMap());
-		} else if ( dbRequest.getDmlType().equals(DbRequest.DML_TYPE_PROCEDURE)) {
-			return processProcedure(apiContext, dbRequest, mapperId, startTime);
-		} else if ( dbRequest.getDmlType().equals(DbRequest.DML_TYPE_INSERT)) {
+        } else if (dbRequest
+                .getDmlType()
+                .equals(DbRequest.DML_TYPE_PROCEDURE)) {
+            return processProcedure(apiContext, dbRequest, mapperId, startTime);
+        } else if (dbRequest
+                .getDmlType()
+                .equals(DbRequest.DML_TYPE_INSERT)) {
             result = insert(apiContext.getSqlSession(), mapperId, apiContext.getCheckedParamMap());
-		} else if ( dbRequest.getDmlType().equals(DbRequest.DML_TYPE_UPDATE)) {
+        } else if (dbRequest
+                .getDmlType()
+                .equals(DbRequest.DML_TYPE_UPDATE)) {
             result = update(apiContext.getSqlSession(), mapperId, apiContext.getCheckedParamMap());
-		} else if ( dbRequest.getDmlType().equals(DbRequest.DML_TYPE_DELETE)) {
+        } else if (dbRequest
+                .getDmlType()
+                .equals(DbRequest.DML_TYPE_DELETE)) {
             result = delete(apiContext.getSqlSession(), mapperId, apiContext.getCheckedParamMap());
-		}
-		long endTime = System.currentTimeMillis();
-		return ApiResult.createApiResult(startTime, endTime, result, true, null);
-	}
+        }
+        long endTime = System.currentTimeMillis();
+        return ApiResult.createApiResult(startTime, endTime, result, true, null);
+    }
 
-	private ApiResult processProcedure(ApiContext apiContext, DbRequest dbRequest, String mapperId, long startTime) {
-		try {
-			Map<String, Object> parameterMap = apiContext.getCheckedParamMap();
-			ApiResult apiResult = null;
-			List<Object> resultList = call(apiContext.getSqlSession(), mapperId, parameterMap);
-			long endTime = System.currentTimeMillis();
-			String setNames = dbRequest.getSetNames();
-			if (McpString.isNotEmpty(setNames)) {
-				String[] setNamesArray = setNames.split(",");
-				for (int i = 0; i < setNamesArray.length; i++) {
-					if (apiResult == null) {
-						apiResult = ApiResult.createApiResult(startTime, endTime, resultList.get(i), true, null);
-					} else {
-						apiResult.addApiResult(setNamesArray[i], ApiResult.createApiResult(startTime, endTime, resultList.get(i), true, null));
-					}
-				}
-				apiResult.put(PARAM_MAP,parameterMap);
-				return apiResult;
-			} else {
-				apiResult = ApiResult.createApiResult(startTime, endTime, resultList.get(0), true, null);
-				apiResult.put(PARAM_MAP,parameterMap);
-				return apiResult;
-			}
-		} catch ( Exception e) {
-			logger.error("Procedure Call Error: api = {}", apiContext.getApiId(), e);
-			return ApiResult.createApiErrorResult("Procedure Call Error",e);
-		}
-	}
+    private ApiResult processProcedure(ApiContext apiContext, DbRequest dbRequest, String mapperId, long startTime) {
+        try {
+            Map<String, Object> parameterMap = apiContext.getCheckedParamMap();
+            ApiResult apiResult = null;
+            List<Object> resultList = call(apiContext.getSqlSession(), mapperId, parameterMap);
+            long endTime = System.currentTimeMillis();
+            String setNames = dbRequest.getSetNames();
+            if (McpString.isNotEmpty(setNames)) {
+                String[] setNamesArray = setNames.split(",");
+                for (int i = 0; i < setNamesArray.length; i++) {
+                    if (apiResult == null) {
+                        if (resultList.size() > 0) {
+                            apiResult = ApiResult.createApiResult(startTime, endTime, resultList.get(i), true, null);
+                        } else {
+                            apiResult = ApiResult.createApiResult(startTime, endTime, new ArrayList<>(), true, null);
+                        }
+                    } else {
+                        if (resultList.size() > 0) {
+                            apiResult.addApiResult(setNamesArray[i], ApiResult.createApiResult(startTime, endTime, resultList.get(i), true, null));
+                        } else {
+                            apiResult.addApiResult(setNamesArray[i], ApiResult.createApiResult(startTime, endTime, new ArrayList<>(), true, null));
+                        }
+                    }
+                }
+                apiResult.put(PARAM_MAP, parameterMap);
+                return apiResult;
+            } else {
+                apiResult = ApiResult.createApiResult(startTime, endTime, resultList.get(0), true, null);
+                apiResult.put(PARAM_MAP, parameterMap);
+                return apiResult;
+            }
+        } catch (Exception e) {
+            logger.error("Procedure Call Error: api = {}", apiContext.getApiId(), e);
+            return ApiResult.createApiErrorResult("Procedure Call Error", e);
+        }
+    }
 
     private String getMapperId(DbRequest dbRequest, ApiContext apiContext) {
         String mapperId = dbRequest.getMapperId();
@@ -93,67 +111,75 @@ public class DbRequestHandler implements RequestHandler {
         return mapperId;
     }
 
-	@Override
-	public void processAsyncRequest(AsyncRequestContext asyncRequestContext) {
-		ApiContext apiContext = asyncRequestContext.getApiContext();
-		DbRequest dbRequest = (DbRequest)asyncRequestContext.getRequest();
-		SqlSession sqlSession = null;
-		try {
-			sqlSession = this.sessionFactory.getSqlSession(apiContext.getApiPath());
-			if ( dbRequest.getDmlType().equals(DbRequest.DML_TYPE_SELECT)) {
-				// 결과를 반환할 수 없으므로 수행하지 않음
-			} else {
-				if ( dbRequest.getDmlType().equals(DbRequest.DML_TYPE_INSERT)) {
-					insert(sqlSession, dbRequest.getMapperId(), apiContext.getCheckedParamMap());
-				} else if ( dbRequest.getDmlType().equals(DbRequest.DML_TYPE_UPDATE)) {
-					update(sqlSession, dbRequest.getMapperId(), apiContext.getCheckedParamMap());
-				} else if ( dbRequest.getDmlType().equals(DbRequest.DML_TYPE_DELETE)) {
-					delete(sqlSession, dbRequest.getMapperId(), apiContext.getCheckedParamMap());
-				}
-				sqlSession.commit();
-			}
-		} catch ( Exception e) {
-			if ( sqlSession != null) {
-				sqlSession.rollback();
-			}
-			logger.error("request exception : {}", e, e);
-		} finally {
-			if ( sqlSession != null) {
-				sqlSession.close();
-			}
-		}
-	}
-	
-	private List<Map<String,Object>> select(SqlSession sqlSession, String mapperId, Map<String, Object> parameterMap) {
-		List<Map<String,Object>> result = sqlSession.selectList(mapperId, parameterMap);
-		return result;
-	}
+    @Override
+    public void processAsyncRequest(AsyncRequestContext asyncRequestContext) {
+        ApiContext apiContext = asyncRequestContext.getApiContext();
+        DbRequest dbRequest = (DbRequest) asyncRequestContext.getRequest();
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = this.sessionFactory.getSqlSession(apiContext.getApiPath());
+            if (dbRequest
+                    .getDmlType()
+                    .equals(DbRequest.DML_TYPE_SELECT)) {
+                // 결과를 반환할 수 없으므로 수행하지 않음
+            } else {
+                if (dbRequest
+                        .getDmlType()
+                        .equals(DbRequest.DML_TYPE_INSERT)) {
+                    insert(sqlSession, dbRequest.getMapperId(), apiContext.getCheckedParamMap());
+                } else if (dbRequest
+                        .getDmlType()
+                        .equals(DbRequest.DML_TYPE_UPDATE)) {
+                    update(sqlSession, dbRequest.getMapperId(), apiContext.getCheckedParamMap());
+                } else if (dbRequest
+                        .getDmlType()
+                        .equals(DbRequest.DML_TYPE_DELETE)) {
+                    delete(sqlSession, dbRequest.getMapperId(), apiContext.getCheckedParamMap());
+                }
+                sqlSession.commit();
+            }
+        } catch (Exception e) {
+            if (sqlSession != null) {
+                sqlSession.rollback();
+            }
+            logger.error("request exception : {}", e, e);
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
 
-	private List<Object> call(SqlSession sqlSession, String mapperId, Map<String, Object> parameterMap) {
-		List<Object> resultList = sqlSession.selectList(mapperId, parameterMap);
-		return resultList;
-	}
-	
-	private List<Map<String,Object>> insert(SqlSession sqlSession, String mapperId, Map<String, Object> parameterMap) {
-		int insertCount = sqlSession.insert(mapperId, parameterMap);
-		return makeCountResult("insertCount",insertCount);
-	}
-	
-	private List<Map<String,Object>> update(SqlSession sqlSession, String mapperId, Map<String, Object> parameterMap) {
-		int updateCount = sqlSession.update(mapperId, parameterMap);
-		return makeCountResult("updateCount",updateCount);
-	}
+    private List<Map<String, Object>> select(SqlSession sqlSession, String mapperId, Map<String, Object> parameterMap) {
+        List<Map<String, Object>> result = sqlSession.selectList(mapperId, parameterMap);
+        return result;
+    }
 
-	private List<Map<String,Object>> delete(SqlSession sqlSession, String mapperId, Map<String, Object> parameterMap) {
-		int deleteCount = sqlSession.delete(mapperId, parameterMap);
-		return makeCountResult("deleteCount",deleteCount);
-	}
-	
-	private List<Map<String,Object>> makeCountResult(String name, int count) {
-		List<Map<String,Object>> resultList = new ArrayList<Map<String,Object>>();
-		HashMap<String,Object> resultMap = new HashMap<String,Object>();
-		resultMap.put(name, count);
-		resultList.add(resultMap);
-		return resultList;
-	}
+    private List<Object> call(SqlSession sqlSession, String mapperId, Map<String, Object> parameterMap) {
+        List<Object> resultList = sqlSession.selectList(mapperId, parameterMap);
+        return resultList;
+    }
+
+    private List<Map<String, Object>> insert(SqlSession sqlSession, String mapperId, Map<String, Object> parameterMap) {
+        int insertCount = sqlSession.insert(mapperId, parameterMap);
+        return makeCountResult("insertCount", insertCount);
+    }
+
+    private List<Map<String, Object>> update(SqlSession sqlSession, String mapperId, Map<String, Object> parameterMap) {
+        int updateCount = sqlSession.update(mapperId, parameterMap);
+        return makeCountResult("updateCount", updateCount);
+    }
+
+    private List<Map<String, Object>> delete(SqlSession sqlSession, String mapperId, Map<String, Object> parameterMap) {
+        int deleteCount = sqlSession.delete(mapperId, parameterMap);
+        return makeCountResult("deleteCount", deleteCount);
+    }
+
+    private List<Map<String, Object>> makeCountResult(String name, int count) {
+        List<Map<String, Object>> resultList = new ArrayList<Map<String, Object>>();
+        HashMap<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put(name, count);
+        resultList.add(resultMap);
+        return resultList;
+    }
 }

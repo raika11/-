15739ba9bac5626f3537b03f7cefadd975ea6052
박성원 <saparksung@gmail.com>
2021-03-04 11:51:57 +0900

    [rcv] spotBugs Warning 해결

--- moka-web/moka-web-rcv/src/main/java/jmnet/moka/web/rcv/util/ThreadUtil.java
+++ moka-web/moka-web-rcv/src/main/java/jmnet/moka/web/rcv/util/ThreadUtil.java
@@ -18,14 +18,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ThreadUtil {
     private static final Object mutex = new Object();
-    private static ThreadGroup THREAD_GROUP;
+    private static volatile ThreadGroup THREAD_GROUP;
 
     private static ThreadGroup getThreadGroup() {
-        if( THREAD_GROUP == null ) {
-            synchronized (mutex) {
-                if (THREAD_GROUP == null)
-                    THREAD_GROUP = new ThreadGroup("RCV");
-            }
+        synchronized (mutex) {
+            if (THREAD_GROUP == null)
+                THREAD_GROUP = new ThreadGroup("RCV");
         }
         return THREAD_GROUP;
     }

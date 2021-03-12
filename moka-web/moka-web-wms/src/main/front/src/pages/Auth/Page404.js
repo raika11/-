88c7b9bf98b6f248bbdb4eb50f6b5a3a52199e0f
김태형 <xorwk1@gmail.com>
2021-03-12 13:21:59 +0900

    [react] JMBM-241 오류페이지를 수정한다.

--- moka-web/moka-web-wms/src/main/front/src/pages/Auth/Page404.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Auth/Page404.js
@@ -9,12 +9,13 @@ import Button from 'react-bootstrap/Button';
 const Page404 = ({ defaultLink }) => (
     <div className="d-flex h-100 justify-content-center align-items-center">
         <div className="text-center">
-            <h1 className="display-1 font-weight-bold">404</h1>
-            <p className="h1">Page not found.</p>
-            <p className="h2 font-weight-normal mt-3 mb-4">The page you are looking for might have been removed.</p>
+            {/*<h1 className="display-1 font-weight-bold">404</h1>*/}
+            <p className="h1">페이지를 찾을 수 없습니다..</p>
+            <p className="h2 font-weight-normal mt-3 mb-2">페이지가 없거나 오류가 발생하였습니다. 이용에 불편을 드려 죄송합니다.</p>
+            <p className="h2 font-weight-normal mb-4">입력하신 주소가 정확한지 확인 후 다시 시도해 주시기 바랍니다.</p>
             <Link to={defaultLink || '/dashboard'}>
                 <Button color="primary" size="lg">
-                    Return to website
+                    메인 페이지로 돌아가기
                 </Button>
             </Link>
         </div>

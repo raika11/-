6939034d3496e1376c074016403abe7debf44a21
전현지 <emproject525@gmail.com>
2021-03-12 13:40:19 +0900

    [react] usedYn 공통 컴포넌트로 노출

--- moka-web/moka-web-wms/src/main/front/src/pages/Menu/MenuAgGridColumns.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Menu/MenuAgGridColumns.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import { faCircle } from '@moka/fontawesome-pro-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import MokaTableUsedYnRenderer from '@components/MokaTable/MokaTableUsedYnRenderer';
 
 export const localeText = { noRowsToShow: '조회 결과가 없습니다.', loadingOoo: '조회 중입니다..' };
 
@@ -8,35 +6,29 @@ export const columnDefs = [
     {
         rowDrag: true,
         width: 24,
+        minWidth: 24,
         suppressMenu: true,
         rowDragText: (params) => {
             return params.rowNode.data.menuDisplayNm;
         },
-        valueSetter: () => true,
     },
     {
         headerName: '번호',
         field: 'menuSeq',
         width: 50,
+        cellStyle: { lineHeight: '34px' },
     },
     {
         headerName: '메뉴명',
         field: 'menuDisplayNm',
         width: 140,
         flex: 1,
+        cellStyle: { lineHeight: '34px', cursor: 'pointer' },
     },
     {
         headerName: '',
         field: 'usedYn',
-        width: 50,
-        cellStyle: { textAlign: 'center' },
-        cellRendererFramework: (params) => {
-            const usedYn = params.data.usedYn;
-            let clazz = 'color-negative';
-            if (usedYn === 'Y') {
-                clazz = 'color-positive';
-            }
-            return <FontAwesomeIcon icon={faCircle} fixedWidth className={clazz} />;
-        },
+        width: 38,
+        cellRendererFramework: MokaTableUsedYnRenderer,
     },
 ];

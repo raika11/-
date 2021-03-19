b556629073dfc031dd01d5ad96cfca92fe214d8e
guregal <guregal@nate.com>
2021-03-19 16:31:55 +0900

    [wpush] 시스템 Controller /command/health 추가

--- moka-web/moka-web-push/src/main/java/jmnet/moka/web/push/mvc/system/controller/SystemController.java
+++ moka-web/moka-web-push/src/main/java/jmnet/moka/web/push/mvc/system/controller/SystemController.java
@@ -1,5 +1,10 @@
 package jmnet.moka.web.push.mvc.system.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-02-05 17:40
  */
 @RestController
-@RequestMapping("/api/system")
+@RequestMapping(produces = "application/json; charset=UTF-8")
 public class SystemController {
+
+    @GetMapping(path = "/command/health", produces = "text/plain")
+    public ResponseEntity<?> _health(HttpServletRequest request, HttpServletResponse response) {
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
 }

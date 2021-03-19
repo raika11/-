823956c676ad88f9026db9020ba3b4d2563ab97f
전현지 <emproject525@gmail.com>
2021-03-19 19:17:40 +0900

    [react] boundary test

--- moka-web/moka-web-wms/src/main/front/src/components/MokaErrorBoundary/MokaErrorFallback.js
+++ moka-web/moka-web-wms/src/main/front/src/components/MokaErrorBoundary/MokaErrorFallback.js
@@ -6,9 +6,15 @@ import Button from 'react-bootstrap/Button';
  */
 const MokaErrorFallback = () => {
     return (
-        <div className="vw-100 vh-100 d-flex flex-column align-items-center justify-content-center" style={{ opacity: '70%' }}>
+        <div className="vw-100 vh-100 d-flex flex-column align-items-center justify-content-center">
             <h1>파일이 변경되어 페이지를 로드하지 못했습니다</h1>
-            <Button size="lg" variant="negative" onClick={() => window.location.reload()}>
+            <Button
+                size="lg"
+                variant="negative"
+                onClick={() => {
+                    window.location.reload();
+                }}
+            >
                 새로고침 하기
             </Button>
         </div>

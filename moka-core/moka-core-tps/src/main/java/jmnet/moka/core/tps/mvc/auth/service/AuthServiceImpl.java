98e0716c96729cc0710231f98a41bf26478ae87e
ince <csinpgs@gmail.com>
2021-03-22 11:45:50 +0900

    [tps] 권한 테스트용 그룹 아이디 추가
    Signed-off-by: ince <csinpgs@gmail.com>

--- moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/auth/service/AuthServiceImpl.java
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/auth/service/AuthServiceImpl.java
@@ -57,10 +57,13 @@ public class AuthServiceImpl implements AuthService, UserDetailsService {
         if (groupMembers != null && !groupMembers.isEmpty()) {
             if (groupMembers
                     .stream()
-                    .filter(gm -> gm
+                    .filter(gm -> (gm
                             .getGroup()
                             .getGroupCd()
-                            .equals(TpsConstants.SUPER_ADMIN_GROUP_CD) && MokaConstants.YES.equals(gm.getUsedYn()))
+                            .equals(TpsConstants.SUPER_ADMIN_GROUP_CD) || gm
+                            .getGroup()
+                            .getGroupCd()
+                            .equals(TpsConstants.TEST_ADMIN_GROUP_CD)) && MokaConstants.YES.equals(gm.getUsedYn()))
                     .count() > 0) {
                 return Arrays.asList(new SimpleGrantedAuthority(TpsConstants.ROLE_SUPERADMIN));
             }

4498ec3c312c123432076db65fbede573f767e30
ince <csinpgs@gmail.com>
2021-03-30 18:53:24 +0900

    [abtest] ab test 프로젝트 최초 commit
    Signed-off-by: ince <csinpgs@gmail.com>

---
+++ moka-web/moka-web-abtest/src/main/java/jmnet/moka/web/abtest/config/WebSecurityConfiguration.java
@@ -0,0 +1,54 @@
+package jmnet.moka.web.abtest.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.session.SessionManagementFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${inbound.ips}")
+    private String[] inboundIps;
+
+    @Bean
+    public IpAuthenticationFilter ipAuthenticationFilter()
+            throws Exception {
+        return new IpAuthenticationFilter(this.authenticationManager(), inboundIps);
+    }
+
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers("/assets/**", "/html/**", "/static/**", "/webjars/**", "/favicon.ico", "/*.js", "/*.map", "/*.png", "/css/**",
+                        "/*.json");
+    }
+
+    @Override
+    public void configure(HttpSecurity http)
+            throws Exception {
+        http.addFilterBefore(ipAuthenticationFilter(), SessionManagementFilter.class);
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/", "/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+
+}

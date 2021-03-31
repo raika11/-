82bcae26ec381c14ef5b0c61fa23a2e503a7400c
김태형 <xorwk1@gmail.com>
2021-03-31 19:03:25 +0900

    [tps] 그룹관리: 그룹 삭제시 메세지 처리

--- moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/group/controller/GroupRestController.java
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/group/controller/GroupRestController.java
@@ -319,7 +319,7 @@ public class GroupRestController extends AbstractCommonController {
             tpsLogger.success(ActionType.DELETE);
 
             // 결과리턴
-            ResultDTO<Boolean> resultDto = new ResultDTO<>(true);
+            ResultDTO<Boolean> resultDto = new ResultDTO<>(true, msg("tps.group.success.delete"));
             return new ResponseEntity<>(resultDto, HttpStatus.OK);
 
         } catch (Exception e) {

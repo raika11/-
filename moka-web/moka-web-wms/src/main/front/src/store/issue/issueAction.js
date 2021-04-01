da46fbaf409a69405d34575580ff374ea9881791
전현지 <emproject525@gmail.com>
2021-04-01 14:24:10 +0900

    [react] 이슈 api, action 추가

--- moka-web/moka-web-wms/src/main/front/src/store/issue/issueAction.js
+++ moka-web/moka-web-wms/src/main/front/src/store/issue/issueAction.js
@@ -1,20 +1,40 @@
 import { createRequestActionTypes } from '@store/commons/saga';
 import { createAction } from 'redux-actions';
 
-export const CLEAR_PACKAGE_STORE = 'package/CLEAR_PACKAGE_STORE';
-export const clearPackageStore = createAction(CLEAR_PACKAGE_STORE);
+/**
+ * 스토어 데이터 변경
+ */
+export const CLEAR_STORE = 'issue/CLEAR_STORE';
+export const clearStore = createAction(CLEAR_STORE);
+export const CLEAR_ISSUE = 'issue/CLEAR_ISSUE';
+export const clearIssue = createAction(CLEAR_ISSUE);
 
-export const CLEAR_PACKAGE_LIST = 'package/CLEAR_PACKAGE_LIST';
-export const clearPackageList = createAction(CLEAR_PACKAGE_LIST);
+/**
+ * 이슈 목록 조회
+ */
+export const [GET_ISSUE_LIST, GET_ISSUE_LIST_SUCCESS, GET_ISSUE_LIST_FAILURE] = createRequestActionTypes('issue/GET_ISSUE_LIST');
+export const getIssueList = createAction(GET_ISSUE_LIST, ({ search, callback }) => ({ search, callback }));
+export const GET_ISSUE_LIST_MODAL = 'issue/GET_ISSUE_LIST_MODAL';
+export const getIssueListModal = createAction(GET_ISSUE_LIST_MODAL, ({ search, callback }) => ({ search, callback }));
+export const [GET_ISSUE_LIST_GROUP_BY_ORDNO] = createRequestActionTypes('issue/GET_ISSUE_LIST_GROUP_BY_ORDNO');
+export const getIssueListGroupByOrdno = createAction(GET_ISSUE_LIST_GROUP_BY_ORDNO, ({ search, callback }) => ({ search, callback }));
 
-export const CLEAR_PACKAGE = 'package/CLEAR_PACKAGE';
-export const clearPackage = createAction(CLEAR_PACKAGE);
+/**
+ * 이슈 조회
+ */
+export const [GET_ISSUE, GET_ISSUE_SUCCESS, GET_ISSUE_FAILURE] = createRequestActionTypes('issue/GET_ISSUE');
+export const getIssue = createAction(GET_ISSUE, ({ pkgSeq, callback }) => ({ pkgSeq, callback }));
+export const [GET_ISSUE_GROUP_BY_ORDNO] = createRequestActionTypes('issue/GET_ISSUE_GROUP_BY_ORDNO');
+export const getIssueGroupByOrdno = createAction(GET_ISSUE_GROUP_BY_ORDNO, ({ pkgSeq, callback }) => ({ pkgSeq, callback }));
 
-export const [GET_PACKAGE_LIST, GET_PACKAGE_LIST_SUCCESS, GET_PACKAGE_LIST_FAILURE] = createRequestActionTypes('package/GET_PACKAGE_LIST');
-export const getPackageList = createAction(GET_PACKAGE_LIST);
+/**
+ * 이슈 저장
+ */
+export const SAVE_ISSUE = 'issue/SAVE_ISSUE';
+export const saveIssue = createAction(SAVE_ISSUE, ({ pkg, callback }) => ({ pkg, callback }));
 
-export const [GET_PACKAGE, GET_PACKAGE_SUCCESS, GET_PACKAGE_FAILURE] = createRequestActionTypes('package/GET_PACKAGE');
-export const getPackage = createAction(GET_PACKAGE);
-
-export const SAVE_PACKAGE = 'package/SAVE_PACKAGE';
-export const savePackage = createAction(SAVE_PACKAGE);
+/**
+ * 이슈 종료
+ */
+export const FINISH_ISSUE = 'issue/FINISH_ISSUE';
+export const finishIssue = createAction(FINISH_ISSUE, ({ pkgSeq, callback }) => ({ pkgSeq, callback }));
--- moka-web/moka-web-wms/src/main/front/src/store/issue/issueSaga.js
+++ moka-web/moka-web-wms/src/main/front/src/store/issue/issueSaga.js
@@ -1,38 +1,31 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
-import { finishLoading, startLoading } from '@store/loading';
-import moment from 'moment';
-import { DB_DATEFORMAT } from '@/constants';
+import {
+    // call, put,
+    takeLatest,
+} from 'redux-saga/effects';
+// import moment from 'moment';
+// import { DB_DATEFORMAT } from '@/constants';
+// import { finishLoading, startLoading } from '@store/loading';
+import {
+    // errorResponse,
+    createRequestSaga,
+} from '@store/commons/saga';
 import * as api from '@store/issue/issueApi';
-import * as action from '@store/issue/issueAction';
-import { errorResponse } from '@store/commons/saga';
+import * as act from '@store/issue/issueAction';
 
-const toPackageListData = (response) => {
-    return response;
-};
+/**
+ * 이슈 목록 조회
+ */
+const getIssueList = createRequestSaga(act.GET_ISSUE_LIST, api.getIssueList);
 
-function* getPackageList({ type, payload }) {
-    yield put(startLoading(type));
+/**
+ * 이슈 목록 조회 (모달)
+ */
+const getIssueListModal = createRequestSaga(act.GET_ISSUE_LIST_MODAL, api.getIssueList, true);
 
-    try {
-        const startDt = payload.startDt && payload.startDt.isValid() ? moment(payload.startDt).format(DB_DATEFORMAT) : null;
-        const endDt = payload.endDt && payload.endDt.isValid() ? moment(payload.endDt).format(DB_DATEFORMAT) : null;
-        const search = { ...payload, startDt, endDt };
-
-        const response = yield call(api.getPackageList, { search });
-        if (response.data.header.success) {
-            const list = toPackageListData(response.data.body.list);
-
-            yield put({ type: `${type}_SUCCESS`, payload: { ...response.data, body: { ...response.data.body, list } } });
-        } else {
-            yield put({ type: `${type}_FAILURE`, payload: response.data });
-        }
-    } catch (e) {
-        yield put({ type: `${type}_FAILURE`, payload: errorResponse(e) });
-    }
-    yield put(finishLoading(type));
-}
-
-export default function* snsSaga() {
-    /************* 메타 **********************/
-    yield takeLatest(action.GET_PACKAGE_LIST, getPackageList);
+/**
+ * saga
+ */
+export default function* saga() {
+    yield takeLatest(act.GET_ISSUE_LIST, getIssueList);
+    yield takeLatest(act.GET_ISSUE_LIST_MODAL, getIssueListModal);
 }

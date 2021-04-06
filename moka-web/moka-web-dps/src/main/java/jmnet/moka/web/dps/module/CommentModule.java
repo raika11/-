2aaa7a8737eaa8a05dcf29e76722535d7673133a
장미라 <ohtah78@gmail.com>
2021-04-06 19:29:22 +0900

    [dps] commentVote는 비회원등록 가능하도록 수정. 댓글등록시, 에피소스Seq도 파라미터로 받도록 추가. referrer를 domain,section로 분리

--- moka-web/moka-web-dps/src/main/java/jmnet/moka/web/dps/module/CommentModule.java
+++ moka-web/moka-web-dps/src/main/java/jmnet/moka/web/dps/module/CommentModule.java
@@ -52,48 +52,199 @@ public class CommentModule implements ModuleInterface {
         return null;
     }
 
-    // 댓글등록
+    /**
+     * 댓글등록
+     *
+     * @param apiContext
+     * @return 댓글등록결과 _DATA안에 들어감.
+     * @throws ApiException
+     */
     public Object insert(ApiContext apiContext)
             throws ApiException {
         Map<String, Object> returnMap = new HashMap<>();
+        try {
+            Map<String, Object> checkedParamMap = apiContext.getCheckedParamMap();
 
-        // totalId와 repSeq중 하나만 세팅되야 함.
-        if (!this.checkId(apiContext)) {
+            // totalId와 repSeq중 하나만 세팅되야 함.
+            if (!this.checkId(checkedParamMap)) {
+                returnMap.put("SUCCESS", false);
+                return returnMap;
+            }
+
+            // 차단정보체크.
+            Banned banned = this.checkBanned(apiContext);
+            if (banned.isBanned()) {
+                returnMap.put("SUCCESS", !banned.isBanned());
+                returnMap.put("CODE", banned.getType());
+                returnMap.put("TARGET", banned.getValue());
+                return returnMap;
+            }
+
+            // 기자체크
+            if (!this.checkReporter(apiContext)) {
+                returnMap.put("SUCCESS", false);
+                returnMap.put("TARGET", "해당 기자가 존재하지 않습니다.");
+                return returnMap;
+            }
+
+            // 기사체크
+            if (!this.checkArticle(apiContext)) {
+                returnMap.put("SUCCESS", false);
+                returnMap.put("TARGET", "해당 기사가 존재하지 않습니다");
+                return returnMap;
+            }
+
+            // 에피소드 체크
+            if (!this.checkEpisode(apiContext)) {
+                returnMap.put("SUCCESS", false);
+                returnMap.put("TARGET", "해당 에피소드가 존재하지 않습니다");
+                return returnMap;
+            }
+
+            // 등록
+            List<Map> result = this.call(apiContext, "dps.comment.insert", checkedParamMap);
+            returnMap.put("SUCCESS", true);
+            returnMap.put("RESULT", result);
+            return returnMap;
+
+        } catch (Exception e) {
+            log.error("module exception : {} {} {}", apiContext.getApiPath(), apiContext.getApiId(), e.getMessage(), e);
             returnMap.put("SUCCESS", false);
+            returnMap.put("TARGET", e.getMessage());
             return returnMap;
         }
-
-        // 차단정보체크.
-        Banned banned = this.checkBanned(apiContext);
-        if (banned.isBanned()) {
-            returnMap.put("SUCCESS", !banned.isBanned());
-            returnMap.put("CODE", banned.getType());
-            returnMap.put("TARGET", banned.getValue());
-            return returnMap;
-        }
-
-        // 기자체크
-        if (!this.checkReporter(apiContext)) {
-            returnMap.put("SUCCESS", false);
-            returnMap.put("TARGET", "해당 기자가 존재하지 않습니다.");
-            return returnMap;
-        }
-
-        // 기사체크
-        if (!this.checkArticle(apiContext)) {
-            returnMap.put("SUCCESS", false);
-            returnMap.put("TARGET", "해당 기사가 존재하지 않습니다");
-            return returnMap;
-        }
-
-        // 등록
-        Map<String, Object> checkedParamMap = apiContext.getCheckedParamMap();
-        List<Map> result = this.call(apiContext, "dps.comment.insert", checkedParamMap);
-        returnMap.put("RESULT", result);
-        return returnMap;
     }
 
-    //기사조회. 기사가 없으면 false
+
+    /**
+     * 댓글삭제
+     *
+     * @param apiContext
+     * @return
+     * @throws ApiException
+     */
+    public Object delete(ApiContext apiContext)
+            throws ApiException {
+        Map<String, Object> returnMap = new HashMap<>();
+        try {
+            Map<String, Object> checkedParamMap = apiContext.getCheckedParamMap();
+            List<Map> result = this.call(apiContext, "dps.comment.delete", checkedParamMap);
+            returnMap.put("SUCCESS", false);
+
+            if (result.size() > 0) {
+                int intVal = Integer.parseInt(result
+                        .get(0)
+                        .get("RESULT")
+                        .toString());
+                if (intVal == 0) {
+                    returnMap.put("SUCCESS", false);
+                } else if (intVal == 1) {
+                    returnMap.put("SUCCESS", true);
+                } else if (intVal == 2) {
+                    returnMap.put("SUCCESS", false);
+                    returnMap.put("CODE", "Unauthorized");
+                }
+            }
+            return returnMap;
+        } catch (Exception e) {
+            log.error("module exception : {} {} {}", apiContext.getApiPath(), apiContext.getApiId(), e.getMessage(), e);
+            returnMap.put("SUCCESS", false);
+            returnMap.put("TARGET", e.getMessage());
+            return returnMap;
+        }
+    }
+
+    /**
+     * 댓글 좋아요/싫어요/신고 등록
+     *
+     * @param apiContext
+     * @return
+     * @throws ApiException
+     */
+    public Object insertVote(ApiContext apiContext)
+            throws ApiException {
+        Map<String, Object> returnMap = new HashMap<>();
+
+        try {
+            Map<String, Object> checkedParamMap = apiContext.getCheckedParamMap();
+            if (!checkedParamMap.containsKey("member_memSeq")) {
+                checkedParamMap.put("member_memSeq", null);
+                checkedParamMap.put("member_loginType", null);
+            }
+            List<Map> result = this.call(apiContext, "dps.comment.insertVote", checkedParamMap);
+
+            if (result.size() > 0) {
+                int intVal = Integer.parseInt(result
+                        .get(0)
+                        .get("RESULT")
+                        .toString());
+                if (intVal == 0) {
+                    returnMap.put("SUCCESS", false);
+                } else if (intVal == 1) {
+                    returnMap.put("SUCCESS", true);
+                } else if (intVal == 2) {
+                    returnMap.put("SUCCESS", false);
+                    returnMap.put("CODE", "Duplicated");
+                }
+            }
+            return returnMap;
+        } catch (Exception e) {
+            log.error("module exception : {} {} {}", apiContext.getApiPath(), apiContext.getApiId(), e.getMessage(), e);
+            returnMap.put("SUCCESS", false);
+            returnMap.put("TARGET", e.getMessage());
+            return returnMap;
+        }
+    }
+
+    /**
+     * totalId, repSeq, epsdSeq중 하나만 세팅되야 함.
+     *
+     * @param checkedParamMap 파라미터정보
+     * @return 셋다 세팅되어 있으면  false
+     */
+    private boolean checkId(Map<String, Object> checkedParamMap) {
+        int checkCnt = 0;
+
+        Integer totalId = 0;
+        if (checkedParamMap.containsKey("totalId") && checkedParamMap.get("totalId") != null) {
+            totalId = Integer.parseInt(checkedParamMap
+                    .get("totalId")
+                    .toString());
+            if (totalId > 0) {
+                checkCnt++;
+            }
+        }
+        Integer repSeq = 0;
+        if (checkedParamMap.containsKey("repSeq") && checkedParamMap.get("repSeq") != null) {
+            repSeq = Integer.parseInt(checkedParamMap
+                    .get("repSeq")
+                    .toString());
+            if (repSeq > 0) {
+                checkCnt++;
+            }
+        }
+        Integer epsdSeq = 0;
+        if (checkedParamMap.containsKey("epsdSeq") && checkedParamMap.get("epsdSeq") != null) {
+            epsdSeq = Integer.parseInt(checkedParamMap
+                    .get("epsdSeq")
+                    .toString());
+            if (epsdSeq > 0) {
+                checkCnt++;
+            }
+        }
+
+        if (checkCnt > 1) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 기사조회
+     *
+     * @param apiContext
+     * @return 기사가 없으면 false
+     */
     private boolean checkArticle(ApiContext apiContext) {
         Map<String, Object> checkedParamMap = apiContext.getCheckedParamMap();
         Integer totalId = 0;
@@ -103,14 +254,21 @@ public class CommentModule implements ModuleInterface {
                     .toString());
         }
 
-        String referrer = null;
-        if (checkedParamMap.containsKey("referrer") && checkedParamMap.get("referrer") != null) {
-            referrer = checkedParamMap
-                    .get("referrer")
+        String domain = null;
+        if (checkedParamMap.containsKey("domain") && checkedParamMap.get("domain") != null) {
+            domain = checkedParamMap
+                    .get("domain")
                     .toString();
         }
 
-        if (totalId > 0 && !(referrer.contains("peoplemic.joins.com") || referrer.contains("joongang.joins.com/jpod"))) {
+        String section = null;
+        if (checkedParamMap.containsKey("section") && checkedParamMap.get("section") != null) {
+            section = checkedParamMap
+                    .get("section")
+                    .toString();
+        }
+
+        if (totalId > 0 && domain.contains("joongang.joins.com") && section.contains("/article")) {
             Map<String, Object> parameterMap = new HashMap<>();
             parameterMap.put("totalId", totalId);
             parameterMap.put("_EXIST", 0);
@@ -128,78 +286,82 @@ public class CommentModule implements ModuleInterface {
         return true;
     }
 
-    // totalId와 repSeq중 하나만 세팅되야 함. 둘다 세팅되어 있으면  false
-    private boolean checkId(ApiContext apiContext) {
+    /**
+     * 기자존재여부 조회
+     *
+     * @param apiContext
+     * @return 존재하지 않으면 false
+     * @throws ApiException
+     */
+    private Boolean checkReporter(ApiContext apiContext)
+            throws ApiException {
         Map<String, Object> checkedParamMap = apiContext.getCheckedParamMap();
-        Integer totalId = 0;
-        if (checkedParamMap.containsKey("totalId") && checkedParamMap.get("totalId") != null) {
-            totalId = Integer.parseInt(checkedParamMap
-                    .get("totalId")
-                    .toString());
-        }
         Integer repSeq = 0;
         if (checkedParamMap.containsKey("repSeq") && checkedParamMap.get("repSeq") != null) {
             repSeq = Integer.parseInt(checkedParamMap
                     .get("repSeq")
                     .toString());
         }
-        if (totalId > 0 && repSeq > 0) {
-            return false;
+
+        if (repSeq > 0) {
+            Map<String, Object> parameterMap = new HashMap<>();
+            parameterMap.put("repSeq", repSeq);
+            parameterMap.put("_EXIST", 0);
+            this.call(apiContext, "dps.reporter.isReporter", parameterMap);
+            if (parameterMap.containsKey("_EXIST")) {
+                int exist = Integer.parseInt(parameterMap
+                        .get("_EXIST")
+                        .toString());
+                if (exist == 0) {
+                    return false;
+                }
+            }
         }
         return true;
     }
 
-    // 댓글삭제
-    public Object delete(ApiContext apiContext)
+    /**
+     * 에피소드존재여부 조회
+     *
+     * @param apiContext
+     * @return 존재하지 않으면 false
+     * @throws ApiException
+     */
+    private Boolean checkEpisode(ApiContext apiContext)
             throws ApiException {
-        Map<String, Object> returnMap = new HashMap<>();
-
         Map<String, Object> checkedParamMap = apiContext.getCheckedParamMap();
-        List<Map> result = this.call(apiContext, "dps.comment.delete", checkedParamMap);
 
-        if (result.size() > 0) {
-            int intVal = Integer.parseInt(result
-                    .get(0)
-                    .get("RESULT")
+        Integer epsdSeq = 0;
+        if (checkedParamMap.containsKey("epsdSeq") && checkedParamMap.get("epsdSeq") != null) {
+            epsdSeq = Integer.parseInt(checkedParamMap
+                    .get("epsdSeq")
                     .toString());
-            if (intVal == 0) {
-                returnMap.put("SUCCESS", false);
-            } else if (intVal == 1) {
-                returnMap.put("SUCCESS", true);
-            } else if (intVal == 2) {
-                returnMap.put("SUCCESS", false);
-                returnMap.put("CODE", "Unauthorized");
+        }
+
+        if (epsdSeq > 0) {
+            Map<String, Object> parameterMap = new HashMap<>();
+            parameterMap.put("epsdSeq", epsdSeq);
+            parameterMap.put("_EXIST", 0);
+            this.call(apiContext, "dps.jpod.isEpisode", parameterMap);
+            if (parameterMap.containsKey("_EXIST")) {
+                int exist = Integer.parseInt(parameterMap
+                        .get("_EXIST")
+                        .toString());
+                if (exist == 0) {
+                    return false;
+                }
             }
         }
-        return returnMap;
+        return true;
     }
 
-    // 댓글 좋아요/싫어요/신고 등록
-    public Object insertVote(ApiContext apiContext)
-            throws ApiException {
-        Map<String, Object> returnMap = new HashMap<>();
-
-        Map<String, Object> checkedParamMap = apiContext.getCheckedParamMap();
-        List<Map> result = this.call(apiContext, "dps.comment.insertVote", checkedParamMap);
-
-        if (result.size() > 0) {
-            int intVal = Integer.parseInt(result
-                    .get(0)
-                    .get("RESULT")
-                    .toString());
-            if (intVal == 0) {
-                returnMap.put("SUCCESS", false);
-            } else if (intVal == 1) {
-                returnMap.put("SUCCESS", true);
-            } else if (intVal == 2) {
-                returnMap.put("SUCCESS", false);
-                returnMap.put("CODE", "Duplicated");
-            }
-        }
-        return returnMap;
-    }
-
-    // 차단정보조회. 차단정보면 Banned.success=false
+    /**
+     * 차단정보조회
+     *
+     * @param apiContext
+     * @return 차단정보면 Banned.success=false
+     * @throws ApiException
+     */
     private Banned checkBanned(ApiContext apiContext)
             throws ApiException {
         Map<String, Object> checkedParamMap = apiContext.getCheckedParamMap();
@@ -286,7 +448,14 @@ public class CommentModule implements ModuleInterface {
                 .build();
     }
 
-    // 프로시저호출
+    /**
+     * 프로시저호출
+     *
+     * @param apiContext
+     * @param mapperId
+     * @param parameterMap
+     * @return
+     */
     private List<Map> call(ApiContext apiContext, String mapperId, Map<String, Object> parameterMap) {
 
         SqlSession sqlSession = null;
@@ -300,7 +469,7 @@ public class CommentModule implements ModuleInterface {
                 sqlSession.rollback();
             }
 
-            log.error("request exception : {}", e.getMessage(), e);
+            log.error("module sql exception : {}", e.getMessage(), e);
             throw e;
         } finally {
             if (sqlSession != null) {
@@ -310,31 +479,5 @@ public class CommentModule implements ModuleInterface {
 
         return resultList;
     }
-
-    // 기자존재여부 조회. 존재하지 않으면 false
-    private Boolean checkReporter(ApiContext apiContext)
-            throws ApiException {
-        Map<String, Object> checkedParamMap = apiContext.getCheckedParamMap();
-
-        if (checkedParamMap.containsKey("repSeq") && checkedParamMap.get("repSeq") != null) {
-            Integer repSeq = Integer.parseInt(checkedParamMap
-                    .get("repSeq")
-                    .toString());
-            Map<String, Object> parameterMap = new HashMap<>();
-            parameterMap.put("repSeq", repSeq);
-            parameterMap.put("_EXIST", 0);
-            this.call(apiContext, "dps.reporter.isReporter", parameterMap);
-            if (parameterMap.containsKey("_EXIST")) {
-                int exist = Integer.parseInt(parameterMap
-                        .get("_EXIST")
-                        .toString());
-                if (exist == 0) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
 
 }

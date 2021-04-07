67eb7799a330c1d4b76fd9ab41e42dff373d0994
정준영 <fdjy0101@gmail.com>
2021-04-07 16:19:05 +0900

    [react] 게시글 테이블 수정

--- moka-web/moka-web-wms/src/main/front/src/pages/Boards/BoardsList/BoardsContentsList/BoardsContentsListAgGrid.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Boards/BoardsList/BoardsContentsList/BoardsContentsListAgGrid.js
@@ -2,14 +2,11 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import moment from 'moment';
-import produce from 'immer';
-import { MokaTable, MokaPagination, MokaLoader } from '@components';
-import { BASIC_DATEFORMAT, DISPLAY_PAGE_NUM, PAGESIZE_OPTIONS } from '@/constants';
+import { MokaTable } from '@components';
+import { BASIC_DATEFORMAT, DISPLAY_PAGE_NUM } from '@/constants';
 import FileItemRenderer from './FileItemRenderer';
 import { boardColumnDefs } from './BoardsContentsListAgGridColumns';
-import { AgGridReact } from 'ag-grid-react';
 import { GET_LIST_MENU_CONTENTS_LIST, changeListMenuSearchOption, getListMenuContentsList } from '@store/board';
-import { Button } from 'react-bootstrap';
 
 /**
  * 게시판 관리 > 게시글 관리 > 게시판 글 목록 AgGrid
@@ -29,6 +26,7 @@ const BoardsContentsListAgGrid = ({ match }) => {
     // const [columnData, setColumnData] = useState(columnDefs);
     // const [gridApi, setGridApi] = useState(null);
     // const [gridColumnApi, setGridColumnApi] = useState(null);
+
     // rowData
     const [rowData, setRowData] = useState([]);
 
@@ -44,21 +42,6 @@ const BoardsContentsListAgGrid = ({ match }) => {
             history.push(`${match.path}/${row.boardId}/${row.parentBoardSeq}/reply/${row.boardSeq}`);
         }
     };
-
-    // const onGridReady = (params) => {
-    //     setGridApi(params.api);
-    //     setGridColumnApi(params.columnApi);
-    // };
-
-    // const handleRowClicked = (row) => {
-    //     // 게시글의 답변이 없으면
-    //     if (row.boardSeq === row.parentBoardSeq) {
-    //         history.push(`${match.path}/${row.boardId}/${row.boardSeq}`);
-    //     } else {
-    //         // 게시글의 답변이 있으면
-    //         history.push(`${match.path}/${row.boardId}/${row.parentBoardSeq}/reply/${row.boardSeq}`);
-    //     }
-    // };
 
     /**
      * 테이블 검색 옵션 변경
@@ -94,94 +77,27 @@ const BoardsContentsListAgGrid = ({ match }) => {
                 };
             }),
         );
-
-        // const newColumns = gridApi.getColumnDefs();
-        // setColumns(newColumns);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [list]);
 
-    // useEffect(() => {
-    //     if (gridApi) {
-    //         let newColumns = gridApi.getColumnDefs();
-    //         newColumns.forEach((c) => {
-    //             Object.keys(c).forEach((key) => {
-    //                 if (c[key] == null) {
-    //                     delete c[key];
-    //                 }
-    //             });
-    //         });
-    //         if (selectBoard.titlePrefixNm1) {
-    //             produce(newColumns, (draft) => {
-    //                 draft.splice(1, 0, {
-    //                     headerName: selectBoard.titlePrefixNm1,
-    //                     field: 'titlePrefix1',
-    //                     // cellStyle: {
-    //                     //     paddingTop: '8px',
-    //                     // },
-    //                     width: 80,
-    //                     tooltipField: 'titlePrefix1',
-    //                 });
-    //             });
-    //         }
-    //         //setColumnData(newColumns);
-    //         //gridApi.setColumnDefs(newColumns);
-    //         console.log(gridApi);
-    //         console.log(newColumns);
-    //     }
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, [gridApi, selectBoard]);
-
-    console.log(boardColumnDefs(selectBoard));
-
     return (
-        <>
-            {/* {columnsState.length > 0 && ( */}
-            <MokaTable
-                className="overflow-hidden flex-fill"
-                headerHeight={50}
-                rowHeight={50}
-                columnDefs={boardColumnDefs(selectBoard)}
-                rowData={rowData}
-                onRowNodeId={(row) => row.boardSeq}
-                onRowClicked={handleRowClicked}
-                loading={loading}
-                total={total}
-                page={search.page}
-                size={search.size}
-                displayPageNum={DISPLAY_PAGE_NUM}
-                onChangeSearchOption={handleChangeSearchOption}
-                selected={contentsInfo.boardSeq || contentsReply.boardSeq}
-                frameworkComponents={{ fileItemRenderer: FileItemRenderer }}
-            />
-            {/* )} */}
-            {/* <div className="position-relative ag-theme-moka-grid overflow-hidden flex-fill">
-                {loading && <MokaLoader />}
-                <AgGridReact
-                    onGridReady={onGridReady}
-                    columnDefs={columnData}
-                    headerHeight={50}
-                    rowHeight={50}
-                    rowData={rowData}
-                    getRowNodeId={(row) => row.boardSeq}
-                    onCellClicked={handleRowClicked}
-                    tooltipShowDelay={0}
-                    localeText={{ noRowsToShow: '조회 결과가 없습니다', loadingOoo: '조회 중입니다' }}
-                    // onSelectionChanged={handleSelectionChanged}
-                    rowSelection="single"
-                    applyColumnDefOrder
-                />
-            </div>
-            <div className="mt-card">
-                <MokaPagination
-                    total={total}
-                    page={search.page}
-                    size={search.size}
-                    onChangeSearchOption={handleChangeSearchOption}
-                    pageSizes={PAGESIZE_OPTIONS}
-                    displayPageNum={DISPLAY_PAGE_NUM}
-                />
-            </div> */}
-        </>
+        <MokaTable
+            className="overflow-hidden flex-fill"
+            headerHeight={50}
+            rowHeight={50}
+            columnDefs={boardColumnDefs(selectBoard)}
+            rowData={rowData}
+            onRowNodeId={(row) => row.boardSeq}
+            onRowClicked={handleRowClicked}
+            loading={loading}
+            total={total}
+            page={search.page}
+            size={search.size}
+            displayPageNum={DISPLAY_PAGE_NUM}
+            onChangeSearchOption={handleChangeSearchOption}
+            selected={contentsInfo.boardSeq || contentsReply.boardSeq}
+            frameworkComponents={{ fileItemRenderer: FileItemRenderer }}
+            applyColumnDefOrder
+        />
     );
 };
 

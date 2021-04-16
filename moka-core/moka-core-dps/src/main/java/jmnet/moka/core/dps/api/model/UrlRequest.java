b8418bda50061d6addea4e715fae21c73737ac47
장미라 <ohtah78@gmail.com>
2021-04-16 20:28:13 +0900

    [dps] type=url버그수정

--- moka-core/moka-core-dps/src/main/java/jmnet/moka/core/dps/api/model/UrlRequest.java
+++ moka-core/moka-core-dps/src/main/java/jmnet/moka/core/dps/api/model/UrlRequest.java
@@ -6,62 +6,59 @@ import java.util.List;
 import jmnet.moka.core.dps.api.handler.UrlRequestHandler;
 
 public class UrlRequest extends AbstractRequest {
-	
-	private static List<String> emptyList = new ArrayList<String>();
-	private String type;
-	private boolean async;
-	private String resultName;
-	private String selector;
-	private String url;
-	private List<String> include = emptyList;
-	private List<String> exclude = emptyList;
-	
-	public UrlRequest(String type, boolean async, String resultName, String url, String include, String exclude, String selector) {
-		super(type, async, resultName);
-		this.url = url;
-		this.selector = selector;
-		if ( include != null && include.length() > 0) {
-			this.include = Arrays.asList(include.split(","));
-		}
-		if ( exclude != null && exclude.length() > 0) {
-			this.exclude = Arrays.asList(include.split(","));
-		}
-	}
 
-	public String getType() {
-		return this.type;
-	}
-	
-	public boolean getAsync() {
-		return this.async;
-	}
-	
-	public String getResultName() {
-		return this.resultName;
-	}
-	
-	public String getUrl() {
-		return this.url;
-	}
-	
-	public String getSelector() {
-		return this.selector;
-	}
-	
-	public List<String> getIncude(){
-		return this.include;
-	}
+    private static List<String> emptyList = new ArrayList<String>();
+    private String selector;
+    private String url;
+    private List<String> include = emptyList;
+    private List<String> exclude = emptyList;
 
-	public List<String> getExclude(){
-		return this.exclude;
-	}
-	
-	@Override
-	public Class<?> getHandlerClass() {
-		return UrlRequestHandler.class;
-	}
-	
-	public String toString() {
-		return String.join("/", this.type,this.url);
-	}
+    public UrlRequest(String type, boolean async, String resultName, String url, String include, String exclude, String selector) {
+        super(type, async, resultName);
+        this.url = url;
+        this.selector = selector;
+        if (include != null && include.length() > 0) {
+            this.include = Arrays.asList(include.split(","));
+        }
+        if (exclude != null && exclude.length() > 0) {
+            this.exclude = Arrays.asList(include.split(","));
+        }
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public boolean getAsync() {
+        return this.async;
+    }
+
+    public String getResultName() {
+        return this.resultName;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public String getSelector() {
+        return this.selector;
+    }
+
+    public List<String> getIncude() {
+        return this.include;
+    }
+
+    public List<String> getExclude() {
+        return this.exclude;
+    }
+
+    @Override
+    public Class<?> getHandlerClass() {
+        return UrlRequestHandler.class;
+    }
+
+    public String toString() {
+        return String.join("/", this.type, this.url);
+    }
 }

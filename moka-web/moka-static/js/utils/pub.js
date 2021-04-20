c275c55f901df76eed6a4653625cdccbe1c9bfea
이진욱 <jw.lee.801207@gmail.com>
2021-04-20 16:37:01 +0900

    [publish] UI 공통정리중

--- moka-web/moka-static/js/utils/pub.js
+++ moka-web/moka-static/js/utils/pub.js
@@ -1,58 +1,63 @@
 $(document).ready(function() {
 
-    $('.select2').select2();
+
 
 
     //li형 select box
-    $(function(){
-        var select = new CustomSelectBox('.select_box');
+
+    $(".btn_close").off().on("click", function(){
+        $btnCk= $(this);
+		closeLayer($btnCk);
     });
 
-    function CustomSelectBox(selector){
-        this.$selectBox = null,
-        this.$select = null,
-        this.$list = null,
-        this.$listLi = null;
-        
-        CustomSelectBox.prototype.init = function(selector){
-            this.$selectBox = $(selector);
-            this.$select = this.$selectBox.find('.box .select');
-            this.$list = this.$selectBox.find('.box .list');
-            this.$listLi = this.$list.children('li');
+
+    //li형 select box
+    const body = document.querySelector('body');
+    const select = document.querySelector(`[data-role="selectBox"]`);
+    const values = select.querySelector(`[date-value="optValue"]`);
+    const option = select.querySelector('ul');
+    const opts = option.querySelectorAll('li');
+
+    /* 셀렉트영역 클릭 시 옵션 숨기기, 보이기 */
+    function selects(e){
+        e.stopPropagation();
+        option.setAttribute('style',`top:${select.offsetHeight}px`)
+        if(option.classList.contains('hide')){
+            option.classList.remove('hide');
+            option.classList.add('show');
+        }else{
+            option.classList.add('hide');
+            option.classList.remove('show');
         }
-        CustomSelectBox.prototype.initEvent = function(e){
-            var that = this;
-            this.$select.on('click', function(e){
-                that.listOn();
-            });
-            this.$listLi.on('click', function(e){
-                that.listSelect($(this));
-            });
-            $(document).on('click', function(e){
-                that.listOff($(e.target));
-            });
-        }
-        CustomSelectBox.prototype.listOn = function(){
-            this.$selectBox.toggleClass('on');
-            if(this.$selectBox.hasClass('on')){
-                this.$list.css('display', 'block');
-            }else{
-                this.$list.css('display', 'none');
-            };
-        }
-        CustomSelectBox.prototype.listSelect = function($target){
-            $target.addClass('selected').siblings('li').removeClass('selected');
-            this.$selectBox.removeClass('on');
-            this.$select.text($target.text());
-            this.$list.css('display', 'none');
-        }
-        CustomSelectBox.prototype.listOff = function($target){
-            if(!$target.is(this.$select) && this.$selectBox.hasClass('on')){
-                this.$selectBox.removeClass('on');
-                this.$list.css('display', 'none');
-            };
-        }
-        this.init(selector);
-        this.initEvent();
+        selectOpt();
     }
+
+    /* 옵션선택 */
+    function selectOpt(){
+        opts.forEach(opt=>{
+            const innerValue = opt.innerHTML;
+            function changeValue(){
+                values.innerHTML = innerValue;
+            }
+            opt.addEventListener('click',changeValue)
+        });
+    }
+
+    /* 렌더링 시 옵션의 첫번째 항목 기본 선택 */
+    function selectFirst(){
+        const firstValue = opts[0].innerHTML;
+        values.innerHTML = `${firstValue}`
+    }
+
+    /* 옵션밖의 영역(=바디) 클릭 시 옵션 숨김 */
+    function hideSelect(){
+        if(option.classList.contains('show')){
+            option.classList.add('hide');
+            option.classList.remove('show');
+        }
+    }
+
+    selectFirst();
+    select.addEventListener('click',selects);
+    body.addEventListener('click',hideSelect);
 });

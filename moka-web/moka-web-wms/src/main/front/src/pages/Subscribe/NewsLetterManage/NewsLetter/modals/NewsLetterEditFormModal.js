8f6892956620d4fadae7eff77faf2020cb9c5962
정준영 <fdjy0101@gmail.com>
2021-04-20 09:33:39 +0900

    [react] 뉴스레터 편집 폼 모달 생성

--- moka-web/moka-web-wms/src/main/front/src/pages/Subscribe/NewsLetterManage/NewsLetter/modals/NewsLetterEditFormModal.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Subscribe/NewsLetterManage/NewsLetter/modals/NewsLetterEditFormModal.js
@@ -1,20 +1,38 @@
-import React, { useState, useCallback } from 'react';
+import React, { useEffect, useCallback } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import Form from 'react-bootstrap/Form';
 import { MokaModal, MokaSearchInput, MokaTable } from '@/components';
+import { getEditFormList, GET_EDIT_FORM_LIST, changeSearchOption, clearStore } from '@store/editForm';
 
 /**
  * 뉴스레터 관리 > 뉴스레터 목록 > 편집폼 선택 모달
  */
 const NewsLetterEditFormModal = ({ show, onHide }) => {
-    const [total] = useState(0);
-    const [loading] = useState(false);
-    const [search] = useState({ page: 1, size: 10 });
+    const dispatch = useDispatch();
+    const { list, total, search, loading } = useSelector(
+        (store) => ({
+            editForm: store.editForm.editForm,
+            list: store.editForm.list,
+            total: store.editForm.total,
+            search: store.editForm.search,
+            loading: store.loading[GET_EDIT_FORM_LIST],
+        }),
+        shallowEqual,
+    );
 
     /**
-     * 테이블 검색 옵션 변경
-     * @param {object} payload 변경된 값
+     * 테이블 검색옵션 변경
      */
-    const handleChangeSearchOption = useCallback((search) => console.log(search), []);
+    const handleChangeSearchOption = useCallback(
+        ({ key, value }) => {
+            let temp = { ...search, [key]: value };
+            if (key !== 'page') {
+                temp['page'] = 0;
+            }
+            dispatch(getEditFormList(changeSearchOption(temp)));
+        },
+        [dispatch, search],
+    );
 
     /**
      * 목록 Row클릭
@@ -22,6 +40,14 @@ const NewsLetterEditFormModal = ({ show, onHide }) => {
     const handleRowClicked = useCallback((row) => {
         console.log(row);
     }, []);
+
+    useEffect(() => {
+        if (show) {
+            dispatch(getEditFormList());
+        } else {
+            dispatch(clearStore());
+        }
+    }, [show, dispatch]);
 
     return (
         <MokaModal
@@ -52,22 +78,22 @@ const NewsLetterEditFormModal = ({ show, onHide }) => {
                     },
                     {
                         headerName: 'NO',
-                        field: 'no',
+                        field: 'formSeq',
                         width: 65,
                     },
                     {
                         headerName: 'ID',
-                        field: 'id',
-                        width: 70,
+                        field: 'formId',
+                        width: 100,
                     },
                     {
                         headerName: 'Form 명',
-                        field: 'formNm',
+                        field: 'formName',
                         flex: 1,
                     },
                 ]}
-                rowData={total}
-                onRowNodeId={(params) => params.seq}
+                rowData={list}
+                onRowNodeId={(params) => params.formSeq}
                 onRowClicked={handleRowClicked}
                 loading={loading}
                 total={total}

32e1ea9ac7f961a32c95d8ace871791c9302de09
이진욱 <jw.lee.801207@gmail.com>
2021-04-21 13:35:48 +0900

    [publish] UI 공통정리중

--- moka-web/moka-static/js/utils/pub.js
+++ moka-web/moka-static/js/utils/pub.js
@@ -3,13 +3,21 @@ $(document).ready(function() {
 
     //li형 select box
     $(".dropdown_toggle").on("click", function(){
-		$(this).parent(".dropdown").toggleClass("open");
+		  $(this).parent(".dropdown").toggleClass("open");
     });
     $(".dropdown_item a").on("click", function(){
         $(this).parents().siblings().removeClass("active");
         $(this).parents().addClass("active");
-		$(this).parents(".dropdown").toggleClass("open");
+        $(this).parents(".dropdown").toggleClass("open");
     });
+
+    $(document).mouseup(function (e){// 외부영역 클릭 시 팝업 닫기
+      var LayerPopup = $(".dropdown");
+      if(LayerPopup.has(e.target).length === 0){
+        LayerPopup.removeClass("open");
+      }
+    });
+    
     
 
 });

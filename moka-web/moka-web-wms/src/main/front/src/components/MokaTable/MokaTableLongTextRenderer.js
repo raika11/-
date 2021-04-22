73cb517c4c8cdff1ccf87255e935162b3dd18ac8
전현지 <emproject525@gmail.com>
2021-04-22 10:48:26 +0900

    [react] longTextRenderer cellClass 받게 수정

--- moka-web/moka-web-wms/src/main/front/src/components/MokaTable/MokaTableLongTextRenderer.js
+++ moka-web/moka-web-wms/src/main/front/src/components/MokaTable/MokaTableLongTextRenderer.js
@@ -1,14 +1,19 @@
 import React, { useState, useImperativeHandle, forwardRef } from 'react';
+import clsx from 'clsx';
 import { GRID_ROW_HEIGHT, GRID_LINE_HEIGHT, WEBKIT_BOX } from '@/style_constants';
 
+const defaultProps = {
+    cellClass: 'ag-preline-cell',
+};
+
 /**
- * ag-grid 셀에 긴 텍스트를 그리는 컴포넌트.
- * text는 field로 넘어온 값을 data에서 찾아서 사용한다
+ * ag-grid 셀에 긴 텍스트를 그리는 컴포넌트
+ * 노출 텍스트는 data[field] 값을 사용한다
  *
  * @param {object} params ag grid params
  */
 const MokaTableLongTextRenderer = forwardRef((params, ref) => {
-    const { colDef } = params;
+    const { colDef, cellClass } = params;
     const [field] = useState(colDef.field);
     const [data, setData] = useState(params.node.data);
     const [style, setStyle] = useState({});
@@ -28,10 +33,12 @@ const MokaTableLongTextRenderer = forwardRef((params, ref) => {
     }, [params]);
 
     return (
-        <div className="h-100 w-100 ag-preline-cell">
+        <div className={clsx('h-100 w-100', cellClass)}>
             <span style={{ margin: 'auto 0', lineHeight: `${GRID_LINE_HEIGHT.M}px`, ...style }}>{data?.[field]}</span>
         </div>
     );
 });
+
+MokaTableLongTextRenderer.defaultProps = defaultProps;
 
 export default MokaTableLongTextRenderer;

9632b375e5c9a3a5ef3d3f813409a69657048fcf
정준영 <fdjy0101@gmail.com>
2021-04-22 13:28:49 +0900

    [react] 뉴스레터 스토어 수정

--- moka-web/moka-web-wms/src/main/front/src/store/newsLetter/newsLetterReducer.js
+++ moka-web/moka-web-wms/src/main/front/src/store/newsLetter/newsLetterReducer.js
@@ -8,17 +8,22 @@ import { PAGESIZE_OPTIONS } from '@/constants';
  */
 export const initialState = {
     newsLetter: {
+        list: [],
+        total: 0,
         error: null,
         search: {
             sort: 'letterSeq,desc',
             page: 0,
             size: PAGESIZE_OPTIONS[0],
+            searchType: '',
+            keyword: '',
+            useTotal: '',
             letterType: '',
             status: '',
             sendType: '',
-            abtestYn: '',
             startDt: null,
             endDt: null,
+            abtestYn: '',
             letterName: '',
         },
         // 모달 검색 조건
@@ -104,10 +109,41 @@ export default handleActions(
     {
         // 스토어 초기화
         [act.CLEAR_STORE]: () => initialState,
+        [act.CLEAR_NEWS_LETTER]: (state) => {
+            return produce(state, (draft) => {
+                draft.newsLetter.letterInfo = initialState.newsLetter.letterInfo;
+            });
+        },
         // 검색조건 변경
         [act.CHANGE_NEWS_LETTER_SEARCH_OPTION]: (state, { payload }) => {
             return produce(state, (draft) => {
                 draft.newsLetter.search = payload;
+            });
+        },
+        // 뉴스레터 상품 목록 조회
+        [act.GET_NEWS_LETTER_LIST_SUCCESS]: (state, { payload: { body } }) => {
+            return produce(state, (draft) => {
+                draft.newsLetter.total = body.totalCnt;
+                draft.newsLetter.list = body.list;
+                draft.newsLetter.error = initialState.error;
+            });
+        },
+        [act.GET_NEWS_LETTER_LIST_FAILURE]: (state, { payload }) => {
+            return produce(state, (draft) => {
+                draft.newsLetter.total = initialState.newsLetter.total;
+                draft.newsLetter.list = initialState.newsLetter.list;
+                draft.newsLetter.error = payload;
+            });
+        },
+        // 뉴스레터 상품 상세 조회
+        [act.GET_NEWS_LETTER_SUCCESS]: (state, { payload: { body } }) => {
+            return produce(state, (draft) => {
+                draft.newsLetter.letterInfo = body;
+            });
+        },
+        [act.GET_NEWS_LETTER_FAILURE]: (state) => {
+            return produce(state, (draft) => {
+                draft.newsLetter.letterInfo = initialState.newsLetter.letterInfo;
             });
         },
     },
--- moka-web/moka-web-wms/src/main/front/src/store/newsLetter/newsLetterSaga.js
+++ moka-web/moka-web-wms/src/main/front/src/store/newsLetter/newsLetterSaga.js
@@ -1,4 +1,15 @@
-// import * as act from './newsLetterAction';
-// import * as api from './newsLetterApi';
+import { takeLatest } from 'redux-saga/effects';
+import { callApiAfterActions, createRequestSaga } from '@store/commons/saga';
+import * as act from './newsLetterAction';
+import * as api from './newsLetterApi';
 
-export default function* newsLetterSaga() {}
+// 뉴스레터 상품 목록 조회
+const getNewsLetterList = callApiAfterActions(act.GET_NEWS_LETTER_LIST, api.getNewsLetterList, (state) => state.newsLetter.newsLetter);
+
+// 뉴스레터 상품 상세 조회
+const getNewsLetter = createRequestSaga(act.GET_NEWS_LETTER, api.getNewsLetter);
+
+export default function* newsLetterSaga() {
+    yield takeLatest(act.GET_NEWS_LETTER_LIST, getNewsLetterList);
+    yield takeLatest(act.GET_NEWS_LETTER, getNewsLetter);
+}

4ddc0a3aacc471b7a752274ad2afacf35803bb9d
김남균 <kingnk007@gmail.com>
2021-04-22 21:46:00 +0900

    [script] 기사 본문의 기자 목록 스크립트 처리 추가

---
+++ moka-web/moka-static/js/resource.js
@@ -0,0 +1,19 @@
+/**
+ * 스크립트에서 사용할 상수 정의.
+ * 대문자로 정의 한다.
+ */
+
+/**
+ * BREAKPOINTS
+ */
+var BREAKPOINT_XLARGE = 1200;
+var BREAKPOINT_LARGE = 1024;
+var BREAKPOINT_MEDIUM = 768;
+
+/**
+ * 기사페이지에서 노출되는 기자 최대 수
+ */
+var MAX_LARGE_REPORTER_NUM = 5;
+var MAX_MEDIUM_REPORTER_NUM = 3;
+
+
--- moka-web/moka-static/js/utils/article.js
+++ moka-web/moka-static/js/utils/article.js
@@ -35,22 +35,57 @@ $(document).ready(function() {
         }
     });
 
+    /* 화면 크기에 따른 기자 목록 */
+    bylineControl(window.innerWidth);
+    $(window).resize(function(e){
+        bylineControl(window.innerWidth);
+    })
+
+    setTimeout(function() {
+        setBylinePopPos()
+    }, 500);
+
+    /* 기자 더보기(외○명) 클릭 */
+    $(".btn_byline_more").click(function(){
+        $this = $(this);
+        var width = window.innerWidth;
+        $this.addClass("active");
+        if($this.hasClass("active")){
+            if(width >= BREAKPOINT_LARGE){
+                $(".byline > a").removeClass("hide");
+                $this.toggleClass("hide");
+            } else {
+                $(".layer_byline").addClass("active");
+            }
+        }
+    });
+
+    /* byline popup close event */
+    $(".layer_byline .btn_close").click(function(){
+        $(".btn_byline_more").removeClass("active");
+        reporterControl(window.innerWidth);
+    });
 });
+
 /* 임시 레이어팝업 */
 function toggleOpenLayer(obj){
 	$(obj).toggleClass("active");
 }
+
 function toggleOpen(obj){
 	$(obj).parent().toggleClass("open");
 }
+
 function openLayer(obj){
 	$(obj).addClass("active");
 }
+
 function closeLayer($btnCk){
 	$btnCk.parents(".layer_popup.active").removeClass("active");
 	$btnCk.parents(".layer_comment.active").removeClass("active");
 	$btnCk.parents(".layer_ticker.active").removeClass("active");
 }
+
 function btnCloseModal(){
 	$(".btn_close").off().on("click", function(){
         $btnCk= $(this);
@@ -58,3 +93,59 @@ function btnCloseModal(){
     });
 }
 
+/* 화면 크기에 따른 기자 노출 수 조정*/
+function bylineControl(width){
+    var maxReporterNum = MAX_MEDIUM_REPORTER_NUM;
+    if(width >= BREAKPOINT_LARGE){
+        maxReporterNum = MAX_LARGE_REPORTER_NUM;   
+    }
+
+    var $byline = $(".byline > a");
+    var $bylineMore = $(".btn_byline_more");
+    var $bylinePop = $(".layer_byline");
+    var hiddenCount = $byline.length - maxReporterNum;
+
+    $byline.each(function(index, item){
+        if(index >= maxReporterNum){
+            $(item).addClass("hide");
+        } else {
+            $(item).removeClass("hide");
+            bylineWidth = $(item).outerWidth();
+        }
+    });
+
+    if(hiddenCount>0){
+        $bylineMore.removeClass("hide");
+        $bylineMore.find("span").text(hiddenCount)
+
+        if($bylineMore.hasClass("active")){
+            if(width >= BREAKPOINT_LARGE){
+                $byline.removeClass("hide");
+                $bylineMore.addClass("hide");
+                $bylinePop.removeClass("active");
+            } else {
+                $bylinePop.addClass("active");
+            }
+        } 
+    } else {
+        $bylineMore.addClass("hide")
+    }
+}
+
+/* set byline popup position */
+function setBylinePopPos(){
+    var $byline = $(".byline > a");
+    var $bylineMore = $(".btn_byline_more");
+    var $bylinePop = $(".layer_byline");
+    var bylineWidth= 0;
+
+    $byline.each(function(index, item){
+        var $item = $(item);
+        if(!$item.hasClass("hide")){
+            bylineWidth = $(item).width();
+        }
+    });
+
+    var bylinePopRight = bylineWidth + $bylineMore.width();
+    $bylinePop.css("left",bylinePopRight+12+"px");
+}

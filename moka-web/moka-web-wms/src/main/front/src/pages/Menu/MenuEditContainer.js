5ad3c4525f070ef36cc3668391f912a479845ae6
전현지 <emproject525@gmail.com>
2021-04-23 16:10:38 +0900

    [react] 메뉴 탭 제거

--- moka-web/moka-web-wms/src/main/front/src/pages/Menu/MenuEditContainer.js
+++ moka-web/moka-web-wms/src/main/front/src/pages/Menu/MenuEditContainer.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import { MokaIconTabs } from '@components';
-import { CARD_DEFAULT_HEIGHT } from '@/style_constants';
 import MenuEditContainerEdit from './MenuEditContainerEdit';
 
 const MenuEditContainer = (props) => {
     const { handleClickDelete, menuSeq, depth, parentMenuId } = props;
-
-    return (
-        <>
-            {/* 탭 */}
-            <MokaIconTabs
-                foldable={false}
-                className="flex-fill"
-                tabContentClassName="w-100"
-                height={CARD_DEFAULT_HEIGHT}
-                tabs={[<MenuEditContainerEdit handleClickDelete={handleClickDelete} menuSeq={menuSeq} depth={depth} parentMenuId={parentMenuId} />]}
-                tabNavWidth={48}
-                tabNavPosition="right"
-                tabNavs={[{ title: '메뉴정보', text: 'Info' }]}
-            />
-        </>
-    );
+    return <MenuEditContainerEdit handleClickDelete={handleClickDelete} menuSeq={menuSeq} depth={depth} parentMenuId={parentMenuId} />;
 };
 
 export default MenuEditContainer;

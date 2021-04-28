bcabd2a7360a58c99f5c3bd91ac19a31295b8962
이진욱 <jw.lee.801207@gmail.com>
2021-04-28 13:51:55 +0900

    [publish] UI 공통 - 페이지 타이들 진행중

--- moka-web/moka-static/html/guide/guide.js
+++ moka-web/moka-static/html/guide/guide.js
@@ -16,11 +16,14 @@ $(document).ready( function() {
 		$(this).parents('.sample_wrap').attr('style', `width: ${view_width}`);
 	});
 
-	// 반응형 미리보기 버튼
+	// 테마별색상 미리보기 버튼
 	$(".btn_view_color").click(function() {
-		var view_width = $(this).attr('data-rel');
-		$(this).parents('#num_ico').removeClass().addClass(view_width);
-		
-		// $(this).parents('#num_ico').addClass(view_width);
+		var view_color = $(this).attr('data-rel');
+		$(this).parents('#num_ico').removeClass().addClass(view_color);
+	});
+
+	$(".btn_view_drop").click(function() {
+		var view_position = $(this).attr('data-rel');
+		$(this).parent().next('.dropdown_menu').addClass(view_position);
 	});
 });

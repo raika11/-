9dd79589e90c7dc044859a89b73ed0195bcb05ef
guregal <guregal@nate.com>
2021-04-29 14:15:21 +0900

    [tps] A/B테스트 결과 목록 조회 API 등록

--- moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/abtest/mapper/AbTestCaseMapper.java
+++ moka-core/moka-core-tps/src/main/java/jmnet/moka/core/tps/mvc/abtest/mapper/AbTestCaseMapper.java
@@ -3,6 +3,7 @@ package jmnet.moka.core.tps.mvc.abtest.mapper;
 import java.util.List;
 import jmnet.moka.common.data.mybatis.support.BaseMapper;
 import jmnet.moka.core.tps.mvc.abtest.dto.AbTestCaseSearchDTO;
+import jmnet.moka.core.tps.mvc.abtest.vo.AbTestCaseResultVO;
 import jmnet.moka.core.tps.mvc.abtest.vo.AbTestCaseSaveVO;
 import jmnet.moka.core.tps.mvc.abtest.vo.AbTestCaseVO;
 
@@ -19,6 +20,14 @@ import jmnet.moka.core.tps.mvc.abtest.vo.AbTestCaseVO;
  * @since 2021-04-15 13:19
  */
 public interface AbTestCaseMapper extends BaseMapper<AbTestCaseVO, AbTestCaseSearchDTO> {
+
+    /**
+     * ABTest 목록 조회
+     *
+     * @param searchDTO 검색 조건
+     * @return 조회 결과
+     */
+    List<AbTestCaseResultVO> findResultList(AbTestCaseSearchDTO searchDTO);
 
     /**
      * ABTest 목록 조회

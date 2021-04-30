73efb966473de4a2e540393b47d2f15f1cff9fda
조용호 <yhcho2675@gmail.com>
2021-04-30 09:11:08 +0900

    [publish] 섹션 홈 퍼블리싱

---
+++ moka-web/moka-static/js/section.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    var swiper_highlight = new Swiper('.swiper-container.swiper_highlight_list', {
+        autoHeight: true,
+        spaceBetween: 1,
+        touchRatio: 1,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpointsInverse: true,
+        breakpoints: {
+            1024: {
+                touchRatio: 0
+            }
+        }
+    });
+});
+    

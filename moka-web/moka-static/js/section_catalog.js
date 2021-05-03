a34dc8ed71a5d24ec2cab0b04fe6e8b6e15eb758
김수연 <dysgsbgi@gmail.com>
2021-05-03 09:02:59 +0900

    [publish] 섹션 - 포토 작업중

--- moka-web/moka-static/js/section_catalog.js
+++ moka-web/moka-static/js/section_catalog.js
@@ -1,9 +1,34 @@
 $(document).ready(function() {
-    var swiper_highlight_inner = new Swiper('.swiper-container.swiper_list', {
+    var showcase= new Swiper('.showcase_swiper .swiper-container', {
+        autoplay: {
+            delay: 3000,
+        },
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
         },
+        breakpoints: {
+            1023: {
+                autoplay: false,
+            }
+        },
     });
+    showcase.autoplay.stop();
+    $(".swiper-button-pause").click(function(){
+        showcase.autoplay.stop();
+    });
+    $(".swiper-button-play").click(function(){
+        showcase.autoplay.start();
+    });
+
+    $(window).resize(function(){
+        if ( $(window).width() < 1024 ) {
+            showcase.autoplay.stop();
+        } else {
+            showcase.autoplay.start();
+        }
+    });
+    
 });
+
 
